cfcffda083351a251db46714b63c511feb67ee27
rlane <>
2012-11-28 13:16:04 +0000

    tidy up code

--- plugins/ICGenTauProductProducer.hh
+++ plugins/ICGenTauProductProducer.hh
@@ -28,18 +28,9 @@ class ICGenTauProductProducer : public edm::EDProducer {
       virtual void endRun(edm::Run&, edm::EventSetup const&);
       virtual void beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
       virtual void endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
-      //virtual void FindDecayProducts(const reco::GenParticle* theParticle, std::auto_ptr<std::vector<unsigned> > container,
-      //edm::Handle<reco::GenParticleCollection> partCollection);
       virtual void FindDecayProducts(const reco::GenParticle* theParticle, std::vector<const reco::GenParticle *> &container);
 
       // ----------member data ---------------------------
-      edm::InputTag input_label_;
-      std::string branch_name_;
-      bool add_all_;
-      double add_all_pt_cut_;
-      double add_all_eta_cut_;
-      std::vector<ic::GenJet> *cand_vec;
       std::vector<std::string> merge_labels_;
-      bool store_gen_particles_;
       
 };
