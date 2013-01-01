d30ad171fac3591c50b7b4a8462d02945520c0f3
agilbert <>
2013-01-01 20:28:16 +0000

    batch submission working at IC using SSD array; tidied up scripts in preparation for Moriond and paper

--- Analysis/HiggsTauTau/scripts/test_mc_2012.sh
+++ Analysis/HiggsTauTau/scripts/test_mc_2012.sh
@@ -18,14 +18,14 @@ else
 fi
 echo $CONFIG
 
-FILELIST=filelists/Dec2/MC_53X
+FILELIST=filelists/Dec26/MC_53X
 PATHS=(
 'VBF_HToTauTau_M-125'
 )
  for i in "${PATHS[@]}"
  do
 
- ./bin/HiggsTauTau --cfg=$CONFIG --filelist=$FILELIST/"$i"_mt_skim_filelist.dat --channel=mt --output_folder=./ --svfit_mode=0 --output_name="$i"_mt_2012.root --make_sync_ntuple=true
+   ./bin/HiggsTauTau --cfg=$CONFIG --filelist=$FILELIST/"$i"_mt_skim_filelist.dat --channel=mt --output_folder=./ --svfit_mode=0 --output_name="$i"_mt_2012.root --make_sync_ntuple=true --do_skim=false --skim_path=/vols/cms04/ajg06/Dec26/MC_53X/VBF_HToTauTau_M-125/mt_skim/
  #./bin/HiggsTauTau --cfg=$CONFIG --filelist=$FILELIST/"$i"_mutauSkim_IC_filelist.dat --channel=em --output_folder=./ --svfit_mode=0 --output_name=MC_TEST_2012.root
  #./bin/HiggsTauTau --cfg=$CONFIG --filelist=$FILELIST/"$i"_mutauSkim_IC_filelist.dat --channel=em --output_folder=./ --svfit_mode=0 --output_name=MC_TEST_2012.root
 
