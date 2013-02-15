5261da805a1a1edd56d5b343cadfe352597ec2b0
amagnan <>
2013-02-15 17:12:12 +0000

    *** empty log message ***

--- Analysis/HiggsNuNu/scripts/submit_mc.sh
+++ Analysis/HiggsNuNu/scripts/submit_mc.sh
@@ -17,75 +17,76 @@ echo "Config file: $CONFIG"
 #Process HiggsNuNu specific backgrounds
 
 #Signal files and DYtoNuNu
-#PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Dec1/VBFH120/
-#for FILELIST in `ls filelists/Dec1_VBFH120_*`
-#  do
-#  echo "Processing files in "$FILELIST
-#
-#  echo $FILELIST > tmp.txt
-#  sed "s/filelists\/Dec1_VBFH120_//" tmp.txt > tmp2.txt
-#
-#  JOB=MC_`sed "s/\.dat//" tmp2.txt`
-#
-#  rm tmp.txt tmp2.txt
-#
-#  echo "JOB name = $JOB"
-#
-#  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> jobs/$JOB.log" jobs/$JOB.sh
-#  $JOBSUBMIT jobs/$JOB.sh
-#
-#done
-
-
-#VBFZ
-#PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Dec5/VBFZ/
-#for FILELIST in `ls filelists/Dec5_VBFZ*`
-#  do
-#  echo "Processing files in "$FILELIST
-#
-#  echo $FILELIST > tmp.txt
-#  sed "s/filelists\/Dec5_VBFZ_//" tmp.txt > tmp2.txt
-#
-#  JOB=MC_`sed "s/\.dat//" tmp2.txt`
-#
-#  rm tmp.txt tmp2.txt
-#
-#  echo "JOB name = $JOB"
-#
-#  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> jobs/$JOB.log" jobs/$JOB.sh
-#  $JOBSUBMIT jobs/$JOB.sh
-#
-#done
-
-#GJets
-#PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Feb1/BKG/
-#for FILELIST in `ls filelists/Feb1_BKG_*`
-#  do
-#  echo "Processing files in "$FILELIST
-#
-#  echo $FILELIST > tmp.txt
-#  sed "s/filelists\/Feb1_BKG_//" tmp.txt > tmp2.txt
-#
-#  JOB=MC_`sed "s/\.dat//" tmp2.txt`
-#
-#  rm tmp.txt tmp2.txt
-#
-#  echo "JOB name = $JOB"
-#
-#  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> jobs/$JOB.log" jobs/$JOB.sh
-#  $JOBSUBMIT jobs/$JOB.sh
-#
-#done
-
-#Process bkg common with HiggsTautau
-PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/agilbert/Dec30/MC_53X/
-#for FILELIST in `ls filelists/Dec30_MC_*`
-for FILELIST in `ls filelists/Dec30_MC_53X_DY*`
+PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Dec1/VBFH120/
+for FILELIST in `ls filelists/Dec1_VBFH120_* filelists/Dec1_ZJets_*`
   do
   echo "Processing files in "$FILELIST
 
   echo $FILELIST > tmp.txt
-  sed "s/filelists\/Dec30_MC_53X_//" tmp.txt > tmp2.txt
+  sed "s/filelists\/Dec1_VBFH120_//" tmp.txt > tmp2a.txt
+  sed "s/filelists\/Dec1_ZJets_//" tmp2a.txt > tmp2.txt
+
+  JOB=MC_`sed "s/\.dat//" tmp2.txt`
+
+  rm tmp.txt tmp2.txt
+
+  echo "JOB name = $JOB"
+
+  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> jobs/$JOB.log" jobs/$JOB.sh
+  $JOBSUBMIT jobs/$JOB.sh
+
+done
+
+
+VBFZ
+PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Dec5/VBFZ/
+for FILELIST in `ls filelists/Dec5_VBFZ*`
+  do
+  echo "Processing files in "$FILELIST
+
+  echo $FILELIST > tmp.txt
+  sed "s/filelists\/Dec5_VBFZ_//" tmp.txt > tmp2.txt
+
+  JOB=MC_`sed "s/\.dat//" tmp2.txt`
+
+  rm tmp.txt tmp2.txt
+
+  echo "JOB name = $JOB"
+
+  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> jobs/$JOB.log" jobs/$JOB.sh
+  $JOBSUBMIT jobs/$JOB.sh
+
+done
+
+#GJets
+PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Feb1/BKG/
+for FILELIST in `ls filelists/Feb1_GJets_*`
+  do
+  echo "Processing files in "$FILELIST
+
+  echo $FILELIST > tmp.txt
+  sed "s/filelists\/Feb1_GJets_//" tmp.txt > tmp2.txt
+
+  JOB=MC_`sed "s/\.dat//" tmp2.txt`
+
+  rm tmp.txt tmp2.txt
+
+  echo "JOB name = $JOB"
+
+  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> jobs/$JOB.log" jobs/$JOB.sh
+  $JOBSUBMIT jobs/$JOB.sh
+
+done
+
+#Process bkg common with HiggsTautau
+PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/agilbert/Dec2/MC_53X/
+for FILELIST in `ls filelists/Dec2_MC_*`
+#for FILELIST in `ls filelists/Dec30_MC_53X_DY*`
+  do
+  echo "Processing files in "$FILELIST
+
+  echo $FILELIST > tmp.txt
+  sed "s/filelists\/Dec2_MC_53X_//" tmp.txt > tmp2.txt
 
   JOB=MC_`sed "s/\.dat//" tmp2.txt`
 
