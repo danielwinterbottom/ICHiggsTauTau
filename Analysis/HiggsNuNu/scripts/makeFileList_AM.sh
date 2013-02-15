0050e13fb5d7c68dea8da9685797976050c38e52
amagnan <>
2013-02-15 14:07:47 +0000

    improve script when more than 1000 files in SE

--- Analysis/HiggsNuNu/scripts/makeFileList_AM.sh
+++ Analysis/HiggsNuNu/scripts/makeFileList_AM.sh
@@ -1,16 +1,36 @@
 #!/bin/sh
 
-if (( "$#" != "3" ))
-then
-echo $# $*
-echo "Input parameter needed: <basedir1> <basedir2> <basedir3> "
+if (( "$#" != "4" ))
+    then
+    echo $# $*
+    echo "Input parameter needed: <userdir> <basedir1> <basedir2> <basedir3> "
 else
-BASEDIR=$1
-VERSIONDIR=$2
-CRABDIR=$3
-srmls srm://gfe02.grid.hep.ph.ic.ac.uk:8443/srm/managerv2?SFN=/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/amagnan/$BASEDIR/$VERSIONDIR/$CRABDIR > tmp1.out
-awk '{print $2}' tmp1.out | grep "EventTree" > tmp2.out 
-OUTFILE=$BASEDIR"_"$VERSIONDIR"_"$CRABDIR".dat"
-sed "s/\/pnfs\/hep.ph.ic.ac.uk\/data\/cms\/store\/user\/amagnan\/$BASEDIR\/$VERSIONDIR\///" tmp2.out > $OUTFILE
-rm tmp1.out tmp2.out
+    USERDIR=$1
+    BASEDIR=$2
+    VERSIONDIR=$3
+    CRABDIR=$4
+    
+    OUTFILE=$BASEDIR"_"$VERSIONDIR"_"$CRABDIR".dat"
+    rm $OUTFILE
+
+    MYOFFSET=0
+    MYCOUNT=500
+
+    while [ "$MYCOUNT" == "500" ]
+      do
+      srmls -count=500 -offset=$MYOFFSET srm://gfe02.grid.hep.ph.ic.ac.uk:8443/srm/managerv2?SFN=/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/$USERDIR/$BASEDIR/$VERSIONDIR/$CRABDIR > tmp1.out
+      
+      let MYCOUNT=`grep EventTree tmp1.out | wc -l`
+      
+      echo " - Retrieved " `expr $MYCOUNT + $MYOFFSET` " files from SE."
+      
+      let MYOFFSET=$MYOFFSET+$MYCOUNT
+      
+      awk '{print $2}' tmp1.out | grep "EventTree" > tmp2.out 
+      sed "s/\/pnfs\/hep.ph.ic.ac.uk\/data\/cms\/store\/user\/amagnan\/$BASEDIR\/$VERSIONDIR\///" tmp2.out >> $OUTFILE
+      rm tmp1.out tmp2.out
+
+
+    done
+
 fi
