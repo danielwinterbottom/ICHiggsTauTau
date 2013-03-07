a49e856f7b0c40d09a3a262cd550ad6d10df3245
amagnan <>
2013-03-07 09:12:13 +0000

    debug skim config

--- Analysis/HiggsNuNu/scripts/submit_wjets.sh
+++ Analysis/HiggsNuNu/scripts/submit_wjets.sh
@@ -11,34 +11,41 @@ export JOBSUBMIT="./scripts/submit_ic_batch_job.sh hepmedium.q"
 echo "Using job-wrapper: " $JOBWRAPPER
 echo "Using job-submission: " $JOBSUBMIT
 
-CONFIG=scripts/DefaultConfigEnuMC.cfg
+CONFIG=scripts/DefaultConfigSkimMC.cfg
 echo "Config file: $CONFIG"
 
-JOBDIR=jobs/enu/
-mkdir -p $JOBDIR
+for CHANNEL in enu munu taunu
+  do
+
+  JOBDIR=jobs/skim/$CHANNEL/
+  OUTDIR=output/skim/$CHANNEL/
+  mkdir -p $JOBDIR
+  mkdir -p $OUTDIR
 
 
 #Process bkg common with HiggsTautau
-PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/agilbert/Dec2/MC_53X/
-for FILELIST in `ls filelists/Dec2_MC_53X_W*ToLNu*`
+  PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/agilbert/Dec2/MC_53X/
+  for FILELIST in `ls filelists/Dec2_MC_53X_W*ToLNu*`
 #for FILELIST in `ls filelists/Dec30_MC_53X_DY*`
-  do
-  echo "Processing files in "$FILELIST
-
-  echo $FILELIST > tmp.txt
-  sed "s/filelists\/Dec2_MC_53X_//" tmp.txt > tmp2.txt
-
-  JOB=MC_`sed "s/\.dat//" tmp2.txt`
-
-  rm tmp.txt tmp2.txt
-
-  echo "JOB name = $JOB"
-
-  $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root &> $JOBDIR/$JOB.log" $JOBDIR/$JOB.sh
-  $JOBSUBMIT $JOBDIR/$JOB.sh
+    do
+    echo "Processing files in "$FILELIST
+    
+    echo $FILELIST > tmp.txt
+    sed "s/filelists\/Dec2_MC_53X_//" tmp.txt > tmp2.txt
+    
+    LOOPOUTDIR=$OUTDIR/`sed "s/\.dat//" tmp2.txt`/
+    JOB=MC_`sed "s/\.dat//" tmp2.txt`
+    
+    rm tmp.txt tmp2.txt
+    
+    echo "JOB name = $JOB"
+    echo "OUTPUT dir = $LOOPOUTDIR"
+    $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root --skim_path=$LOOPOUTDIR --channel=$CHANNEL &> $JOBDIR/$JOB.log" $JOBDIR/$JOB.sh
+    $JOBSUBMIT $JOBDIR/$JOB.sh
+    
+  done
 
 done
-
 
 
 #if (( "$#" != "2" ))
