f79384fb294bd214dd7419cea3a1815c434f78b8
pdunne <>
2013-03-13 13:32:58 +0000

    updating submission script for new data files

--- Analysis/HiggsNuNu/scripts/submit_data.sh
+++ Analysis/HiggsNuNu/scripts/submit_data.sh
@@ -63,40 +63,19 @@ for METCUT in 130
 	      echo "Using job-submission: " $JOBSUBMIT
 	  fi
 	  
-	  PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/amagnan/Dec1/MET/
-	  for FILELIST in `ls filelists/$QUEUEDIR/Dec1_MET_*`
-#for FILELIST in `ls filelists/Dec1_MET_MET-2012C-11Dec2012-v1.dat`
+	  PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/pdunne/Mar20/MET/
+	  
+	  for FILELIST in `ls filelists/$QUEUEDIR/Mar20_MET_MET-2012*`
 	    do
 	    echo "Processing files in "$FILELIST
 	    
 	    echo $FILELIST > tmp.txt
-	    sed "s/filelists\/$QUEUEDIR\/Dec1_MET_//" tmp.txt > tmp2.txt
+	    sed "s/filelists\/$QUEUEDIR\/Mar20_MET_//" tmp.txt > tmp2.txt
 	    
 	    JOB=Data_`sed "s/\.dat//" tmp2.txt`
 	    
 	    rm tmp.txt tmp2.txt
 	    
-	    echo "JOB name = $JOB"
-	    
-	    $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root --output_folder=$OUTPUTDIR --met_cut=$METCUT --signal_region=$DOQCD $SYSTOPTIONS --channel=$CHANNEL &> $JOBDIR/$JOB.log" $JOBDIR/$JOB.sh
-	    $JOBSUBMIT $JOBDIR/$JOB.sh
-
-	  done
-	  
-	  
-	  PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/pdunne/Dec1/MET/
-	  
-	  for FILELIST in `ls filelists/$QUEUEDIR/MET-*`
-	    do
-	    echo "Processing files in "$FILELIST
-	    
-	    echo $FILELIST > tmp.txt
-	    sed "s/filelists\/$QUEUEDIR\///" tmp.txt > tmp2.txt
-	    
-	    JOB=Data_`sed "s/\.dat//" tmp2.txt`
-	    
-	    rm tmp.txt tmp2.txt
-    
 	    echo "JOB name = $JOB"
 	    
 	    $JOBWRAPPER "./bin/HiggsNuNu --cfg=$CONFIG --filelist="$FILELIST" --input_prefix=$PREFIX --output_name=$JOB.root --output_folder=$OUTPUTDIR --met_cut=$METCUT --signal_region=$DOQCD $SYSTOPTIONS --channel=$CHANNEL &> $JOBDIR/$JOB.log" $JOBDIR/$JOB.sh
@@ -108,13 +87,13 @@ for METCUT in 130
 	done
 	
       done
-    
+      
     done
     
   done
   
 done
-  
+
 #if (( "$#" != "2" ))
 #then
 #echo ""
