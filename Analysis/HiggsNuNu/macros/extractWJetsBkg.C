4e025521c6175d5051b7adc69fca355827e00555
pdunne <>
2013-03-19 19:32:07 +0000

    adding systematics to extractwjetsbackground macro

--- Analysis/HiggsNuNu/macros/extractWJetsBkg.C
+++ Analysis/HiggsNuNu/macros/extractWJetsBkg.C
@@ -112,11 +112,11 @@ int extractWJetsBkg(){//main
   lSelVecControl.push_back("DPhi");
   lSelVecControl.push_back("TightMjj");
 
-  std::string TOPDIR = "TABLES/";
+  std::string TOPDIR = "../TABLES/";
 
   const unsigned nSteps = lSelVecSignal.size();
 
-  unsigned MET[3] = {130,0,70};
+  unsigned MET[1] = {130,0,70};
 
   for (unsigned iMET(0); iMET<3; ++iMET){//loop on MET values
 
@@ -126,212 +126,245 @@ int extractWJetsBkg(){//main
 
       std::cout << " -- Processing signal region " << iQCD << std::endl;
 
-      std::ostringstream lFolder;
-      lFolder << "MET" << MET[iMET] << "/DOQCD" << iQCD << "/";
-
-      evtsArray lSel[3][nSteps];
-      
-      for (unsigned iS(0); iS<nSteps; ++iS){//loop on steps
-
-	std::ifstream lTable;
-	std::ostringstream lName;
-
-
-	lName << TOPDIR << "/nunu/" << lFolder.str() << "/SummaryTable_" << lSelVecSignal[iS] << ".dat";
-	lTable.open(lName.str().c_str());
-	if(!lTable.is_open()){
-	  cerr<<"Unable to open file: "<<lName.str()<<endl;
-	  return 1; 
-	}
-	lTable>>lSel[0][iS];
-	lTable.close();
+      for (unsigned iSyst(0); iSyst<3; ++iSyst){//loop over differenet systematics                                                                                      
+	std::cout<< "--Processing systematic " <<iSyst<<std::endl;
+	std::ostringstream lFolder;
+        if(iSyst==0){
+          lFolder << "MET" << MET[iMET] << "/DOQCD" << iQCD << "/";
+        }
+        if(iSyst==1){
+          lFolder << "MET" << MET[iMET] << "/DOQCD" << iQCD << "/JESUP/";
+        }
+        if(iSyst==2){
+          lFolder << "MET" << MET[iMET] << "/DOQCD" << iQCD << "/JESDOWN/";
+        }
+	evtsArray lSel[3][nSteps];
 	
-	lName.str("");
-	lName << TOPDIR << "/enu/" << lFolder.str() << "/SummaryTable_" << lSelVecControl[iS] << ".dat";
-	lTable.open(lName.str().c_str());
-	if(!lTable.is_open()){
-	  cerr<<"Unable to open file: "<<lName.str()<<endl;
-	  return 1; 
-	}
-	lTable>>lSel[1][iS];
-	lTable.close();
-	
-	lName.str("");
-	lName << TOPDIR << "/munu/" << lFolder.str() << "/SummaryTable_" << lSelVecControl[iS] << ".dat";
-	lTable.open(lName.str().c_str());
-	if(!lTable.is_open()){
-	  cerr<<"Unable to open file: "<<lName.str()<<endl;
-	  return 1; 
-	}
-	lTable>>lSel[2][iS];
-	lTable.close();
-	
-      }//loop on steps
-
-
-      if (iQCD==2) continue;
-
-      //print Wjets components
-      std::string lChannel[3] = {"nunu","enu","munu"};
-      for (unsigned iCh(0); iCh<3; ++iCh){//loop on channel
-	std::ostringstream lName;
-	lName << TOPDIR << "/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD << "/WandZJetsTable.txt";
-	std::ofstream lOutfile;
-	lOutfile.open(lName.str().c_str());
-
-	lOutfile << "\\begin{tabular}{|l|c|c|c||c|c|c|}" << std::endl
-		 << "\\hline" << std::endl
-		 << "Step & W$\\rightarrow e\\nu$ & W$\\rightarrow\\mu\\nu$ & W$\\rightarrow\\tau\\nu$ & Z$\\rightarrow\\nu\\nu$ & Z$\\rightarrow$ll & EWK Z+2j  \\\\" << std::endl
-		 << "\\hline" << std::endl
-		 << "LooseMjj+DPhi &  $" 
-		 << lSel[iCh][DPhi][WJets_enu].number  << "\\pm" << lSel[iCh][DPhi][WJets_enu].error << "$ & $" 
-		 << lSel[iCh][DPhi][WJets_munu].number  << "\\pm" << lSel[iCh][DPhi][WJets_munu].error  << "$ & $" 
-		 << lSel[iCh][DPhi][WJets_taunu].number  << "\\pm" << lSel[iCh][DPhi][WJets_taunu].error << "$ & $" 
-		 << lSel[iCh][DPhi][ZJets_nunu].number  << "\\pm" << lSel[iCh][DPhi][ZJets_nunu].error << "$ & $" 
-		 << lSel[iCh][DPhi][ZJets_ll].number  << "\\pm" << lSel[iCh][DPhi][ZJets_ll].error << "$ & $" 
-		 << lSel[iCh][DPhi][ZJets_vbf].number  << "\\pm" << lSel[iCh][DPhi][ZJets_vbf].error 
-		 << "$ \\\\" << std::endl
-		 << "TightMjj+DPhi & $" 
-		 << lSel[iCh][DPhi+1][WJets_enu].number  << "\\pm" << lSel[iCh][DPhi+1][WJets_enu].error << "$ & $" 
-		 << lSel[iCh][DPhi+1][WJets_munu].number  << "\\pm" << lSel[iCh][DPhi+1][WJets_munu].error  << "$ & $" 
-		 << lSel[iCh][DPhi+1][WJets_taunu].number  << "\\pm" << lSel[iCh][DPhi+1][WJets_taunu].error << "$ & $" 
-		 << lSel[iCh][DPhi+1][ZJets_nunu].number  << "\\pm" << lSel[iCh][DPhi+1][ZJets_nunu].error << "$ & $" 
-		 << lSel[iCh][DPhi+1][ZJets_ll].number  << "\\pm" << lSel[iCh][DPhi+1][ZJets_ll].error << "$ & $" 
-		 << lSel[iCh][DPhi+1][ZJets_vbf].number  << "\\pm" << lSel[iCh][DPhi+1][ZJets_vbf].error 
-		 << "$ \\\\" << std::endl
-		 << "\\hline" << std::endl
-		 << "\\end{tabular}" << std::endl;
-	
-	lOutfile.close();
-
-      }//loop on channel
-
-      for (unsigned int iMjj(0); iMjj<2; ++iMjj){//loop on Mjj WP
-	
-	for (unsigned iCh(1); iCh<3; ++iCh){//loop on lep flavour
+	for (unsigned iS(0); iS<nSteps; ++iS){//loop on steps
 	  
-	  events lNCdata;
-	  events lBkgCMC;
-	  events lBkgSMC;
-	  events lNCMC;
-	  events lNSMC;
-	  // std::cout << lSel[iCh][DPhi+iMjj][Data] 
-	  // 	    << " " << lSel[iCh][DPhi+iMjj][Top]
-	  // 	    << " " << lSel[iCh][DPhi+iMjj][VV]
-	  // 	    << " " << lSel[iCh][DPhi+iMjj][ZJets]
-	  // 	    << std::endl;
-
-	  lNCdata = lSel[iCh][DPhi+iMjj][Data];
-	  lNCdata -= lSel[iCh][DPhi+iMjj][Top];
-	  lNCdata -= lSel[iCh][DPhi+iMjj][VV];
-	  lNCdata -= lSel[iCh][DPhi+iMjj][ZJets];
-	  lBkgCMC = lSel[iCh][DPhi+iMjj][Top];
-	  lBkgCMC += lSel[iCh][DPhi+iMjj][VV];
-	  lBkgCMC += lSel[iCh][DPhi+iMjj][ZJets];
-	  lBkgCMC.sample = "$N_{EWK}$";
-	  lBkgSMC = lSel[0][DPhi+iMjj][Top];
-	  lBkgSMC += lSel[0][DPhi+iMjj][VV];
-	  lBkgSMC += lSel[0][DPhi+iMjj][ZJets];
-	  lBkgSMC.sample = "$N_{EWK}$";
-
-	  efficiency eps_lepveto_S;
-	  eps_lepveto_S.name = "$\\epsilon_{lepsel}$";
-	  efficiency eps_VBF_S;
-	  eps_VBF_S.name = "$\\epsilon_{VBF}$";
-	  efficiency eps_lepsel_C;
-	  eps_lepsel_C.name = "$\\epsilon_{lepsel}$";
-	  efficiency eps_VBF_C;
-	  eps_VBF_C.name = "$\\epsilon_{VBF}$";
-	  events lNSdata;
-
-	  if (iCh==2) {
-	    lNCdata.sample = "$N_{W\\rightarrow \\mu\\nu}^{data}$";
-	    lNSdata.sample = "$N_{W\\rightarrow \\mu\\nu}^{data}$";
-	    lNSMC = lSel[0][DPhi+iMjj][WJets_munu];
-	    lNCMC = lSel[iCh][DPhi+iMjj][WJets_munu];
-	    lNSMC.sample = "$N_{W\\rightarrow \\mu\\nu}^{MC}$";
-	    lNCMC.sample = "$N_{W\\rightarrow \\mu\\nu}^{MC}$";
-	    eps_lepveto_S.num = lSel[0][Lep][WJets_munu];
-	    eps_lepveto_S.den = lSel[0][JetPair][WJets_munu];
-	    
-	    eps_VBF_S.num = lSel[0][DPhi+iMjj][WJets_munu];
-	    eps_VBF_S.den = lSel[0][Lep][WJets_munu];
-	    
-	    eps_lepsel_C.num = lSel[iCh][Lep][WJets_munu];
-	    eps_lepsel_C.den = lSel[iCh][JetPair][WJets_munu];
-	    
-	    eps_VBF_C.num = lSel[iCh][DPhi+iMjj][WJets_munu];
-	    eps_VBF_C.den = lSel[iCh][Lep][WJets_munu];
-	  }
-	  else {
-	    lNCdata.sample = "$N_{W\\rightarrow e\\nu}^{data}$";
-	    lNSdata.sample = "$N_{W\\rightarrow e\\nu}^{data}$";
-	    lNSMC = lSel[0][DPhi+iMjj][WJets_enu];
-	    lNCMC = lSel[iCh][DPhi+iMjj][WJets_enu];
-	    lNSMC.sample = "$N_{W\\rightarrow e\\nu}^{MC}$";
-	    lNCMC.sample = "$N_{W\\rightarrow e\\nu}^{MC}$";
-	    eps_lepveto_S.num = lSel[0][Lep][WJets_enu];
-	    eps_lepveto_S.den = lSel[0][JetPair][WJets_enu];
-	    
-	    eps_VBF_S.num = lSel[0][DPhi+iMjj][WJets_enu];
-	    eps_VBF_S.den = lSel[0][Lep][WJets_enu];
-	    
-	    eps_lepsel_C.num = lSel[iCh][Lep][WJets_enu];
-	    eps_lepsel_C.den = lSel[iCh][JetPair][WJets_enu];
-	    
-	    eps_VBF_C.num = lSel[iCh][DPhi+iMjj][WJets_enu];
-	    eps_VBF_C.den = lSel[iCh][Lep][WJets_enu];
-	  }
-	  lNSdata.number = lNCdata.number * lNSMC.number / lNCMC.number;
-	  lNSdata.error = lNSdata.number * sqrt(pow(lNCdata.error/lNCdata.number,2)+pow(lNCMC.error/lNCMC.number,2)+pow(lNSMC.error/lNSMC.number,2));
-	  
-
-	  events result;
-	  result.number = lNCdata.number *(eps_lepveto_S.eff()/eps_lepsel_C.eff())*(eps_VBF_S.eff()/eps_VBF_C.eff());
-	  result.error = result.number * sqrt(pow(eps_lepveto_S.error()/eps_lepveto_S.eff(),2)+pow(eps_lepsel_C.error()/eps_lepsel_C.eff(),2)+pow(eps_VBF_S.error()/eps_VBF_S.eff(),2)+pow(eps_VBF_C.error()/eps_VBF_C.eff(),2)+pow(lNCdata.error/lNCdata.number,2));
-
+	  std::ifstream lTable;
 	  std::ostringstream lName;
-	  lName << TOPDIR << "/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD ;
-	  if (iMjj==0) lName << "/DataDrivenWJetsTable_looseMjj.txt";
-	  else if (iMjj==1) lName << "/DataDrivenWJetsTable_tightMjj.txt";
+	  
+	  
+	  lName << TOPDIR << "/nunu/" << lFolder.str() << "/SummaryTable_" << lSelVecSignal[iS] << ".dat";
+	  lTable.open(lName.str().c_str());
+	  if(!lTable.is_open()){
+	    cerr<<"Unable to open file: "<<lName.str()<<endl;
+	    return 1; 
+	  }
+	  lTable>>lSel[0][iS];
+	  lTable.close();
+	  
+	  lName.str("");
+	  lName << TOPDIR << "/enu/" << lFolder.str() << "/SummaryTable_" << lSelVecControl[iS] << ".dat";
+	  lTable.open(lName.str().c_str());
+	  if(!lTable.is_open()){
+	    cerr<<"Unable to open file: "<<lName.str()<<endl;
+	    return 1; 
+	  }
+	  lTable>>lSel[1][iS];
+	  lTable.close();
+	  
+	  lName.str("");
+	  lName << TOPDIR << "/munu/" << lFolder.str() << "/SummaryTable_" << lSelVecControl[iS] << ".dat";
+	  lTable.open(lName.str().c_str());
+	  if(!lTable.is_open()){
+	    cerr<<"Unable to open file: "<<lName.str()<<endl;
+	    return 1; 
+	  }
+	  lTable>>lSel[2][iS];
+	  lTable.close();
+	  
+	}//loop on steps
+	
+	
+	if (iQCD==2) continue;
+	
+	//print Wjets components
+	std::string lChannel[3] = {"nunu","enu","munu"};
+	for (unsigned iCh(0); iCh<3; ++iCh){//loop on channel
+	  std::ostringstream lName;
+	  lName << TOPDIR << "/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD << "/WandZJetsTable.txt";
+
+	  if(iSyst==0){
+	    lName << TOPDIR <<"/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD << "/WandZJetsTable.txt";
+	  }
+	  if(iSyst==1){
+	    lName << TOPDIR <<"/"  << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD << "/JESUP/WandZJetsTable.txt";
+	  }
+	  if(iSyst==2){
+	    lName << TOPDIR <<"/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD << "/JESDOWN/WandZJetsTable.txt";
+	  }
+
 	  std::ofstream lOutfile;
 	  lOutfile.open(lName.str().c_str());
-	  lOutfile << "\\begin{tabular}{|l|c|c|}" << std::endl
-		   << "\\hline" << std::endl
-		   << " & Signal Region & Control Region \\\\" << std::endl
-		   << "\\hline" << std::endl
-		   << "$N_{data}$ & XXX & " << lSel[iCh][DPhi+iMjj][Data].number << "\\\\" << std::endl;
-	  lOutfile << std::setprecision(2)
-		   << lBkgSMC.sample << " & " 
-		   << " n/a & " 
-		   << lBkgCMC << " \\\\" << std::endl
-		   << eps_lepveto_S.name  << " & " 
-		   << eps_lepveto_S  << " & " 
-		   << eps_lepsel_C << " \\\\" << std::endl
-		   << eps_VBF_S.name  << " & " 
-		   << eps_VBF_S  << " & " 
-		   << eps_VBF_C << " \\\\" << std::endl
-		    << std::setprecision(3)
-		   << lNSMC.sample  << " & " 
-		   << lNSMC << " & " 
-		   << lNCMC << " \\\\" << std::endl
-		   << lNSdata.sample << " & " 
-		   << "\\textcolor{red}{" << lNSdata << "} & " 
-		   << lNCdata << " \\\\ " << std::endl 
-		   << "\\hline" << std::endl
-	    //<< "with Eff & " << result << " & \\\\" << std::endl
-	    //	   << "\\hline" << std::endl
-		   << "\\end{tabular}" << std::endl; 
-	  lOutfile.close();
-
 	  
-	}//loop on lep flavour
+	  lOutfile << "\\begin{tabular}{|l|c|c|c||c|c|c|}" << std::endl
+		   << "\\hline" << std::endl
+		   << "Step & W$\\rightarrow e\\nu$ & W$\\rightarrow\\mu\\nu$ & W$\\rightarrow\\tau\\nu$ & Z$\\rightarrow\\nu\\nu$ & Z$\\rightarrow$ll & EWK Z+2j  \\\\" << std::endl
+		   << "\\hline" << std::endl
+		   << "LooseMjj+DPhi &  $" 
+		   << lSel[iCh][DPhi][WJets_enu].number  << "\\pm" << lSel[iCh][DPhi][WJets_enu].error << "$ & $" 
+		   << lSel[iCh][DPhi][WJets_munu].number  << "\\pm" << lSel[iCh][DPhi][WJets_munu].error  << "$ & $" 
+		   << lSel[iCh][DPhi][WJets_taunu].number  << "\\pm" << lSel[iCh][DPhi][WJets_taunu].error << "$ & $" 
+		   << lSel[iCh][DPhi][ZJets_nunu].number  << "\\pm" << lSel[iCh][DPhi][ZJets_nunu].error << "$ & $" 
+		   << lSel[iCh][DPhi][ZJets_ll].number  << "\\pm" << lSel[iCh][DPhi][ZJets_ll].error << "$ & $" 
+		   << lSel[iCh][DPhi][ZJets_vbf].number  << "\\pm" << lSel[iCh][DPhi][ZJets_vbf].error 
+		   << "$ \\\\" << std::endl
+		   << "TightMjj+DPhi & $" 
+		   << lSel[iCh][DPhi+1][WJets_enu].number  << "\\pm" << lSel[iCh][DPhi+1][WJets_enu].error << "$ & $" 
+		   << lSel[iCh][DPhi+1][WJets_munu].number  << "\\pm" << lSel[iCh][DPhi+1][WJets_munu].error  << "$ & $" 
+		   << lSel[iCh][DPhi+1][WJets_taunu].number  << "\\pm" << lSel[iCh][DPhi+1][WJets_taunu].error << "$ & $" 
+		   << lSel[iCh][DPhi+1][ZJets_nunu].number  << "\\pm" << lSel[iCh][DPhi+1][ZJets_nunu].error << "$ & $" 
+		   << lSel[iCh][DPhi+1][ZJets_ll].number  << "\\pm" << lSel[iCh][DPhi+1][ZJets_ll].error << "$ & $" 
+		   << lSel[iCh][DPhi+1][ZJets_vbf].number  << "\\pm" << lSel[iCh][DPhi+1][ZJets_vbf].error 
+		   << "$ \\\\" << std::endl
+		   << "\\hline" << std::endl
+		   << "\\end{tabular}" << std::endl;
+	  
+	  lOutfile.close();
+	  
+	}//loop on channel
 	
-      }//loop on Mjj WP
+	for (unsigned int iMjj(0); iMjj<2; ++iMjj){//loop on Mjj WP
+	  
+	  for (unsigned iCh(1); iCh<3; ++iCh){//loop on lep flavour
+	    
+	    events lNCdata;
+	    events lBkgCMC;
+	    events lBkgSMC;
+	    events lNCMC;
+	    events lNSMC;
+	    // std::cout << lSel[iCh][DPhi+iMjj][Data] 
+	    // 	    << " " << lSel[iCh][DPhi+iMjj][Top]
+	    // 	    << " " << lSel[iCh][DPhi+iMjj][VV]
+	    // 	    << " " << lSel[iCh][DPhi+iMjj][ZJets]
+	    // 	    << std::endl;
+	    
+	    lNCdata = lSel[iCh][DPhi+iMjj][Data];
+	    lNCdata -= lSel[iCh][DPhi+iMjj][Top];
+	    lNCdata -= lSel[iCh][DPhi+iMjj][VV];
+	    lNCdata -= lSel[iCh][DPhi+iMjj][ZJets];
+	    lBkgCMC = lSel[iCh][DPhi+iMjj][Top];
+	    lBkgCMC += lSel[iCh][DPhi+iMjj][VV];
+	    lBkgCMC += lSel[iCh][DPhi+iMjj][ZJets];
+	    lBkgCMC.sample = "$N_{EWK}$";
+	    lBkgSMC = lSel[0][DPhi+iMjj][Top];
+	    lBkgSMC += lSel[0][DPhi+iMjj][VV];
+	    lBkgSMC += lSel[0][DPhi+iMjj][ZJets];
+	    lBkgSMC.sample = "$N_{EWK}$";
+	    
+	    efficiency eps_lepveto_S;
+	    eps_lepveto_S.name = "$\\epsilon_{lepsel}$";
+	    efficiency eps_VBF_S;
+	    eps_VBF_S.name = "$\\epsilon_{VBF}$";
+	    efficiency eps_lepsel_C;
+	    eps_lepsel_C.name = "$\\epsilon_{lepsel}$";
+	    efficiency eps_VBF_C;
+	    eps_VBF_C.name = "$\\epsilon_{VBF}$";
+	    events lNSdata;
+	    
+	    if (iCh==2) {
+	      lNCdata.sample = "$N_{W\\rightarrow \\mu\\nu}^{data}$";
+	      lNSdata.sample = "$N_{W\\rightarrow \\mu\\nu}^{data}$";
+	      lNSMC = lSel[0][DPhi+iMjj][WJets_munu];
+	      lNCMC = lSel[iCh][DPhi+iMjj][WJets_munu];
+	      lNSMC.sample = "$N_{W\\rightarrow \\mu\\nu}^{MC}$";
+	      lNCMC.sample = "$N_{W\\rightarrow \\mu\\nu}^{MC}$";
+	      eps_lepveto_S.num = lSel[0][Lep][WJets_munu];
+	      eps_lepveto_S.den = lSel[0][JetPair][WJets_munu];
+	      
+	      eps_VBF_S.num = lSel[0][DPhi+iMjj][WJets_munu];
+	      eps_VBF_S.den = lSel[0][Lep][WJets_munu];
+	      
+	      eps_lepsel_C.num = lSel[iCh][Lep][WJets_munu];
+	      eps_lepsel_C.den = lSel[iCh][JetPair][WJets_munu];
+	      
+	      eps_VBF_C.num = lSel[iCh][DPhi+iMjj][WJets_munu];
+	      eps_VBF_C.den = lSel[iCh][Lep][WJets_munu];
+	    }
+	    else {
+	      lNCdata.sample = "$N_{W\\rightarrow e\\nu}^{data}$";
+	      lNSdata.sample = "$N_{W\\rightarrow e\\nu}^{data}$";
+	      lNSMC = lSel[0][DPhi+iMjj][WJets_enu];
+	      lNCMC = lSel[iCh][DPhi+iMjj][WJets_enu];
+	      lNSMC.sample = "$N_{W\\rightarrow e\\nu}^{MC}$";
+	      lNCMC.sample = "$N_{W\\rightarrow e\\nu}^{MC}$";
+	      eps_lepveto_S.num = lSel[0][Lep][WJets_enu];
+	      eps_lepveto_S.den = lSel[0][JetPair][WJets_enu];
+	      
+	      eps_VBF_S.num = lSel[0][DPhi+iMjj][WJets_enu];
+	      eps_VBF_S.den = lSel[0][Lep][WJets_enu];
+	      
+	      eps_lepsel_C.num = lSel[iCh][Lep][WJets_enu];
+	      eps_lepsel_C.den = lSel[iCh][JetPair][WJets_enu];
+	    
+	      eps_VBF_C.num = lSel[iCh][DPhi+iMjj][WJets_enu];
+	      eps_VBF_C.den = lSel[iCh][Lep][WJets_enu];
+	    }
+	    lNSdata.number = lNCdata.number * lNSMC.number / lNCMC.number;
+	    lNSdata.error = lNSdata.number * sqrt(pow(lNCdata.error/lNCdata.number,2)+pow(lNCMC.error/lNCMC.number,2)+pow(lNSMC.error/lNSMC.number,2));
+	    
+	    
+	    events result;
+	    result.number = lNCdata.number *(eps_lepveto_S.eff()/eps_lepsel_C.eff())*(eps_VBF_S.eff()/eps_VBF_C.eff());
+	    result.error = result.number * sqrt(pow(eps_lepveto_S.error()/eps_lepveto_S.eff(),2)+pow(eps_lepsel_C.error()/eps_lepsel_C.eff(),2)+pow(eps_VBF_S.error()/eps_VBF_S.eff(),2)+pow(eps_VBF_C.error()/eps_VBF_C.eff(),2)+pow(lNCdata.error/lNCdata.number,2));
+	    
+	    std::ostringstream lName;
 
+	    if(iSyst==0){
+	      lName << TOPDIR << "/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD;
+            }
+            if(iSyst==1){
+	      lName << TOPDIR << "/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD <<"/JESUP";
+            }
+            if(iSyst==2){
+	      lName << TOPDIR << "/" << lChannel[iCh] << "/MET" << MET[iMET] << "/DOQCD" << iQCD <<"/JESDOWN";
+            }
+
+	    if (iMjj==0) lName << "/DataDrivenWJetsTable_looseMjj.txt";
+	    else if (iMjj==1) lName << "/DataDrivenWJetsTable_tightMjj.txt";
+	    std::ofstream lOutfile;
+	    lOutfile.open(lName.str().c_str());
+	    lOutfile << "\\begin{tabular}{|l|c|c|}" << std::endl
+		     << "\\hline" << std::endl
+		     << " & Signal Region & Control Region \\\\" << std::endl
+		     << "\\hline" << std::endl
+		     << "$N_{data}$ & XXX & " << lSel[iCh][DPhi+iMjj][Data].number << "\\\\" << std::endl;
+	    lOutfile << std::setprecision(2)
+		     << lBkgSMC.sample << " & " 
+		     << " n/a & " 
+		     << lBkgCMC << " \\\\" << std::endl
+		     << eps_lepveto_S.name  << " & " 
+		     << eps_lepveto_S  << " & " 
+		     << eps_lepsel_C << " \\\\" << std::endl
+		     << eps_VBF_S.name  << " & " 
+		     << eps_VBF_S  << " & " 
+		     << eps_VBF_C << " \\\\" << std::endl
+		     << std::setprecision(3)
+		     << lNSMC.sample  << " & " 
+		     << lNSMC << " & " 
+		     << lNCMC << " \\\\" << std::endl
+		     << lNSdata.sample << " & " 
+		     << "\\textcolor{red}{" << lNSdata << "} & " 
+		     << lNCdata << " \\\\ " << std::endl 
+		   << "\\hline" << std::endl
+	      //<< "with Eff & " << result << " & \\\\" << std::endl
+	      //	   << "\\hline" << std::endl
+		     << "\\end{tabular}" << std::endl; 
+	    lOutfile.close();
+
+	    
+	  }//loop on lep flavour
+	
+	}//loop on Mjj WP
+	
+      }//loop on systematics
+      
     }//loop on signal region
-
+    
   }//loop on MET values
-
+  
   return 0;
 }//main
+  
