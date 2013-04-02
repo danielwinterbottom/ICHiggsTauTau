ebe8ca1cccb01cbb20c390e99597fc4b8cf233f2
pdunne <>
2013-04-02 14:05:02 +0000

    *** empty log message ***

--- Analysis/HiggsNuNu/interface/HinvJESUncertainty.h
+++ Analysis/HiggsNuNu/interface/HinvJESUncertainty.h
@@ -12,7 +12,8 @@
 #include <string>
 #include "boost/bind.hpp"
 #include "PhysicsTools/FWLite/interface/TFileService.h"
-
+#include "Math/GenVector/VectorUtil.h"
+#include "Math/Vector4Dfwd.h"
 
 namespace ic {
   
@@ -75,48 +76,91 @@ namespace ic {
       int HinvJESUncertainty<T,M>::Execute(TreeEvent *event) {
       std::vector<T *> & vec = event->GetPtrVec<T>(input_label_);//get the collection (should be a jet collection)
       M *met = event->GetPtr<M>(met_label_);//get the met
-      double metpt = met->pt();
+      /*double metpt = met->pt();
       double metenergy = met->energy();
       double metphi = met->phi();
       double metpx = metpt*cos(metphi);
-      double metpy = metpt*sin(metphi);
-      //std::vector<M *> & vecmet = event->GetPtrVec<M>(met_label_);
+      double metpy = metpt*sin(metphi);*/
+      
+      //Get MET information
+      double metpx = met->vector().px();
+      double metpy = met->vector().py();
+      double metet = met->vector().energy();
+
       for (unsigned i = 0; i < vec.size(); ++i) {//loop over the collection and correct by JES
-	double jetpt = vec[i]->pt();
+	/*double jetpt = vec[i]->pt();
 	double jeteta = vec[i]->eta();
 	double jetphi = vec[i]->phi();
-	double jetenergy = vec[i]->energy();
+	double jetenergy = vec[i]->energy();*/
+	
+	//Get jet information
+	double jetpx = vec[i]->vector().px();
+	double jetpy = vec[i]->vector().py();
+	double jetpz = vec[i]->vector().pz();
+	double jete = vec[i]->vector().E();
+	double jetpt = sqrt(jetpx*jetpx+jetpy*jetpy);
+	double jeteta = vec[i]->eta();
+
+	//Get JES uncertainty
 	total->setJetPt(jetpt);
 	total->setJetEta(jeteta);
 	double uncert = total->getUncertainty(upordown_);
-	JEScorrfac->Fill(jetpt,uncert);
-      //std::cout<<uncert<<std::endl;
+	JEScorrfac->Fill(jetpt,uncert); //Fill histogram of uncertainty against pt
+
 	if(upordown_==true){//upper uncertainty
-	  vec[i]->set_pt(jetpt*(1+uncert));
+	  //Correct jet
+	  double newjetpx = jetpx*(1+uncert);
+	  double newjetpy = jetpy*(1+uncert);
+	  double newjetpz = jetpz*(1+uncert);
+	  double newjete = jete+uncert*sqrt(jetpx*jetpx+jetpy*jetpy+jetpz*jetpz);
+	  ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >  newjet(newjetpx,newjetpy,newjetpz,newjete);
+	  vec[i]->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(newjet));
+
+	  //Correct met
+	  metpx = metpx - jetpx*uncert;
+	  metpy = metpy - jetpy*uncert;
+
+	  /*vec[i]->set_pt(jetpt*(1+uncert));
 	  vec[i]->set_energy(jetenergy*(1+uncert));
 	  metpx = metpx-((jetpt*uncert)*cos(jetphi));
 	  metpy = metpy-((jetpt*uncert)*sin(jetphi));
-	  metenergy = metenergy-jetenergy*uncert;
+	  metenergy = metenergy-jetenergy*uncert;*/
 	}
 	else if(upordown_==false){//lower uncertainty
-        vec[i]->set_pt(jetpt*(1-uncert));
-        vec[i]->set_energy(jetenergy*(1-uncert));
-	metpx = metpx+((jetpt*uncert)*cos(jetphi));
-	metpy = metpy+((jetpt*uncert)*sin(jetphi));
-	metenergy = metenergy+jetenergy*uncert;
+	  //Correct jet
+	  double newjetpx = jetpx*(1-uncert);
+	  double newjetpy = jetpy*(1-uncert);
+	  double newjetpz = jetpz*(1-uncert);
+	  double newjete = jete-uncert*sqrt(jetpx*jetpx+jetpy*jetpy+jetpz*jetpz);
+	  ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > newjet(newjetpx,newjetpy,newjetpz,newjete);
+	  vec[i]->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(newjet));
+
+	  //Correct met
+	  metpx = metpx + jetpx*uncert;
+	  metpy = metpy + jetpy*uncert;
+	  
+	  /*vec[i]->set_pt(jetpt*(1-uncert));
+	  vec[i]->set_energy(jetenergy*(1-uncert));
+	  metpx = metpx+((jetpt*uncert)*cos(jetphi));
+	  metpy = metpy+((jetpt*uncert)*sin(jetphi));
+	  metenergy = metenergy+jetenergy*uncert;*/
 	}
       }
-      metpt=sqrt(metpx*metpx+metpy*metpy);
-    if(metpx>0){
-      metphi=atan(metpy/metpx);
-    }
-    if(metpx<0){
-      metphi=(3.14159265358979-atan(metpy/(-1*metpx)));
-    }
-    met->set_pt(metpt);
-    met->set_phi(metphi);
-    //met->set_energy(metenergy); //commented out to see how much of an effect it has
-    return 0;
+      metet=sqrt(metpx*metpx+metpy*metpy);
+    
+      ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > new_met(metpx,metpy,0,metet);
+      met->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(new_met));
+      return 0;
+      /*if(metpx>0){
+	metphi=atan(metpy/metpx);
+      }
+      if(metpx<0){
+	metphi=(3.14159265358979-atan(metpy/(-1*metpx)));
+      }
+      met->set_pt(metpt);
+      met->set_phi(metphi);
+      //met->set_energy(metenergy); //commented out to see how much of an effect it has
+      */
     }
     
     template <class T,class M>
