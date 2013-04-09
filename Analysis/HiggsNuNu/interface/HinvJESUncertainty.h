61e8e1f74ef4ddc649b520e92f4d41d5f09a762e
pdunne <>
2013-04-09 09:56:39 +0000

    fixing bug with JESUP

--- Analysis/HiggsNuNu/interface/HinvJESUncertainty.h
+++ Analysis/HiggsNuNu/interface/HinvJESUncertainty.h
@@ -21,13 +21,19 @@ namespace ic {
     class HinvJESUncertainty : public ModuleBase {
   private:
     JetCorrectionUncertainty *total;
+    JetCorrectionUncertainty *total2;
     CLASS_MEMBER(HinvJESUncertainty,fwlite::TFileService*, fs);
     CLASS_MEMBER(HinvJESUncertainty, bool, is_data)
+    CLASS_MEMBER(HinvJESUncertainty, bool, dojessyst)
     CLASS_MEMBER(HinvJESUncertainty, bool, upordown)
     CLASS_MEMBER(HinvJESUncertainty, std::string, input_label)
     CLASS_MEMBER(HinvJESUncertainty, std::string, met_label)
-    TH2F* JEScorrfac;
-      
+    TH2F* JESupcorrfac;
+    TH2F* JESdowncorrfac;
+    TH2F* JESusedcorrfac;
+    TH1F* JESmetdiff;
+    TH1F* JESwhichran;
+    
 
   public:
     HinvJESUncertainty(std::string const& name);
@@ -42,7 +48,8 @@ namespace ic {
     HinvJESUncertainty<T,M>::HinvJESUncertainty(std::string const& name) : ModuleBase(name) {
     is_data_ = true;
     input_label_ = "pfJetsPFlow";
-    upordown_ = true; //true is up false is down                                                                                                                        
+    upordown_ = true; //true is up false is down
+    dojessyst_ = false;
   }
     
     template <class T,class M>
@@ -55,19 +62,25 @@ namespace ic {
       std::cout << "----------------------------------------" << std::endl;
       std::cout << "PreAnalysis Info for HinvJESUncertainty" << std::endl;
       std::cout << "----------------------------------------" << std::endl;
-      std::cout << "Doing up error? "<<upordown_<<std::endl;
+      std::cout << "Doing JES? "<<dojessyst_<<std::endl;
+      std::cout << "If doing JES are we doing up error? "<<upordown_<<std::endl;
+      
       
       std::cout<<"Getting JES uncertainty parameters from file."<<std::endl;
       //getting the correct uncertainty, note currently module is only being run on mc
       if(is_data_){
-      total = new JetCorrectionUncertainty(*(new JetCorrectorParameters("data/jec/Fall12_V7_DATA_Uncertainty_AK5PF.txt")));
+	total = new JetCorrectionUncertainty(*(new JetCorrectorParameters("data/jec/Fall12_V7_DATA_Uncertainty_AK5PF.txt")));
       }
       else{
 	total = new JetCorrectionUncertainty(*(new JetCorrectorParameters("data/jec/Fall12_V7_MC_Uncertainty_AK5PF.txt")));
       }
       std::cout<<"Got parameters successfully"<<std::endl;
       TFileDirectory const& dir = fs_->mkdir("JES");
-            JEScorrfac = dir.make<TH2F>("JEScorrfac","JEScorrfac",1000,0.,1000.,100,-5.,5.);
+      JESupcorrfac = dir.make<TH2F>("JESupcorrfac","JESupcorrfac",1000,0.,1000.,1000,-0.3,0.3);
+      JESdowncorrfac = dir.make<TH2F>("JESdowncorrfac","JESdowncorrfac",1000,0.,1000.,1000,-0.3,0.3);
+      JESusedcorrfac = dir.make<TH2F>("JESusedcorrfac","JESusedcorrfac",1000,0.,1000.,1000,-0.3,0.3);
+      JESmetdiff = dir.make<TH1F>("JESmetdiff","JESmetdiff",1000,-10.,10.);
+      JESwhichran = dir.make<TH1F>("JESwhichran","JESwhichran",1000,-10.,10.);
       std::cout<<"Made plot dir"<<std::endl;
       return 0;
     }
@@ -79,91 +92,78 @@ namespace ic {
       }
       std::vector<T *> & vec = event->GetPtrVec<T>(input_label_);//get the collection (should be a jet collection)
       M *met = event->GetPtr<M>(met_label_);//get the met
-      /*double metpt = met->pt();
-      double metenergy = met->energy();
-      double metphi = met->phi();
-      double metpx = metpt*cos(metphi);
-      double metpy = metpt*sin(metphi);*/
       
-      //Get MET information
+      //Get MET information and create variables to be updated
       double metpx = met->vector().px();
       double metpy = met->vector().py();
       double metet = met->vector().energy();
-
-      for (unsigned i = 0; i < vec.size(); ++i) {//loop over the collection and correct by JES
-	/*double jetpt = vec[i]->pt();
-	double jeteta = vec[i]->eta();
-	double jetphi = vec[i]->phi();
-	double jetenergy = vec[i]->energy();*/
-	
+      double newmetpx = metpx;
+      double newmetpy = metpy;
+      double newmetet = metet;
+      
+      for (unsigned i = 0; i < vec.size(); ++i) {//loop over the collection
 	//Get jet information
 	double jetpx = vec[i]->vector().px();
 	double jetpy = vec[i]->vector().py();
 	double jetpz = vec[i]->vector().pz();
 	double jete = vec[i]->vector().E();
-	double jetpt = sqrt(jetpx*jetpx+jetpy*jetpy);
+	double jetpt = vec[i]->pt();
 	double jeteta = vec[i]->eta();
-
+	
 	//Get JES uncertainty
 	total->setJetPt(jetpt);
 	total->setJetEta(jeteta);
-	double uncert = total->getUncertainty(upordown_);
-	JEScorrfac->Fill(jetpt,uncert); //Fill histogram of uncertainty against pt
-
-	if(upordown_==true){//upper uncertainty
-	  //Correct jet
-	  double newjetpx = jetpx*(1+uncert);
-	  double newjetpy = jetpy*(1+uncert);
-	  double newjetpz = jetpz*(1+uncert);
-	  double newjete = jete*(1+uncert);
-	  ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >  newjet(newjetpx,newjetpy,newjetpz,newjete);
-	  vec[i]->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(newjet));
-
-	  //Correct met
-	  metpx = metpx - jetpx*uncert;
-	  metpy = metpy - jetpy*uncert;
-
-	  /*vec[i]->set_pt(jetpt*(1+uncert));
-	  vec[i]->set_energy(jetenergy*(1+uncert));
-	  metpx = metpx-((jetpt*uncert)*cos(jetphi));
-	  metpy = metpy-((jetpt*uncert)*sin(jetphi));
-	  metenergy = metenergy-jetenergy*uncert;*/
+	double upuncert = total->getUncertainty(true);
+	total->setJetPt(jetpt);
+	total->setJetEta(jeteta);
+	double downuncert = total->getUncertainty(false);
+	
+	//std::cout<<uncert<<std::endl;
+	JESupcorrfac->Fill(jetpt,upuncert); //Fill histogram of uncertainty against pt
+	JESdowncorrfac->Fill(jetpt,downuncert); //Fill histogram of uncertainty against pt
+	
+	if(dojessyst_){//if not central value correct by the JES uncertainty
+	  if(upordown_==true){//upper uncertainty
+	    JESusedcorrfac->Fill(jetpt,upuncert); //Fill histogram of uncertainty against pt
+	    JESwhichran->Fill(1.);
+	    //Correct jet
+	    double newjetpx = jetpx*(1+upuncert);
+	    double newjetpy = jetpy*(1+upuncert);
+	    double newjetpz = jetpz*(1+upuncert);
+	    double newjete = jete*(1+upuncert);
+	    ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >  newjet(newjetpx,newjetpy,newjetpz,newjete);
+	    vec[i]->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(newjet));
+	    
+	    //Correct met
+	    newmetpx = metpx - (jetpx*upuncert);
+	    newmetpy = metpy - (jetpy*upuncert);
+	    
+	  }
+	  else if(upordown_==false){//lower uncertainty
+	    JESusedcorrfac->Fill(jetpt,downuncert); //Fill histogram of uncertainty against pt
+	    JESwhichran->Fill(-1.);
+	    //Correct jet
+	    double newjetpx = jetpx*(1-downuncert);
+	    double newjetpy = jetpy*(1-downuncert);
+	    double newjetpz = jetpz*(1-downuncert);
+	    double newjete = jete*(1-downuncert);
+	    ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > newjet(newjetpx,newjetpy,newjetpz,newjete);
+	    vec[i]->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(newjet));
+	    
+	    //Correct met
+	    newmetpx = newmetpx + (jetpx*downuncert);
+	    newmetpy = newmetpy + (jetpy*downuncert);	    
+	  }
 	}
-	else if(upordown_==false){//lower uncertainty
-	  //Correct jet
-	  double newjetpx = jetpx*(1-uncert);
-	  double newjetpy = jetpy*(1-uncert);
-	  double newjetpz = jetpz*(1-uncert);
-	  double newjete = jete*(1-uncert);
-	  ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > newjet(newjetpx,newjetpy,newjetpz,newjete);
-	  vec[i]->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(newjet));
-
-	  //Correct met
-	  metpx = metpx + jetpx*uncert;
-	  metpy = metpy + jetpy*uncert;
-	  
-	  /*vec[i]->set_pt(jetpt*(1-uncert));
-	  vec[i]->set_energy(jetenergy*(1-uncert));
-	  metpx = metpx+((jetpt*uncert)*cos(jetphi));
-	  metpy = metpy+((jetpt*uncert)*sin(jetphi));
-	  metenergy = metenergy+jetenergy*uncert;*/
-	}
+	else JESwhichran->Fill(0.);
       }
-      metet=sqrt(metpx*metpx+metpy*metpy);
-    
-      ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > new_met(metpx,metpy,0,metet);
+      newmetet=sqrt(newmetpx*newmetpx+newmetpy*newmetpy);
+      JESmetdiff->Fill(newmetet-metet);
+      
+      ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > new_met(newmetpx,newmetpy,0,newmetet);
       met->set_vector(ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> >(new_met));
+      
       return 0;
-      /*if(metpx>0){
-	metphi=atan(metpy/metpx);
-      }
-      if(metpx<0){
-	metphi=(3.14159265358979-atan(metpy/(-1*metpx)));
-      }
-      met->set_pt(metpt);
-      met->set_phi(metphi);
-      //met->set_energy(metenergy); //commented out to see how much of an effect it has
-      */
     }
     
     template <class T,class M>
