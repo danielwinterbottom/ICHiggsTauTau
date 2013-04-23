3c04ccff520baf6c39f39f56b5201dc27a983ab7
agilbert <>
2013-04-23 12:16:51 +0000

    implement a Print method for EventInfo

--- src/EventInfo.cc
+++ src/EventInfo.cc
@@ -14,6 +14,19 @@ namespace ic {
   }
 
   void EventInfo::Print() const {
+    std::cout << "Run: " << run_ << "  Lumi: " << lumi_block_ << "  Event: " << event_ << std::endl;
+    std::cout << "Jet Rho: " << jet_rho_ << std::endl;
+    std::cout << "Lepton Rho: " << lepton_rho_ << std::endl;
+    std::cout << "Good vertices: " << good_vertices_ << std::endl;
+    for (SDMap::const_iterator it = weights_.begin(); it != weights_.end(); ++it) {
+      std::cout << it->first << "\t\t" << it->second << "\t\t";
+      SBMap::const_iterator st_it = weight_status_.find(it->first);
+      if (st_it != weight_status_.end()) {
+        std::cout << st_it->second << std::endl;
+      } else {
+        std::cout << true << std::endl;
+      }
+    }
   }
 }
     
