6199b028bd5b94ece7d360d84f228c4281be11d7
amagnan <>
2013-04-23 14:06:42 +0000

    *** empty log message ***

--- Analysis/HiggsNuNu/test/ControlPlots.cpp
+++ Analysis/HiggsNuNu/test/ControlPlots.cpp
@@ -242,8 +242,8 @@ int main(int argc, char* argv[]){
   files.push_back("Data_MET-2012C-24Aug2012-v1");
   files.push_back("Data_MET-2012C-11Dec2012-v1"); 
   files.push_back("Data_MET-2012C-PromptReco-v2");
-  //files.push_back("Data_MET-2012D-PromptReco-v1");
-  files.push_back("Data_MET-2012D-PromptReco-v1_7_269fb");
+  files.push_back("Data_MET-2012D-PromptReco-v1");
+  //files.push_back("Data_MET-2012D-PromptReco-v1_7_269fb");
   //files.push_back("Data_MET-2012D-PromptReco-v1_7_193fb");
   files.push_back("MC_QCD-Pt-30to50-pythia6");
   files.push_back("MC_QCD-Pt-50to80-pythia6");
