978b985950e77e27e8b8bec5f72220ae48534a73
agilbert <>
2013-05-13 12:45:54 +0000

    add script to download mssm xs files (too large for cvs)

---
+++ Analysis/HiggsTauTau/data/scale_factors/get_mssm_xs_files.sh
@@ -0,0 +1,2 @@
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/MSSMNeutral/out.mhmax_mu200_8_nnlo.tanBeta_gte1_FHv274.root
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/MSSMNeutral/out.mhmax_mu200_7_nnlo.tanBeta_gte1.root
