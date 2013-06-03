d491af78a7013299ffcf0af806f81826be33764a
rlane <>
2013-06-03 17:11:02 +0000

    change binning on jet pt plots and labels for emu lepton kinematic plots

--- Analysis/HiggsTauTau/scripts/plot_mssm_control_2012.sh
+++ Analysis/HiggsTauTau/scripts/plot_mssm_control_2012.sh
@@ -1,5 +1,5 @@
 PROD=Paper
-YEAR=2011
+YEAR=2012
 PARAMS=./scripts/"$PROD"_params_$YEAR.dat
 #SCALE_ET_JET="--draw_band_on_stack=true --band_size_fractional=0.09"
 #SCALE_MT_JET="--draw_band_on_stack=true --band_size_fractional=0.09"
@@ -95,17 +95,17 @@ SCALE_ET_MT=""
   --paramfile=$PARAMS $SCALE_MT_JET --log_y=false --extra_pad=1.7
 
 
-./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=et --rebin=5 --non_mass_plot=true \
+./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=et --rebin=2 --non_mass_plot=true \
   --method=8 --category="1jet" --plot_name="jpt_1" --x_axis_label="Leading jet p_{T} [GeV]" \
   --custom_x_axis_range=true --x_axis_min=0 --x_axis_max=200 \
   --paramfile=$PARAMS $SCALE_ET_JET --log_y=false --extra_pad=1.1
 
-./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=mt --rebin=5 --non_mass_plot=true \
+./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=mt --rebin=2 --non_mass_plot=true \
   --method=8 --category="1jet" --plot_name="jpt_1" --x_axis_label="Leading jet p_{T} [GeV]" \
   --custom_x_axis_range=true --x_axis_min=0 --x_axis_max=200 \
   --paramfile=$PARAMS $SCALE_MT_JET --log_y=false --extra_pad=1.1
 
-  ./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=em --rebin=5 --non_mass_plot=true \
+  ./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=em --rebin=2 --non_mass_plot=true \
   --method=8 --category="1jet" --plot_name="jpt_1" --x_axis_label="Leading jet p_{T} [GeV]" \
   --custom_x_axis_range=true --x_axis_min=0 --x_axis_max=200 \
   --paramfile=$PARAMS $SCALE_MT_JET --log_y=false --extra_pad=1.1
@@ -188,7 +188,7 @@ SCALE_ET_MT=""
   --paramfile=$PARAMS $SCALE_MT_INC --log_y=false
 
 ./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=em --rebin=2 --non_mass_plot=true \
-  --method=8 --category="inclusive" --plot_name="pt_1"  --x_axis_label="Muon p_{T} [GeV]" \
+  --method=8 --category="inclusive" --plot_name="pt_1"  --x_axis_label="Electron p_{T} [GeV]" \
   --custom_x_axis_range=true --x_axis_min=0 --x_axis_max=150 \
   --paramfile=$PARAMS $SCALE_MT_INC --log_y=false
 
@@ -204,7 +204,7 @@ SCALE_ET_MT=""
   --paramfile=$PARAMS $SCALE_MT_INC --log_y=false
 
 ./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=em --rebin=2 --non_mass_plot=true \
-  --method=8 --category="inclusive" --plot_name="pt_2"  --x_axis_label="Tau p_{T} [GeV]" \
+  --method=8 --category="inclusive" --plot_name="pt_2"  --x_axis_label="Muon p_{T} [GeV]" \
   --custom_x_axis_range=true --x_axis_min=0 --x_axis_max=150 \
   --paramfile=$PARAMS $SCALE_MT_INC --log_y=false
 
@@ -220,7 +220,7 @@ SCALE_ET_MT=""
   --paramfile=$PARAMS --extra_pad=1.4 $SCALE_MT_INC --log_y=false
 
 ./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=em --rebin=2 --non_mass_plot=true \
-  --method=8 --category="inclusive" --plot_name="eta_1"  --x_axis_label="Muon #eta" \
+  --method=8 --category="inclusive" --plot_name="eta_1"  --x_axis_label="Electron #eta" \
   --custom_x_axis_range=false --x_axis_min=0 --x_axis_max=150 \
   --paramfile=$PARAMS --extra_pad=1.4 $SCALE_MT_INC --log_y=false
 
@@ -236,7 +236,7 @@ SCALE_ET_MT=""
   --paramfile=$PARAMS --extra_pad=1.5 $SCALE_MT_INC --log_y=false
 
 ./bin/HiggsTauTauPlot3 --cfg=scripts/plot_mssm_$YEAR.cfg --channel=em --rebin=2 --non_mass_plot=true \
-  --method=8 --category="inclusive" --plot_name="eta_2"  --x_axis_label="Tau #eta" \
+  --method=8 --category="inclusive" --plot_name="eta_2"  --x_axis_label="Muon #eta" \
   --custom_x_axis_range=false --x_axis_min=0 --x_axis_max=150 \
   --paramfile=$PARAMS --extra_pad=1.5 $SCALE_MT_INC --log_y=false
 
