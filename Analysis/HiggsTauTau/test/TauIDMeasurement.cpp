c1e7d75f692d83e2c94bc23377959697103ad7a1
agilbert <>
2013-06-09 13:33:00 +0000

    Tau ID measurement updates

--- Analysis/HiggsTauTau/test/TauIDMeasurement.cpp
+++ Analysis/HiggsTauTau/test/TauIDMeasurement.cpp
@@ -422,16 +422,12 @@ int main(int argc, char* argv[]){
   }
   if (output_name.find("DYJetsToLL") != output_name.npos) httWeights.set_do_mtau_fakerate(true);
   if (is_embedded) httWeights.set_do_trg_weights(false).set_trg_applied_in_mc(false).set_do_idiso_weights(false).set_do_id_weights(false);
-  if (output_name.find("WJetsToLNu") != output_name.npos  ||
-      output_name.find("W1JetsToLNu") != output_name.npos ||
-      output_name.find("W2JetsToLNu") != output_name.npos ||
-      output_name.find("W3JetsToLNu") != output_name.npos ||
-      output_name.find("W4JetsToLNu") != output_name.npos) {
+  if (output_name.find("WJetsToLNuSoup") != output_name.npos) {
     httWeights.set_do_w_soup(true);
     httWeights.SetWTargetFractions(0.743925, 0.175999, 0.0562617, 0.0168926, 0.00692218);
     httWeights.SetWInputYields(76102995.0, 23141598.0, 34044921.0, 15539503.0, 13382803.0);
   }
-  if (output_name.find("JetsToLL") != output_name.npos) {
+  if (output_name.find("DYJets") != output_name.npos && output_name.find("Soup") != output_name.npos) {
     httWeights.set_do_dy_soup(true);
     httWeights.SetDYTargetFractions(0.723342373, 0.190169492, 0.061355932, 0.017322034, 0.007810169);
     httWeights.SetDYInputYields(30459503.0, 24045248.0, 21852156.0, 11015445.0, 6402827.0);
