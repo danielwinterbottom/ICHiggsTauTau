7cb36d24b2ba7a8a2ab220ac57d86a83f9af84b5
Rebecca Lane <rcl11@imperial.ac.uk>
2013-06-17 14:02:24 +0200

    adding the MC skim to the same script

--- Analysis/TagAndProbe/scripts/June6_skim.sh
+++ Analysis/TagAndProbe/scripts/June6_skim.sh
@@ -52,3 +52,43 @@ JOBSUBMIT=true
       $JOBSUBMIT jobs/$JOB.sh
   done
  
+ CONFIG=data/cfg_files/2012ElectronSkim.cfg
+ echo $CONFIG
+ PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/rlane/June6/Data_53X/
+ 
+ PATHS=(
+       'DYJetsToLL'
+       )
+  for i in "${PATHS[@]}"
+  do
+      echo "$i"
+ 
+      mkdir -p $SKIMPATH/tandp_e_skim/$i
+      
+      JOB="$i"_tandp_e_skim
+      $JOBWRAPPER "./bin/TagAndProbe --is_data=0 --is_elec=1 --cfg=$CONFIG --filelist="$FILELIST"_"$i".dat \
+      --do_skim=true --skim_path=$SKIMPATH/tandp_e_skim/"$i"/ --input_prefix=$PREFIX &> jobs/$JOB.log ; \
+      ./scripts/hadd_and_filelist.sh $SKIMPATH tandp_e_skim/"$i" "$FILELIST"_"$JOB".dat" jobs/$JOB.sh
+      $JOBSUBMIT jobs/$JOB.sh
+  done
+ 
+ CONFIG=data/cfg_files/2012MuonSkim.cfg
+ echo $CONFIG
+ PREFIX=root://xrootd.grid.hep.ph.ic.ac.uk//store/user/rlane/June6/Data_53X/
+ 
+ PATHS=(
+       'DYJetsToLL'
+       )
+  for i in "${PATHS[@]}"
+  do
+      echo "$i"
+ 
+      mkdir -p $SKIMPATH/tandp_mu_skim/$i
+      
+      JOB="$i"_tandp_mu_skim
+      $JOBWRAPPER "./bin/TagAndProbe --is_data=0 --is_elec=0 --cfg=$CONFIG --filelist="$FILELIST"_"$i".dat \
+      --do_skim=true --skim_path=$SKIMPATH/tandp_mu_skim/"$i"/ --input_prefix=$PREFIX &> jobs/$JOB.log ; \
+      ./scripts/hadd_and_filelist.sh $SKIMPATH tandp_mu_skim/"$i" "$FILELIST"_"$JOB".dat" jobs/$JOB.sh
+      $JOBSUBMIT jobs/$JOB.sh
+  done
+ 
