8192912d64f365cb6d67d0737119e700f778cde2
amagnan <a.magnan@imperial.ac.uk>
2013-09-16 17:04:45 +0200

    adding option to disable grid setup to use at CERN

--- Analysis/HiggsNuNu/scripts/generate_job.sh
+++ Analysis/HiggsNuNu/scripts/generate_job.sh
@@ -2,15 +2,27 @@
 
 if [ -z $2 ]
 then
-    echo "Must specify <script input> <script output>"
+    echo "Must specify <script input> <script output> <optional:GridSetup>"
     exit
 fi
 
 INPUT=$1
 OUTPUT=$2
+GRIDSETUP=1
+
+if (( "$#" == "3" ))
+    then
+    echo "Option grid setup: $3"
+    let GRIDSETUP=$3
+fi
 
 echo "cd $PWD" &> $OUTPUT
-echo "source /vols/cms/grid/setup.sh" >> $OUTPUT
+if (( "$GRIDSETUP" != "0" )); then
+    echo "Grid setup is enabled."
+    echo "source /vols/cms/grid/setup.sh" >> $OUTPUT
+else 
+    echo "Grid setup is disabled."
+fi
 echo "export SCRAM_ARCH=slc5_amd64_gcc462" >> $OUTPUT
 echo "eval \`scramv1 runtime -sh\`" >> $OUTPUT
 echo "source $PWD/scripts/setup_libs.sh" >> $OUTPUT
