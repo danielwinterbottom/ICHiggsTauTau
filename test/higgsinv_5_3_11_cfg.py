bf10b7423dda4c133f927853a954e625316b6da0
Patrick <pdunne@cern.ch>
2013-12-18 12:34:54 +0100

    fixing global tag typo

--- test/higgsinv_5_3_11_cfg.py
+++ test/higgsinv_5_3_11_cfg.py
@@ -727,17 +727,19 @@ if (release == '53X'):
       #fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/p/pdunne/public/TauPlusX-2012D.root')
       #fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/p/pdunne/private/CMSSW_5_3_7/src/UserCode/ICHiggsTauTau/test/higgsinvcrab/pickevents.root')
       #fileNames = cms.untracked.vstring("/store/user/srimanob/invHiggs/22Jan13/0008F234-739C-E211-A836-002590832A48.root")
-      fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/a/amagnan/CMSSW_5_3_11_patch6/src/UserCode/ICHiggsTauTau/python/embedded_dataTestFile.root')       
+      #fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/a/amagnan/CMSSW_5_3_11_patch6/src/UserCode/ICHiggsTauTau/python/embedded_dataTestFile.root')
+      fileNames = cms.untracked.vstring('root://gfe02.grid.hep.ph.ic.ac.uk:1097')
      )
-    process.GlobalTag.globaltag = cms.string('FT_53_V21A_AN6::All')
+    process.GlobalTag.globaltag = cms.string('FT53_V21A_AN6::All')
   else:
     process.source = cms.Source(
       "PoolSource",
       fileNames = cms.untracked.vstring(
       #'file:/Volumes/Storage/samples/VBF_HToTauTau_M-125-53X.root'
       #'file:/afs/cern.ch/work/p/pdunne/public/VBF_HToTauTau_M-125-53X.root'
-      'file:/afs/cern.ch/work/a/amagnan/CMSSW_5_3_11_patch6/src/UserCode/ICHiggsTauTau/python/testFile_W2j.root'       
-      #'file:/afs/cern.ch/work/a/amagnan/CMSSW_5_3_11_patch6/src/UserCode/ICHiggsTauTau/python/embedded_mcW2jTestfile.root'       
+      #'file:/afs/cern.ch/work/a/amagnan/CMSSW_5_3_11_patch6/src/UserCode/ICHiggsTauTau/python/testFile_W2j.root'       
+      #'file:/afs/cern.ch/work/a/amagnan/CMSSW_5_3_11_patch6/src/UserCode/ICHiggsTauTau/python/embedded_mcW2jTestfile.root'
+      'root://gfe02.grid.hep.ph.ic.ac.uk:1097'
       )
         #'file:/Volumes/Storage/samples/VBF_HToTauTau_M-125-53X.root'
         #'file:/Volumes/Storage/samples/embed_mutau_v1_DYJetsToLL.root'
