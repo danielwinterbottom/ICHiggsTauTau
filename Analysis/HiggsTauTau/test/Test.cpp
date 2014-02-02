1203031e4a7c43d57026542fdd84487fcf2c52c2
Andrew Gilbert <andrew.gilbert@cern.ch>
2014-02-02 16:34:18 +0000

    moving CombineHarvester to separate pacakge

--- Analysis/HiggsTauTau/test/Test.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/interface/CombineHarvester.h"
-
-int main() {
-
-	ch::CombineHarvester h;
-	h.ParseDatacard("paper_final/125/htt_mt_1_7TeV.txt", "125", "7TeV", "mt", 1);
-	h.PrintAll();
-}
