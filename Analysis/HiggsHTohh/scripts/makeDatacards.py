1b8b1bc2977f23b0fb50a686a2d3f4724084af41
Rebecca Lane <rcl11@imperial.ac.uk>
2014-03-10 12:46:34 +0100

    adding systematics for emu, tidying up the options to use m_sv or m_jj for the distribution to fit

--- Analysis/HiggsHTohh/scripts/makeDatacards.py
+++ Analysis/HiggsHTohh/scripts/makeDatacards.py
@@ -31,11 +31,11 @@ parser.add_option("--extra", dest="extra", type='string', default='',
 parser.add_option("-s", "--scheme", dest="scheme", type='string', default='HTohh',
                   help="datacard scheme")
 parser.add_option("--mvis", dest="mvis", action='store_true', default=False,
-                  help="Only make inputs for visible mass, no svfit.")
+                  help="Make inputs for visible mass.")
 parser.add_option("--svfit", dest="svfit", action='store_true', default=False,
-                  help="Only make inputs for svfit mass.")
+                  help="Make inputs for svfit mass.")
 parser.add_option("--dijet", dest="dijet", action='store_true', default=False,
-                  help="Only make inputs for dijet mass.")
+                  help="Make inputs for dijet mass.")
 parser.add_option("-e", dest="energy", type='string', default='8',
                   help="The C.O.M. energy is written into the datacard name, default is 8")
 
@@ -60,6 +60,10 @@ if not channels:
 for channel in channels:
   validate_channel(channel)
 
+if not (options.mvis or options.svfit or options.dijet) :
+  print 'Error, please specify distribution to plot.'
+  sys.exit(1)
+
 
 PARAMS=options.params
 CFG=options.config
@@ -82,113 +86,30 @@ extra_channel = {
   "em" : ' --fix_empty_bins="Fakes" --fix_negative_bins="Fakes,Fakes.*Up,Fakes.*Down"'
 }
 
-
-#################################################################
-#### Control Plots
-#################################################################
-if options.scheme == 'control_plots':
-  BINS_FINE="0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,225,250,275,300,325,350"
-  BINS="0,20,40,60,80,100,120,140,160,180,200,250,300,350"
-  extra_global += ' --syst_ggh_pt="QCDscale_ggH1in"'
-  extra_channel["em"] += '  --set_alias="sel:em_gf_mva>-0.5"'
-  extra_global += ' --hww_masses=110,115,120,125,130,135,140,145,150,155,160'
-  extra_channel["et"] += ' --syst_zl_shift="CMS_htt_ZLScale_etau_'+COM+'TeV:1.02:0.98"'
-  extra_channel["mt"] += ' --syst_zl_shift="CMS_htt_ZLScale_mutau_'+COM+'TeV:1.02:0.98"'
-  extra_channel["et"] += ' --set_alias="inclusive:pt_2>30." --set_alias="1jet:(n_jets>=1 && pt_2>30.)" --set_alias="twojet:(n_jets>=2 && pt_2>30.)" --set_alias="sel:mt_1<30."'
-  extra_channel["mt"] += ' --set_alias="inclusive:pt_2>30." --set_alias="1jet:(n_jets>=1 && pt_2>30.)" --set_alias="twojet:(n_jets>=2 && pt_2>30.)" --set_alias="sel:mt_1<30."'
-  scheme_et = [
-    ("8",   "inclusive",                "0jet_low",              BINS_FINE,  ""),
-    ("2",   "twojet",                   "vbf_loose",             BINS,       (
-      ' --syst_qcd_shape="CMS_htt_QCDShape_etau_vbf_loose_'+COM+'TeV:50:1.0:0.10"'
-      ' --set_alias="w_vbf_os:1"'
-      ' --set_alias="w_shape_os:1"')),
-    ("2",   "twojet",                   "vbf",                   BINS,       (
-      ' --syst_qcd_shape="CMS_htt_QCDShape_etau_vbf_'+COM+'TeV:50:1.0:0.10"'
-      ' --set_alias="w_vbf_os:1"'
-      ' --set_alias="w_shape_os:1"')),
-    ("2",   "1jet",                     "1jet_medium",           BINS_FINE,  (
-      ' --syst_qcd_shape="CMS_htt_QCDShape_etau_1jet_medium_'+COM+'TeV:50:1.0:0.10"'
-      ' --set_alias="w_shape_os:1"'))
-  ]
-  scheme_mt = [
-    ("8",   "inclusive",                "0jet_low",              BINS_FINE,  ""),
-    ("2",   "twojet",                   "vbf_loose",             BINS, (
-      ' --syst_qcd_shape="CMS_htt_QCDShape_mutau_vbf_loose_'+COM+'TeV:50:1.0:0.10"'
-      ' --set_alias="w_vbf_os:1"'
-      ' --set_alias="w_shape_os:1"')),
-    ("2",   "twojet",                   "vbf",                   BINS, (
-      ' --syst_qcd_shape="CMS_htt_QCDShape_mutau_vbf_'+COM+'TeV:50:1.0:0.10"'
-      ' --set_alias="w_vbf_os:1"'
-      ' --set_alias="w_shape_os:1"')),
-    ("2",   "1jet",                     "1jet_medium",           BINS_FINE,  (
-      ' --set_alias="w_shape_os:1"'
-      ' --syst_qcd_shape="CMS_htt_QCDShape_mutau_1jet_medium_'+COM+'TeV":50:1.1:0.10'))
-  ]
-  scheme_mtmet = [
-    ("8",   "inclusive",                "inclusive",              BINS_FINE,  ' --syst_l1met=CMS_L1etm_mutausoft_inclusive_8TeV'),
-    ("2",   "twojet",                   "vbf",                    BINS, (
-      ' --syst_l1met=CMS_L1etm_mutau_soft_vbf_8TeV'
-      ' --set_alias="w_vbf_os:1"'
-      ' --set_alias="w_shape_os:1"')),
-    ("2",   "1jet",                     "1jet_medium",            BINS_FINE, (
-      ' --syst_l1met=CMS_L1etm_mutausoft_1jet_medium_8TeV'
-      ' --syst_qcd_shape="CMS_htt_QCDShape_mutausoft_1jet_medium_'+COM+'TeV:50:1.1:0.10"'
-      ' --set_alias="w_shape_os:1" '
-      ' --qcd_os_ss_factor=1.06'))
-  ]
-  scheme_em = [
-    ("8",   "inclusive",       "0jet_low",  BINS_FINE, ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("2",   "twojet",          "vbf_loose", BINS,      ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("2",   "twojet",          "vbf",       BINS,      ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("2",   "1jet",            "1jet_low",  BINS_FINE, ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"')
-  ]
-  bkg_schemes = {
-    'et'    : 'et_default',
-    'mt'    : 'mt_with_zmm',
-    'mtmet' : 'mt_default',
-    'em'    : 'em_default'
-  }
-  sig_scheme = 'sm_default'
-  ANA = 'sm'
-
-#################################################################
-#### Old MSSM scheme
-#################################################################
-if options.scheme == 'old_mssm':
-  BINS_FINE="0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,225,250,275,300,325,350,400,500,700,1000,1500"
-  BINS="0,20,40,60,80,100,120,140,160,180,200,250,300,350,400,500,700,1000,1500"
-  scheme_et = [
-    ("8",    "inclusive",   "inclusive",  BINS_FINE,  ''),
-    ("11",   "nobtag",      "nobtag",     BINS_FINE,  ''),
-    ("12",   "btag",        "btag",       BINS,       ' --sub_ztt_top_frac=0.015') 
-  ]
-  scheme_mt = [
-    ("8",    "inclusive",   "inclusive",  BINS_FINE,  ''),
-    ("11",   "nobtag",      "nobtag",     BINS_FINE,  ''),
-    ("12",   "btag",        "btag",       BINS,       ' --sub_ztt_top_frac=0.015') 
-  ]
-  scheme_em = [
-    ("8",    "inclusive",   "inclusive",  BINS_FINE,  ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("11",   "nobtag",      "nobtag",     BINS_FINE,  ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("12",   "btag",        "btag",       BINS,       ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV" --sub_ztt_top_frac=0.015') 
-  ]
-  bkg_schemes = {
-    'et' : 'et_default',
-    'mt' : 'mt_with_zmm',
-    'em' : 'em_default'
-  }
-  extra_channel["et"] += ' --set_alias="sel:mt_1<30."'
-  extra_channel["mt"] += ' --set_alias="sel:mt_1<30."'
-  sig_scheme = 'mssm_default'
-  ANA = 'mssm'
+#### Set which distribution to include in datacards (note that because of different binnings it is only possible to run one at a time)
+plots = [ 
+  ('m_vis'  , 'M_{#tau#tau}^{vis} [GeV]'  , '-mvis' ,   "100"    ,"150"),
+  ('m_sv'   , 'M_{#tau#tau} [GeV]'        , ''      ,   "100"   ,"150"),
+  ('prebjet_mjj'   , 'M_{jj} [GeV]'        , ''      ,   "80"   ,"160")
+ ]
+if options.mvis: 
+    plots = [plots[0]]
+if options.svfit:
+    plots = [plots[1]]
+if options.dijet: 
+    plots = [plots[2]]
 
 #################################################################
 #### New HTohh scheme
 #################################################################
 
 if options.scheme == 'HTohh':
-  BINS_FINE="0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,225,250,275,300,325,350"
-  BINS="0,20,40,60,80,100,120,140,160,180,200,250,300,350"
+  if options.mvis or options.svfit:  
+    BINS_FINE="[0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,225,250,275,300,325,350]"
+    BINS="[0,20,40,60,80,100,120,140,160,180,200,250,300,350]"
+  elif options.dijet :
+    BINS_FINE="(30,0,600)"
+    BINS="(15,0,600)"
   scheme_et = [
     ("8",    "inclusive",   "inclusive",  BINS_FINE,  (
       ' --syst_w_fake_rate="CMS_htt_WShape_etau_inclusive_'+COM+'TeV"')),
@@ -220,10 +141,23 @@ if options.scheme == 'HTohh':
       ' --sub_ztt_top_frac=0.015'))
   ]
   scheme_em = [
-    ("8",    "inclusive",   "inclusive",  BINS_FINE,  ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("0",   "0tag",      "0tag",     BINS_FINE,  ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("1",   "1tag",      "1tag",     BINS_FINE,  ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'),
-    ("2",   "2tag",        "2tag",       BINS,  ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"') 
+    ("8",    "inclusive",   "inclusive",  BINS_FINE,  (
+      ' --syst_tquark="CMS_htt_TTbarShape_em_inclusive_'+COM+'TeV"'
+      ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"')),
+    ("0",   "0tag",      "0tag",     BINS_FINE, (
+      ' --syst_tquark="CMS_htt_TTbarShape_em_0tag_'+COM+'TeV"'
+      ' --syst_fakes_os_ss_shape="CMS_htt_FakeShape_em_0tag_'+COM+'TeV"'
+      ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"')),
+    ("1",   "1tag",      "1tag",     BINS_FINE,  (
+      ' --syst_tquark="CMS_htt_TTbarShape_em_1tag_'+COM+'TeV"'
+      ' --syst_qcd_shape="CMS_htt_FakeShape_em_1tag_'+COM+'TeV:50:1.1:0.10"'
+      ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'
+      ' --sub_ztt_top_frac=0.015')),
+    ("2",   "2tag",        "2tag",       BINS, ( 
+      ' --syst_tquark="CMS_htt_TTbarShape_em_2tag_'+COM+'TeV"'
+      ' --syst_qcd_shape="CMS_htt_FakeShape_em_2tag_'+COM+'TeV:50:1.1:0.10"'
+      ' --syst_tau_scale="CMS_scale_e_'+COM+'TeV"'
+      ' --sub_ztt_top_frac=0.015'))
   ]
   bkg_schemes = {
     'et' : 'et_default',
@@ -245,17 +179,8 @@ cat_schemes = {
    'em' : scheme_em
 }
 
-plots = [ 
-  ('m_vis'  , 'M_{#tau#tau}^{vis} [GeV]'  , '-mvis' ,   "100"    ,"150"),
-  ('m_sv'   , 'M_{#tau#tau} [GeV]'        , ''      ,   "100"   ,"150"),
-  ('prebjet_mjj'   , 'M_{jj} [GeV]'        , ''      ,   "80"   ,"160")
- ]
-if options.mvis: 
-    plots = [plots[0]]
-if options.svfit:
-    plots = [plots[1]]
-if options.dijet: 
-    plots = [plots[2]]
+
+   
 
 for pl in plots:
   var     = pl[0]
@@ -277,7 +202,7 @@ for pl in plots:
       os.system('$CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsHTohh/bin/HiggsHTohhPlot --cfg=%(CFG)s --channel=%(ch)s'
         #' --method=%(cat_num)s --cat=%(cat_str)s --datacard=%(dc)s'
         ' --method=8 --cat=%(cat_str)s --datacard=%(dc)s'
-        ' --var="%(var)s[%(bin)s]" --norm_bins=true '
+        ' --var="%(var)s%(bin)s" --norm_bins=true '
         ' --background_scheme=%(bkg_scheme)s --signal_scheme=%(sig_scheme)s'
         ' --x_axis_label="%(xlab)s" --y_axis_label="dN/dm_{#tau#tau} [1/GeV]"'
         ' --blind=%(BLIND)s --x_blind_min=%(blind_min)s --x_blind_max=%(blind_max)s --verbosity 0'
