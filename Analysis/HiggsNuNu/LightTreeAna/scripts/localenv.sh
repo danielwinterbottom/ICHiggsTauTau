b66977acb6da55a16400e386b8b2582810d8b5c0
amagnan <a.magnan@imperial.ac.uk>
2014-06-12 17:27:13 +0200

    remove other useless lines

--- Analysis/HiggsNuNu/LightTreeAna/scripts/localenv.sh
+++ Analysis/HiggsNuNu/LightTreeAna/scripts/localenv.sh
@@ -1,7 +1,6 @@
 export CMSSW_BASE=~/SOFTWARE/CMS/Hinvisible/
 export ROOFITSYS=$ROOTSYS/roofit/roofit/
 alias lmake='make -f LocalMakefile'
-LOCAL_COMPIL=1
 ln -s ../interface/HiggsNuNuAnalysisTools.h interface/
 ln -s ../src/HiggsNuNuAnalysisTools.cc src/
 ln -s LocalRules.mk Rules.top
