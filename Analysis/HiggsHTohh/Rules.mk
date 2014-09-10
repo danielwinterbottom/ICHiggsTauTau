fcbb46e7b8ec3707990110b5b1ef0dadaaa09e0f
Rebecca Lane <rcl11@imperial.ac.uk>
2014-09-10 11:53:48 +0200

    Tidying Rules.mk and library setup script

--- Analysis/HiggsHTohh/Rules.mk
+++ Analysis/HiggsHTohh/Rules.mk
@@ -1,3 +1,3 @@
 SUBDIRS 	:= 
 LIB_DEPS 	:= Core Utilities Modules HiggsTauTau 
-LIB_EXTRA := -lCondFormatsJetMETObjects -L HHKinFit -lHHKinFit 
+LIB_EXTRA := -lCondFormatsJetMETObjects -L$(PWD)/HHKinFit -lHHKinFit 
--- Analysis/HiggsHTohh/scripts/setup_libs.sh
+++ Analysis/HiggsHTohh/scripts/setup_libs.sh
@@ -2,3 +2,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMSSW_BASE/src/UserCode/ICHiggsTauTau/A
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/Utilities/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/Modules/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsHTohh/HHKinFit/
