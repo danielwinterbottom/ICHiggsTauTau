5e05638475c0cd4a629c793e75b3f1ce755ae6e5
Andrew Gilbert <andrew.gilbert@cern.ch>
2014-09-29 16:55:48 +0200

    update CombineHarvester tests and plotting

--- Analysis/CombineTools/test/ParametricMSSM.cpp
+++ Analysis/CombineTools/test/ParametricMSSM.cpp
@@ -11,6 +11,7 @@
 #include "CombineTools/interface/MorphFunctions.h"
 #include "CombineTools/interface/HttSystematics.h"
 #include "CombineTools/interface/RooHttSMPdf.h"
+#include "CombineTools/interface/Systematics.h"
 // #include "CombineTools/interface/RooHttYield.h"s
 // #include "TH1F.h"
 #include "RooWorkspace.h"
@@ -28,6 +29,12 @@ using std::map;
 using std::set;
 
 int main() {
+  bool create_asimov = false;
+  bool do_parametric = true;
+  bool inject_signal = false;
+  string inject_mass = "900";
+
+
   TH1::AddDirectory(false);
   ch::CombineHarvester cb;
 
@@ -49,7 +56,9 @@ int main() {
   std::cout << " done\n";
 
   std::cout << "Adding signal processes...";
-  cb.AddProcesses(masses, {"htt"}, {"7TeV", "8TeV"}, {"mt"}, {"ggH"}, mt_cats,
+  cb.AddProcesses(masses, {"htt"}, {"7TeV", "8TeV"}, {"mt"}, {"ggH"}, {mt_cats[0]},
+                  true);
+  cb.AddProcesses(masses, {"htt"}, {"7TeV", "8TeV"}, {"mt"}, {"bbH"}, {mt_cats[1]},
                   true);
   std::cout << " done\n";
 
@@ -80,7 +89,6 @@ int main() {
                      {p + "_" + e});
     }
   }
-
   for (std::string const& e : {"7TeV", "8TeV"}) {
     for (std::string const& p : {"ggH", "bbH"}) {
       std::cout << "Scaling for process " << p << " and era " << e << "\n";
@@ -90,91 +98,141 @@ int main() {
     }
   }
 
+  cb.era({"8TeV"});
+
   std::cout << "Setting standardised bin names...";
   cb.ForEachObs(ch::SetStandardBinName<ch::Observation>);
   cb.ForEachProc(ch::SetStandardBinName<ch::Process>);
   cb.ForEachNus(ch::SetStandardBinName<ch::Nuisance>);
   std::cout << " done\n";
 
-  cb.era({"8TeV"}).bin_id({8});
+  // cb.era({"8TeV"}).bin_id({8});
+  set<string> lm_bins =
+      cb.GenerateSetFromObs<string>(mem_fn(&ch::Observation::bin));
+
+  if (create_asimov) {
+    for (auto const& b : lm_bins) {
+      ch::CombineHarvester tmp = std::move(
+          cb.cp().bin({b}).backgrounds().nus_type({"shape"}, false));
+      TH1F tot_bkg = tmp.GetShape();
+      double bkg_rate = tot_bkg.Integral();
+      tot_bkg.Scale(tmp.GetObservedRate()/tot_bkg.Integral());
+      // tot_bkg.Add(&sig, 0.0);
+      // for (int i = 1; i <= tot_bkg.GetNbinsX(); ++i) {
+      //   tot_bkg.SetBinContent(i, std::floor(tot_bkg.GetBinContent(i) + 0.5));
+      // }
+      tot_bkg.Scale(1.0/tot_bkg.Integral());
+      tmp.ForEachObs([&](ch::Observation * obs) {
+        obs->set_shape(ic::make_unique<TH1F>(tot_bkg));
+      });
+    }
+  }
 
   ch::CombineHarvester cb_hm = cb;
 
-  cb.VariableRebin({0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
-                    140, 150, 160, 170, 180, 190, 200});
-
-  // Drop shape uncerts on signal in the hm for now,
-  // the fb versions aren't in the datacards
-  cb_hm.FilterNus([&](ch::Nuisance const* p) {
-    return p->type() == "shape";
+  cb_hm.ForEachObs([&](ch::Observation* in) {
+    in->set_bin(in->bin() + "_hm");
+  });
+  cb_hm.ForEachProc([&](ch::Process* in) {
+    in->set_bin(in->bin() + "_hm");
+  });
+  cb_hm.ForEachNus([&](ch::Nuisance* in) {
+    in->set_bin(in->bin() + "_hm");
   });
 
-  bool do_parametric = true;
+  set<string> hm_bins =
+      cb_hm.GenerateSetFromObs<string>(mem_fn(&ch::Observation::bin));
+
+
+
+
+  cb.cp().bin_id({8}).VariableRebin(
+    {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120,
+      130, 140, 150, 160, 170, 180, 190, 200});
+  cb.cp().bin_id({9}).VariableRebin(
+      {0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200});
+
+  // Drop shape uncerts on signal in the hm for now,
+  // the fb versions aren't in the datacards and
+  // we want to keep the comparison fair
+  cb_hm.FilterNus([&](ch::Nuisance const* p) {
+    return p->type() == "shape" && p->signal();
+  });
+
+  std::cout << "Doing bbb for the low mass...\n";
+  cb.cp().process({"W", "QCD", "ZTT", "ZL", "ZJ", "TT", "VV"})
+      .MergeBinErrors(0.05, 0.4);
+  cb.cp().process({"W", "QCD", "ZTT", "ZL", "ZJ", "TT", "VV"})
+      .AddBinByBin(0.05, true, &cb);
+  std::cout << "...done\n";
+
+
 
   if (do_parametric) {
-
-  std::vector<double> bins_hm;
-  double x = 200.;
-  while (x < 1501.) {
-    bins_hm.push_back(x);
-    x += 5.;
-  }
-  cb_hm.VariableRebin(bins_hm);
-  cb_hm.PrintAll();
-
-  RooWorkspace ws("htt", "htt");
-  set<string> bins =
-      cb_hm.GenerateSetFromObs<string>(mem_fn(&ch::Observation::bin));
-  for (auto const& b : bins) {
-    ch::CombineHarvester tmp = std::move(cb_hm.cp().bin({b}).backgrounds());
-    TH1F tot_bkg = tmp.GetShape();
-
-    TH1F sig = cb_hm.cp().bin({b}).mass({"900"}).signals().GetShape();
-
-    tot_bkg.Scale(tmp.GetObservedRate()/tot_bkg.Integral());
-    tot_bkg.Add(&sig, 0.0);
-    for (int i = 1; i <= tot_bkg.GetNbinsX(); ++i) {
-      tot_bkg.SetBinContent(i, std::floor(tot_bkg.GetBinContent(i) + 0.5));
-    }
-    double final_rate = tot_bkg.Integral();
-    tot_bkg.Scale(1.0/tot_bkg.Integral());
-    // tmp.ForEachObs([&](ch::Observation * obs) {
-    //   obs->set_rate(final_rate);
-    //   obs->set_shape(ic::make_unique<TH1F>(tot_bkg));
-    // });
-    tmp.process({"ZTT"});
-    tmp.ForEachProc([&](ch::Process *proc) {
-      proc->set_process("bkg");
-      proc->set_rate(final_rate);
-      proc->set_shape(nullptr);
+    cb_hm.FilterNus([&](ch::Nuisance const* p) {
+      return p->type() == "shape";
     });
 
-    cb_hm.process({"ggH", "bkg"});
+    std::vector<double> bins_hm;
+    double x = 200.;
+    while (x < 1501.) {
+      bins_hm.push_back(x);
+      x += 5.;
+    }
+    cb_hm.VariableRebin(bins_hm);
+    // cb_hm.PrintAll();
 
-    RooRealVar mtt("CMS_th1x", "CMS_th1x", 0,
-                   static_cast<float>(tot_bkg.GetNbinsX()));
-    RooRealVar lA((b + "_a").c_str(), "", 50, 0.01, 1000);
-    RooRealVar lB((b + "_b").c_str(), "", 50, -10500,
-                  10500);
-    RooRealVar lC((b + "_c").c_str(), "", 50, 0,
-                  10500);
-    // std::string fn = "exp(-(m-"+iStr+")/(a+0.001*b*(m-"+iStr+")))";
-    std::string fn = "exp(-CMS_th1x/("+b+"_a+"+b+"_b*0.001*CMS_th1x))";
-    std::cout << "fn = " << fn << std::endl;
-    RooGenericPdf bkg_pdf((b + "_bkgpdf").c_str(), fn.c_str(),
-                          RooArgList(mtt, lA, lB));
-    RooRealVar bkg_norm((b + "_bkgpdf_norm").c_str(), "", 1., 0., 10000.);
-    ws.import(bkg_pdf);
-    ws.import(bkg_norm);
-  }
+    RooWorkspace ws("htt", "htt");
+    for (auto const& b : hm_bins) {
+      ch::CombineHarvester tmp = std::move(cb_hm.cp().bin({b}).backgrounds());
+      TH1F tot_bkg = tmp.GetShape();
+      double bkg_error = 0.;
+      double bkg_rate = tot_bkg.IntegralAndError(1, tot_bkg.GetNbinsX(), bkg_error);
+      double bkg_uncert = tmp.GetUncertainty();
+       // tot_bkg.Integral();
+      std::cout << "bkg_rate: " << bkg_rate << "\t" << bkg_error << "\t" << tmp.GetUncertainty() <<  "\n";
+      // Here we just play a small trick to reduce the background to one process,
+      // ZTT, which we then change to the new bkg pdf
+      tmp.process({"ZTT"});
+      tmp.ForEachProc([&](ch::Process *proc) {
+        proc->set_process("bkg");
+        proc->set_rate(1.0);
+        proc->set_shape(nullptr);
+      });
 
-  cb_hm.AddWorkspace(&ws);
-  cb_hm.cp().backgrounds().ExtractPdfs("htt", "$CHANNEL_bkgpdf");
-  cb_hm.PrintAll();
+
+      RooRealVar mtt("CMS_th1x", "CMS_th1x", 0,
+                     static_cast<float>(tot_bkg.GetNbinsX()));
+      RooRealVar lA((b + "_a").c_str(), "", 50, 0.01, 1000);
+      RooRealVar lB((b + "_b").c_str(), "", 50, -10500,
+                    10500);
+      RooRealVar lC((b + "_c").c_str(), "", 50, 0,
+                    10500);
+      std::string cond = "(("+b+"_a+"+b+"_b*0.001*CMS_th1x) > 0) * ";
+      std::string fn = cond += "exp(-CMS_th1x/("+b+"_a+"+b+"_b*0.001*CMS_th1x))";
+      std::cout << "fn = " << fn << std::endl;
+      RooGenericPdf bkg_pdf((b + "_bkgpdf").c_str(), fn.c_str(),
+                            RooArgList(mtt, lA, lB));
+      // RooRealVar bkg_norm((b + "_bkgpdf_norm").c_str(), "", bkg_rate, 0., bkg_rate*10.);
+      RooRealVar bkg_norm((b + "_bkgpdf_norm").c_str(), "", bkg_rate);
+      tmp.cp().process({"bkg"})
+          .AddSyst(&cb_hm, "CMS_htt_norm_$BIN", "lnN", ch::syst::SystMap<>::init
+          (1.0 + (bkg_uncert/bkg_rate)));
+      bkg_norm.setConstant();
+      ws.import(bkg_pdf);
+      ws.import(bkg_norm);
+    }
+    cb_hm.process({"ggH", "bbH", "bkg"});
+
+    cb_hm.AddWorkspace(&ws);
+    cb_hm.cp().backgrounds().ExtractPdfs("htt", "$CHANNEL_bkgpdf");
+    // cb_hm.PrintAll();
   } else {
-    cb_hm.VariableRebin(
+    cb_hm.cp().bin_id({8}).VariableRebin(
         {200, 225, 250, 275, 300, 325, 350, 400, 500, 700, 1000, 1500});
-    std::cout << "Merging bin errors...\n";
+    cb_hm.cp().bin_id({9}).VariableRebin(
+        {200, 250, 300, 350, 400, 500, 700, 1000, 1500});
+    std::cout << "Doing bbb for the high mass...\n";
     cb_hm.cp().process({"W", "QCD", "ZTT", "ZL", "ZJ", "TT", "VV"})
         .MergeBinErrors(0.05, 0.4);
     cb_hm.cp().process({"W", "QCD", "ZTT", "ZL", "ZJ", "TT", "VV"})
@@ -182,68 +240,23 @@ int main() {
     std::cout << "...done\n";
   }
 
-  // // cb.bin({"htt_mt_5_8TeV", "htt_mt_6_8TeV"});
-  // // cb.nus_type({"shape"}, false);
-  // RooWorkspace ws("htt", "htt");
-
-  // bool do_morphing = true;
-  // if (do_morphing) {
-  //   set<string> bins =
-  //       cb.GenerateSetFromObs<string>(mem_fn(&ch::Observation::bin));
-  //   ch::CombineHarvester cb_sig = std::move(cb.cp().signals());
-  //   RooMsgService::instance().setGlobalKillBelow(RooFit::WARNING);
-  //   ch::BuildRooMorphing(ws, cb_sig, "MH", true);
-  //   cb.FilterNus([&](ch::Nuisance const* p) {
-  //     return p->type() == "shape" && p->signal();
-  //   });
-  //   cb.mass({"125", "*"});
-  //   cb.cp().signals().ForEachProc([&](ch::Process* p) {
-  //     p->set_shape(nullptr);
-  //     p->set_rate(1.0);
-  //   });
-
-  //   cb.AddWorkspace(&ws);
-  //   cb.cp().signals().ExtractPdfs("htt", "$CHANNEL_$PROCESS_mpdf");
-  // }
-  // cb.cp().nus_type({"shape"}).PrintAll();
-
-
-  // cb.AddWorkspace(&ws);
-  // ws.Print();
-  // cb.cp().signals().ExtractPdfs("htt", "$CHANNEL_$PROCESS_pdf");
-
-  // cb.cp().mass({"125", "*"}).PrintAll();
-  // std::cout << "Merging bin errors...\n";
-  // cb.cp().bin_id({0, 1, 2, 3, 4}).process({"W", "QCD"})
-  //     .MergeBinErrors(0.1, 0.4);
-  // cb.cp().bin_id({5}).era({"7TeV"}).process({"W"})
-  //     .MergeBinErrors(0.1, 0.4);
-  // cb.cp().bin_id({5, 6}).era({"8TeV"}).process({"W"})
-  //     .MergeBinErrors(0.1, 0.4);
-  // cb.cp().bin_id({7}).era({"8TeV"}).process({"W", "ZTT"})
-  //     .MergeBinErrors(0.1, 0.4);
-  // std::cout << "...done\n";
-
-  // std::cout << "Generating bbb uncertainties...\n";
-  // cb.cp().bin_id({0, 1, 2, 3, 4}).process({"W", "QCD"})
-  //     .AddBinByBin(0.1, true, &cb);
-  // cb.cp().bin_id({5}).era({"7TeV"}).process({"W"})
-  //     .AddBinByBin(0.1, true, &cb);
-  // cb.cp().bin_id({5, 6}).era({"8TeV"}).process({"W"})
-  //     .AddBinByBin(0.1, true, &cb);
-  // cb.cp().bin_id({7}).era({"8TeV"}).process({"W", "ZTT"})
-  //     .AddBinByBin(0.1, true, &cb);
-  // std::cout << "...done\n";
-
-  TFile output("htt_mt.mssm.root", "RECREATE");
-  cb_hm.cp().mass({"900", "*"}).WriteDatacard("htt_mssm_hm.txt", output);
-  // for (auto b : bins) {
-  //   for (auto m : masses /*{"125"}*/) {
-  //     std::cout << "Writing datacard for bin: " << b << " and mass: " << m
-  //               << "\n";
-  //     cb.cp().bin({b}).mass({m, "*"}).WriteDatacard(
-  //         b + "_" + m + ".txt", output);
-  //   }
-  // }
+  TFile output("parametric/htt_mt.mssm.root", "RECREATE");
+  // cb_hm.cp().mass({"900", "*"}).WriteDatacard("htt_mssm_hm.txt", output);
+  for (auto b : hm_bins) {
+    for (auto m : masses) {
+      std::cout << "Writing datacard for bin: " << b << " and mass: " << m
+                << "\n";
+      cb_hm.cp().bin({b}).mass({m, "*"}).WriteDatacard("parametric/"+
+          b + "_" + m + ".txt", output);
+    }
+  }
+  for (auto b : lm_bins) {
+    for (auto m : masses) {
+      std::cout << "Writing datacard for bin: " << b << " and mass: " << m
+                << "\n";
+      cb.cp().bin({b}).mass({m, "*"}).WriteDatacard("parametric/"+
+          b + "_" + m + ".txt", output);
+    }
+  }
 
 }
