e03469a4f1275556d2e8f95e6206218b151be84f
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2014-10-01 16:01:40 +0100

    Adding MT MVA module

---
+++ Analysis/HiggsHTohh/interface/HhhMTMVABoth.h
@@ -0,0 +1,52 @@
+#ifndef ICHiggsTauTau_HiggsTauTau_HhhMTMVABoth_h
+#define ICHiggsTauTau_HiggsTauTau_HhhMTMVABoth_h
+
+// #include "UserCode/ICHiggsTauTau/Analysis/Core/interface/TreeEvent.h"
+#include "UserCode/ICHiggsTauTau/Analysis/Core/interface/ModuleBase.h"
+#include <string>
+
+namespace TMVA {
+	class Reader;
+}
+
+namespace ic {
+
+class TreeEvent;
+
+class HhhMTMVABoth : public ModuleBase {
+ private:
+ 
+ 	CLASS_MEMBER(HhhMTMVABoth, std::string, ditau_label)
+ 	CLASS_MEMBER(HhhMTMVABoth, std::string, met_label)
+ 	CLASS_MEMBER(HhhMTMVABoth, std::string, gf_mva_file_bdt)
+  CLASS_MEMBER(HhhMTMVABoth, std::string, gf_mva_file_bdtg)
+	CLASS_MEMBER(HhhMTMVABoth, std::string, mva_input_data)
+
+ 	TMVA::Reader *gf_reader_bdt_;
+	TMVA::Reader *gf_reader_bdtg_;
+  // TMVA::Reader *vbf_reader_;
+
+ 	float fmet_;
+ 	float fpt_1_;
+ 	float fmt_1_;
+ 	float fpt_2_;
+	float femu_dphi_;
+	float fpzeta_;
+	float fprebjetpt_1_;
+	float fprebjetpt_2_;
+	
+	int		nprebjets_;
+
+ public:
+  HhhMTMVABoth(std::string const& name);
+  virtual ~HhhMTMVABoth();
+
+  virtual int PreAnalysis();
+  virtual int Execute(TreeEvent *event);
+  virtual int PostAnalysis();
+  virtual void PrintInfo();
+};
+
+}
+
+#endif
--- Analysis/HiggsHTohh/src/HhhCategories.cc
+++ Analysis/HiggsHTohh/src/HhhCategories.cc
@@ -144,6 +144,7 @@ namespace ic {
         outtree_->Branch("n_jetsingap_lowpt", &n_jetsingap_lowpt_);
         outtree_->Branch("l1_met",            &l1_met_);
         outtree_->Branch("calo_nohf_met",     &calo_nohf_met_);
+				outtree_->Branch("em_gf_mva_bdt",					&em_gf_mva_bdt_);
         if (channel_ == channel::em) {
           outtree_->Branch("em_gf_mva",         &em_gf_mva_);
           // outtree_->Branch("em_vbf_mva",        &em_vbf_mva_);
@@ -344,6 +345,7 @@ namespace ic {
 			em_gf_mva_bdt_ = event->Exists("em_gf_mva_bdt") ? event->Get<double>("em_gf_mva_bdt") : 0.;
       // em_vbf_mva_ = event->Exists("em_vbf_mva") ? event->Get<double>("em_vbf_mva") : 0.;
     }
+    em_gf_mva_bdt_ = event->Exists("em_gf_mva_bdt")? event->Get<double>("em_gf_mva_bdt") : 0.;
     if (event->Exists("mass_scale")) {
       m_sv_ = m_sv_ * event->Get<double>("mass_scale");
       m_vis_ = m_vis_ * event->Get<double>("mass_scale");
