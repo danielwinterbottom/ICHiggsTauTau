e82015b3366991d7ae70047b7f468a8072c199c3
Patrick <pdunne@cern.ch>
2015-02-17 15:52:59 +0000

    adding new lepton scale factors

---
+++ Analysis/HiggsNuNu/data/scale_factors/makeMuonParkedTxtFiles.C
@@ -0,0 +1,99 @@
+#include "TFile.h"
+#include "TGraphAsymmErrors.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+
+
+int makeMuonParkedTxtFiles(){//main
+
+  TFile *muId_ = new TFile("MuonEfficiencies_Run2012ReReco_53X.root");
+  TFile *muIso_ = new TFile("MuonEfficiencies_ISO_Run_2012ReReco_53X.root");
+
+  TGraphAsymmErrors *hist_muon[4][3][3];
+
+  muId_->cd();
+
+  hist_muon[0][0][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_Loose_pt_abseta<0.9");
+  hist_muon[0][0][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_Loose_pt_abseta<0.9");
+  hist_muon[0][0][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_Loose_pt_abseta<0.9");
+  hist_muon[0][1][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_Loose_pt_abseta0.9-1.2");
+  hist_muon[0][1][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_Loose_pt_abseta0.9-1.2");
+  hist_muon[0][1][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_Loose_pt_abseta0.9-1.2");
+  hist_muon[0][2][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_Loose_pt_abseta1.2-2.1");
+  hist_muon[0][2][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_Loose_pt_abseta1.2-2.1");
+  hist_muon[0][2][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_Loose_pt_abseta1.2-2.1");
+
+  hist_muon[1][0][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_Tight_pt_abseta<0.9");
+  hist_muon[1][0][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_Tight_pt_abseta<0.9");
+  hist_muon[1][0][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_Tight_pt_abseta<0.9");
+  hist_muon[1][1][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_Tight_pt_abseta0.9-1.2");
+  hist_muon[1][1][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_Tight_pt_abseta0.9-1.2");
+  hist_muon[1][1][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_Tight_pt_abseta0.9-1.2");
+  hist_muon[1][2][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_Tight_pt_abseta1.2-2.1");
+  hist_muon[1][2][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_Tight_pt_abseta1.2-2.1");
+  hist_muon[1][2][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_Tight_pt_abseta1.2-2.1");
+
+  muIso_->cd();
+
+  hist_muon[2][0][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_combRelIsoPF04dBeta<02_Tight_pt_abseta<0.9");
+  hist_muon[2][0][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_combRelIsoPF04dBeta<02_Tight_pt_abseta<0.9");
+  hist_muon[2][0][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_combRelIsoPF04dBeta<02_Tight_pt_abseta<0.9");
+  hist_muon[2][1][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_combRelIsoPF04dBeta<02_Tight_pt_abseta0.9-1.2");
+  hist_muon[2][1][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_combRelIsoPF04dBeta<02_Tight_pt_abseta0.9-1.2");
+  hist_muon[2][1][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_combRelIsoPF04dBeta<02_Tight_pt_abseta0.9-1.2");
+  hist_muon[2][2][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_combRelIsoPF04dBeta<02_Tight_pt_abseta1.2-2.1");
+  hist_muon[2][2][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_combRelIsoPF04dBeta<02_Tight_pt_abseta1.2-2.1");
+  hist_muon[2][2][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_combRelIsoPF04dBeta<02_Tight_pt_abseta1.2-2.1");
+
+  hist_muon[3][0][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_combRelIsoPF04dBeta<012_Tight_pt_abseta<0.9");
+  hist_muon[3][0][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_combRelIsoPF04dBeta<012_Tight_pt_abseta<0.9");
+  hist_muon[3][0][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_combRelIsoPF04dBeta<012_Tight_pt_abseta<0.9");
+  hist_muon[3][1][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_combRelIsoPF04dBeta<012_Tight_pt_abseta0.9-1.2");
+  hist_muon[3][1][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_combRelIsoPF04dBeta<012_Tight_pt_abseta0.9-1.2");
+  hist_muon[3][1][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_combRelIsoPF04dBeta<012_Tight_pt_abseta0.9-1.2");
+  hist_muon[3][2][0] = (TGraphAsymmErrors*)gDirectory->Get("DATA_combRelIsoPF04dBeta<012_Tight_pt_abseta1.2-2.1");
+  hist_muon[3][2][1] = (TGraphAsymmErrors*)gDirectory->Get("MC_combRelIsoPF04dBeta<012_Tight_pt_abseta1.2-2.1");
+  hist_muon[3][2][2] = (TGraphAsymmErrors*)gDirectory->Get("DATA_over_MC_combRelIsoPF04dBeta<012_Tight_pt_abseta1.2-2.1");
+
+  double etaMin[3] = {0,0.9,1.2};
+  double etaMax[3] = {0.9,1.2,2.1};
+
+  std::string lFileName[4] = {"mu_loose_id","mu_tight_id","mu_loose_iso","mu_tight_iso"};
+  std::string lDataType[3] = {"data_eff","mc_eff","SF"};
+  
+  std::ostringstream lName;
+  
+  for (unsigned iWP(0);iWP<4;++iWP){//loop on WP
+    
+    for (unsigned iData(0);iData<3;++iData){//loop on data type: data, MC, SF
+      lName.str("");
+      lName << lFileName[iWP] << "_" << lDataType[iData] << ".txt";
+      std::ofstream lOut(lName.str().c_str());
+      
+      for (unsigned iEta(0); iEta<3; ++iEta){//loop on eta bin
+	
+	const unsigned nBins = hist_muon[iWP][iEta][iData]->GetN();
+	for (unsigned iPt(0); iPt<nBins; ++iPt){//loop on pT bins
+	  double x = 0;
+	  double y = 0;
+	  hist_muon[iWP][iEta][iData]->GetPoint(iPt,x,y);
+	  double xMax = x+hist_muon[iWP][iEta][iData]->GetErrorXhigh(iPt);
+	  double xMin = x-hist_muon[iWP][iEta][iData]->GetErrorXlow(iPt);
+	  double yErrPlus = hist_muon[iWP][iEta][iData]->GetErrorYhigh(iPt);
+	  double yErrMinus = hist_muon[iWP][iEta][iData]->GetErrorYlow(iPt);
+	  
+	  lOut << xMin << " " << xMax << " " << etaMin[iEta] << " " << etaMax[iEta] << " " << y << " " << yErrMinus << " " << yErrPlus << std::endl;
+	}//loop on pT bins
+	
+      }//loop on eta bin
+      
+      lOut.close();
+    }//loop on data type
+    
+  }//loop on WP
+  
+  
+  return 1;
+}//
---
+++ Analysis/HiggsNuNu/data/scale_factors/makeParkedEleSyst.C
@@ -0,0 +1,224 @@
+#include "TFile.h"
+#include "TGraphAsymmErrors.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <set>
+#include <iomanip>
+
+
+
+void findElectronPtEtaBin(const double pt, const double eta, unsigned & ptBin, unsigned & etaBin){
+  
+  if (fabs(eta) < 0.8) etaBin=0;
+  else if (fabs(eta) < 1.442) etaBin=1;
+  else if (fabs(eta) < 1.556) etaBin=2;
+  else if (fabs(eta) < 2.0) etaBin=3;
+  else etaBin=4;
+  if (pt<15) ptBin=0;
+  else if (pt<20) ptBin=1;
+  else if (pt<30) ptBin=2;
+  else if (pt<40) ptBin=3;
+  else if (pt<50) ptBin=4;
+  else ptBin=5;
+    
+}
+
+int makeParkedEleSyst(){//main
+
+  bool doTight = true;
+
+  std::ifstream lInput;
+  if (doTight) lInput.open("ele_tight_id.txt");
+  else lInput.open("ele_veto_id_data_eff.txt");
+  if(!lInput.is_open()){
+    std::cerr << "Unable to open file. " << std::endl;
+    return 1;
+  }
+
+  std::ofstream lOutput;
+  if (!doTight) lOutput.open("ele_veto_id_data_eff_with_syst.txt");
+  else lOutput.open("ele_tight_id_with_syst.txt");
+ 
+  double syst[5][6];
+//   if (doTight) {
+//     //eta 0-0.8
+//     syst[0][0] = 0.038//0.042;
+//     syst[0][1] = 0.036//0.044;
+//       syst[0][2] = 0.009//0.002;
+//       syst[0][3] = 0.002//0.002;
+//       syst[0][4] = 0.001//0.001;
+//       syst[0][5] = 0.003//0.001;
+//     //eta 0.8-1.442
+//       syst[1][0] = 0.043//0.024;
+//       syst[1][1] = 0.034//0.008;
+//       syst[1][2] = //0.005;
+//       syst[1][3] = //0.005;
+//       syst[1][4] = //0.001;
+//       syst[1][5] = //0.004;
+//     //eta 1.442-1.556
+//       syst[2][0] = //0.203;
+//       syst[2][1] = //0.049;
+//       syst[2][2] = //0.003;
+//       syst[2][3] = //0.002;
+//       syst[2][4] = //0.001;
+//       syst[2][5] = //0.004;
+//     //eta 1.556-2.0
+//       syst[3][0] = //0.031;
+//       syst[3][1] = //0.019;
+//       syst[3][2] = //0.008;
+//       syst[3][3] = //0.002;
+//       syst[3][4] = //0.001;
+//       syst[3][5] = //0.001;
+//     //eta 2.0-2.5
+//       syst[4][0] = //0.027;
+//       syst[4][1] = //0.037;
+//       syst[4][2] = //0.006;
+//       syst[4][3] = //0.001;
+//       syst[4][4] = //0.002;
+//       syst[4][5] = //0.001;
+//   }
+//   else {
+//     //this is the systematics on the ratio data/MC, 
+//     //applied as absolute value on eff(data)
+//     //eta 0-0.8
+//     syst[0][0] = //0.033;
+//       syst[0][1] = //0.024;
+//       syst[0][2] = //0.003;
+//       syst[0][3] = //0.002;
+//       syst[0][4] = //0.001;
+//       syst[0][5] = //0.001;
+//     //eta 0.8-1.442
+//       syst[1][0] = //0.062;
+//       syst[1][1] = //0.023;
+//       syst[1][2] = //0.007;
+//       syst[1][3] = //0.001;
+//       syst[1][4] = //0.004;
+//       syst[1][5] = //0.001;
+//     //eta 1.442-1.556
+//       syst[2][0] = //0.131;
+//       syst[2][1] = //0.079;
+//       syst[2][2] = //0.011;
+//       syst[2][3] = //0.002;
+//       syst[2][4] = //0.004;
+//       syst[2][5] = //0.003;
+//     //eta 1.556-2.0
+//       syst[3][0] = //0.052;
+//       syst[3][1] = //0.036;
+//       syst[3][2] = //0.003;
+//       syst[3][3] = //0.001;
+//       syst[3][4] = //0.001;
+//       syst[3][5] = //0.001;
+//     //eta 2.0-2.5
+//       syst[4][0] = //0.051;
+//       syst[4][1] = //0.020;
+//       syst[4][2] = //0.003;
+//       syst[4][3] = //0.001;
+//       syst[4][4] = //0.001;
+//       syst[4][5] = //0.001;
+//   }
+//relative error on SF nb this is used as the error on the data eff for loose
+    //eta 0-0.8
+  syst[0][0] = 0.11;
+  syst[0][1] = 0.069;
+  syst[0][2] = 0.014;
+  syst[0][3] = 0.0028;
+  syst[0][4] = 0.0014;
+  syst[0][5] = 0.0041;
+    //eta 0.8-1.442
+  syst[1][0] = 0.11;
+  syst[1][1] = 0.069;
+  syst[1][2] = 0.014;
+  syst[1][3] = 0.0028;
+  syst[1][4] = 0.0014;
+  syst[1][5] = 0.0041;
+    //eta 1.442-1.556
+  syst[2][0] = 0.11;
+  syst[2][1] = 0.083;
+  syst[2][2] = 0.057;
+  syst[2][3] = 0.024;
+  syst[2][4] = 0.0028;
+  syst[2][5] = 0.0043;
+    //eta 1.556-2.0
+  syst[3][0] = 0.12;
+  syst[3][1] = 0.04;
+  syst[3][2] = 0.022;
+  syst[3][3] = 0.0059;
+  syst[3][4] = 0.003;
+  syst[3][5] = 0.0053;
+    //eta 2.0-2.5
+  syst[4][0] = 0.12;
+  syst[4][1] = 0.04;
+  syst[4][2] = 0.022;
+  syst[4][3] = 0.0059;
+  syst[4][4] = 0.003;
+  syst[4][5] = 0.0053;
+
+
+  while(1){
+    double pTmin = 0;
+    double pTmax = 0;
+    double etaMin = 0;
+    double etaMax = 0;
+    double SF = 0;
+    double SFerrPlus = 0;
+    double SFerrMinus = 0;
+    lInput>>pTmin>>pTmax>>etaMin>>etaMax>>SF>>SFerrMinus>>SFerrPlus;
+    //protect against blank line at the end of the file
+    
+    std::cout << pTmin << "-" << pTmax << " "
+	      << etaMin << "-" << etaMax << " "
+	      << SF << " "
+	      << SFerrPlus << " (" 
+	      << SFerrPlus/SF*100 << "%) " ;
+
+    unsigned etaBin=0;
+    unsigned pTbin=0;
+    findElectronPtEtaBin((pTmin+pTmax)/2.,(etaMin+etaMax)/2.,pTbin,etaBin);
+
+    //std::cout << "[" << syst[etaBin][pTbin] << " (" 
+    //<< syst[etaBin][pTbin]/SF*100 << "%)] " ;
+    //SF=a/b, dSF/SF=da/a ++ db/b
+    //SFveto=(1-a)/(1-b), dSFV/SFV=da/(1-a) ++ db(1-b)
+    //dSFV/SFV=da/a*a/(1-a) ++ db/b*b/(1-b)
+    //using the approximation a/1-a ~ b/1-b
+    //dSFV/SFV=a/(1-a) * dSF/SF
+    //so dSFV=b/(1-b)*dSF
+    //transferring everything to the numerator 
+    //d(1-a) = da = dSF*a/(1-a)
+    if (!doTight && SF != 1) syst[etaBin][pTbin] = syst[etaBin][pTbin]*SF/(1-SF);
+
+    std::cout << syst[etaBin][pTbin] << " (" 
+	      << syst[etaBin][pTbin]/SF*100 << "%) " ;
+
+    SFerrPlus = sqrt(pow(SFerrPlus,2)+pow(syst[etaBin][pTbin]*SF,2));
+    SFerrMinus = sqrt(pow(SFerrMinus,2)+pow(syst[etaBin][pTbin]*SF,2));
+
+    std::cout << SFerrPlus << " (" 
+	      << SFerrPlus/SF*100 << "%)"  
+	      << std::endl ;
+
+
+    if (pTmin > 1) lOutput //<< std::setprecision(2)
+      << static_cast<unsigned>(pTmin*1000+0.5)/1000. << " " 
+			   << static_cast<unsigned>(pTmax*1000+0.5)/1000.  << " " 
+			   << static_cast<unsigned>(etaMin*1000+0.5)/1000.  << " " 
+			   << static_cast<unsigned>(etaMax*1000+0.5)/1000.  << " " 
+			   << static_cast<unsigned>(SF*1000+0.5)/1000.  << " " 
+			   << static_cast<unsigned>(SFerrMinus*1000+0.5)/1000.  << " " 
+			   << static_cast<unsigned>(SFerrPlus*1000+0.5)/1000.
+			   << std::endl;
+
+
+    if(lInput.eof()){
+      break; 
+    }
+  }
+
+  lInput.close();
+  lOutput.close();
+
+  return 0;
+
+
+}
