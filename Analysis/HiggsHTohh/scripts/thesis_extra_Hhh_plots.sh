6574e886d1fc5c7bf29cfe67fcf972f26797cc4e
Rebecca Lane <rcl11@imperial.ac.uk>
2015-03-02 18:33:14 +0100

    correction to thesis plots

--- Analysis/HiggsHTohh/scripts/thesis_extra_Hhh_plots.sh
+++ Analysis/HiggsHTohh/scripts/thesis_extra_Hhh_plots.sh
@@ -15,9 +15,10 @@ MT_INC_SHIFT="  --draw_error_band=true --syst_tau_scale=CMS_scale_t_mutau_8TeV -
 ./bin/PlotCompare -p "M-H350:350GeV:datacard_m_H_hh_2jet2tagSFMassCuts_mt_2012.root:muTau_2jet2tagSFMassCuts:ggHTohhTo2Tau2B350:-1:2:604" -p "M-H300:300GeV:datacard_m_H_hh_2jet2tagSFMassCuts_mt_2012.root:muTau_2jet2tagSFMassCuts:ggHTohhTo2Tau2B300:-1:2:4" -p "M-H260:260GeV:datacard_m_H_hh_2jet2tagSFMassCuts_mt_2012.root:muTau_2jet2tagSFMassCuts:ggHTohhTo2Tau2B260:-1:2:868"  --norm_mode=0 --big_label="#mu#tau_{h}" --outname="m_H_kinfit_signalmasses_2jet2tagSFMassCuts_mt" --x_axis_title="m_{H}^{kinfit} [GeV]" --norm_bins=true  --custom_y_axis_min=true --y_axis_min=0.0 --custom_x_axis_range=true --x_axis_max=499.9
 
 
-./bin/HiggsHTohhPlot --cfg=scripts/new_plot_mssm_2012.cfg --channel=mt   --method=20 --cat="2jet1tagSFMassCuts" --set_alias="sel:1" --var="mt_1(25,0,200)"   --x_axis_label="m_{T} [GeV]" --datacard="2jet1tagSFMassCuts" --extra_pad=1.5 --background_scheme="mt_with_zmm" --draw_error_band=true --auto_error_band=0.00001
+./bin/HiggsHTohhPlot --cfg=scripts/new_plot_mssm_2012.cfg --channel=mt   --method=15 --cat="2jet1tagSFMassCuts" --set_alias="sel:1" --var="mt_1(25,0,200)"   --x_axis_label="m_{T} [GeV]" --datacard="2jet1tagSFMassCuts" --extra_pad=1.5 --background_scheme="mt_with_zmm" --draw_error_band=true --auto_error_band=0.00001
 
-./bin/HiggsHTohhPlot --cfg=scripts/new_plot_mssm_2012.cfg --channel=mt   --method=16 --cat="2jet2tagSFMassCuts" --set_alias="sel:1" --var="mt_1(22,0,180)"   --x_axis_label="m_{T} [GeV]" --datacard="2jet2tagSFMassCuts" --extra_pad=1.5 --background_scheme="mt_with_zmm" --draw_error_band=true --auto_error_band=0.00001
+./bin/HiggsHTohhPlot --cfg=scripts/new_plot_mssm_2012.cfg --channel=mt   --method=16 --cat="2jet2tagSFMassCuts" --set_alias="sel:1" --var="mt_1(25,0,200)"   --x_axis_label="m_{T} [GeV]" --datacard="2jet2tagSFMassCuts" --extra_pad=1.5 --background_scheme="mt_with_zmm" --draw_error_band=true --auto_error_band=0.00001
+
 
 
 ./bin/HiggsHTohhPlot --cfg=scripts/new_plot_mssm_2012.cfg --channel=mt --set_alias="sel:mt_1<30."   --method=8 --cat="2jet0tagMassCuts" --var="n_prebjets_SF(7,-0.5,6.5)"  --x_axis_label="Number of b-jets after b-tag SF"  --log_y=true --datacard="2jet0tagMassCuts"   --background_scheme="mt_with_zmm" --custom_y_axis_min=true --y_axis_min=0.0099 --extra_pad=100 --blind=true --x_blind_min=-0.5 --x_blind_max=5.5
