06afb6a339546d3ac6bff84a59a3312e515ff1b5
Rebecca Lane <rcl11@imperial.ac.uk>
2015-03-31 14:56:30 +0200

    Saving script to run embedded ttbar jobs

---
+++ Analysis/HiggsTauTau/scripts/Embedded_MC_2012.sh
@@ -0,0 +1,4 @@
+eval ./bin/HiggsTauTau --cfg=scripts/Paper_2012.cfg --is_data=0 --input_prefix=root://eoscms//eos/cms/store/user/rlane/httskims//June6/MC_53X/ --tau_scale_mode=0 --filelist=filelists/June6_MC_53X_Embedded-ETau-TTJets_FullLeptMGDecays_et_skim.dat --channel=et  --is_embedded=true --output_name=Embedded-TTJets_FullLeptMGDecays_et_2012.root
+
+eval ./bin/HiggsTauTau --cfg=scripts/Paper_2012.cfg --is_data=0 --input_prefix=root://eoscms//eos/cms/store/user/rlane/httskims//June6/MC_53X/ --tau_scale_mode=0 --filelist=filelists/June6_MC_53X_Embedded-MTau-TTJets_FullLeptMGDecays_mt_skim.dat --channel=mt  --is_embedded=true --output_name=Embedded-TTJets_FullLeptMGDecays_mt_2012.root
+
