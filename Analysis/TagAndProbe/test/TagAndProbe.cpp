bc64936a79b0add5dd4267b7332fd66c6fa13431
Rebecca Lane <rcl11@imperial.ac.uk>
2015-04-24 12:33:55 +0200

    Renaming data/ to input/ in TagAndProbe directory

--- Analysis/TagAndProbe/test/TagAndProbe.cpp
+++ Analysis/TagAndProbe/test/TagAndProbe.cpp
@@ -219,19 +219,19 @@ int main(int argc, char* argv[]){
         }
     }
   
-  std::string data_pu_path = "data/pileup/Data_Pileup_2011_HCP-500bins.root";
-  std::string mc_pu_path = "data/pileup/MC_Fall11_PU_S6-500bins.root";
+  std::string data_pu_path = "input/pileup/Data_Pileup_2011_HCP-500bins.root";
+  std::string mc_pu_path = "input/pileup/MC_Fall11_PU_S6-500bins.root";
   if (era == "2012A" || era=="2012B" || era=="2012")
   {
-      data_pu_path = "data/pileup/Data_Pileup_2012_ReReco-600bins.root";
-      mc_pu_path = "data/pileup/MC_Summer12_PU_S10-600bins.root";
+      data_pu_path = "input/pileup/Data_Pileup_2012_ReReco-600bins.root";
+      mc_pu_path = "input/pileup/MC_Summer12_PU_S10-600bins.root";
   }
   TH1D data_pu  = GetFromTFile<TH1D>(data_pu_path, "/", "pileup");
   TH1D mc_pu    = GetFromTFile<TH1D>(mc_pu_path, "/", "pileup");
  
   std::string data_json;
-  if (era == "2012A" || era == "2012B" || era == "2012C" || era == "2012D") data_json = "data/json/data_2012_moriond.txt" ;
-  else data_json           =  "data/json/json_data_2011.txt";
+  if (era == "2012A" || era == "2012B" || era == "2012C" || era == "2012D") data_json = "input/json/data_2012_moriond.txt" ;
+  else data_json           =  "input/json/json_data_2011.txt";
 
 //==============================================================================================
   // Create analysis object
