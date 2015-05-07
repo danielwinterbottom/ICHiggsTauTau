578f78213efa63fddf4f7b7ac6976e83affd2054
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-05-07 14:48:37 +0200

    Removing MVAMEtPairProducer from this repo, adding full new recipe cfg and example crab3 cfg

---
+++ test/crab_miniaod.py
@@ -0,0 +1,18 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName= 'May6_MINIAOD'
+config.General.workArea = 'crab_projects'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'miniaod_cfg_new_recipes.py'
+config.JobType.pyCfgParams = ['isData=0','release=72XMINIAOD']
+
+config.Data.inputDataset='/GluGluToHToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.splitting='FileBased'
+config.Data.unitsPerJob =10
+
+config.Data.outLFN='/store/user/adewit/May6_MINIAOD/'
+config.Data.publication=False
+config.Data.publishDataName='Crab3_test'
+config.Site.storageSite='T2_UK_London_IC'
