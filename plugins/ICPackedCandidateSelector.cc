e4ebc384184279f66625cefe852807bfddab3471
Patrick <pdunne@cern.ch>
2015-05-12 19:17:22 +0100

    move major version check to stop compilation bug

--- plugins/ICPackedCandidateSelector.cc
+++ plugins/ICPackedCandidateSelector.cc
@@ -1,7 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "UserCode/ICHiggsTauTau/plugins/ICPackedCandidateSelector.hh"
 
-using namespace pat;
 #if CMSSW_MAJOR_VERSION >= 7
+using namespace pat;
 DEFINE_FWK_MODULE(PATPackedCandidateSelector);
 #endif
