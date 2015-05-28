16bb33799fbc6631f3031cd73e24a42a0262d4cd
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-05-28 18:15:51 +0200

    Condensed version of seg faulting code

---
+++ Analysis/HiggsTauTau/interface/HTTSequenceCrash.h
@@ -0,0 +1,49 @@
+#ifndef ICHiggsTauTau_HiggsTauTau_HTTSequenceCrash_h
+#define ICHiggsTauTau_HiggsTauTau_HTTSequenceCrash_h
+
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <iostream>
+#include "Core/interface/ModuleBase.h"
+#include "Utilities/interface/json.h"
+#include "HiggsTauTau/interface/HTTConfig.h"
+#include "PhysicsTools/FWLite/interface/TFileService.h"
+
+namespace ic {
+
+class HTTSequenceCrash {
+ private:
+ fwlite::TFileService *fs;
+ std::vector<std::shared_ptr<ic::ModuleBase>> *seq = new std::vector<std::shared_ptr<ic::ModuleBase>>;
+ //std::vector<std::shared_ptr<ic::ModuleBase>> *seq = new std::vector<std::shared_ptr<ic::ModuleBase>>;
+ std::string channel_str;
+ Json::Value js;
+ 
+ public:
+  typedef std::vector<std::shared_ptr<ic::ModuleBase>> ModuleSequence;
+  HTTSequenceCrash(std::string& chan,std::string& var, Json::Value const& js);
+  ~HTTSequenceCrash();
+  ModuleSequence* getSequence(){return seq;}
+  void BuildSequence();
+  void BuildETPairs();
+  void BuildMTPairs();
+  void BuildEMPairs();
+
+  void BuildTauSelection();
+
+  void BuildDiElecVeto();
+  void BuildDiMuonVeto();
+
+  void BuildExtraMuonVeto();
+  void BuildExtraElecVeto();
+
+  template<class T>
+  void BuildModule(T const& mod) {
+     seq->push_back(std::shared_ptr<ModuleBase>(new T(mod)));
+  }
+};
+}
+
+#endif
