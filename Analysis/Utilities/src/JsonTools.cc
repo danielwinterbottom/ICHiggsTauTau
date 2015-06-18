f33e6da8afe75e7f020d831ba2b26efaa81e2e82
Adinda de Wit <amd12@ic.ac.uk>
2015-06-18 15:48:35 +0100

    Indentation...

--- Analysis/Utilities/src/JsonTools.cc
+++ Analysis/Utilities/src/JsonTools.cc
@@ -29,79 +29,79 @@ Json::Value ExtractJsonFromFlatString(std::string const&str){
   boost::split(split_str,str,boost::is_any_of(":"));
   boost::split(split_lastarg,split_str.at(split_str.size()-1),boost::is_any_of("^"));
   std::string jsonstr = "{\"";
-	if(split_lastarg.size()==1){
-  	for(unsigned i = 0; i<split_str.size()-1;i++){
-		 if(i==split_str.size()-2){
-  	  jsonstr = jsonstr+split_str.at(i)+"\":";
-  	 } else jsonstr = jsonstr+split_str.at(i)+"\":{\"";
-		}
-		bool is_num = true;
+  if(split_lastarg.size()==1){
+    for(unsigned i = 0; i<split_str.size()-1;i++){
+      if(i==split_str.size()-2){
+        jsonstr = jsonstr+split_str.at(i)+"\":";
+      } else jsonstr = jsonstr+split_str.at(i)+"\":{\"";
+    }
+    bool is_num = true;
     try{
-		  boost::lexical_cast<double>(split_str.at(split_str.size()-1));
-	  }
-		catch(boost::bad_lexical_cast& e){
-		  is_num = false;
-		}
-		bool is_bool=false;
+      boost::lexical_cast<double>(split_str.at(split_str.size()-1));
+    }
+    catch(boost::bad_lexical_cast& e){
+      is_num = false;
+    }
+    bool is_bool=false;
     if(strcmp((split_str.at(split_str.size()-1)).c_str(),"false")==0||strcmp((split_str.at(split_str.size()-1)).c_str(),"true")==0){
-		 is_bool = true;
-		}
-		if(is_bool || is_num){
-		 jsonstr = jsonstr+split_str.at(split_str.size()-1)+std::string(split_str.size()-1,'}');
-		} else {
-		  if(strcmp(split_str.at(split_str.size()-1).c_str(),"")==0){
-		   jsonstr = jsonstr+"[]"+std::string(split_str.size()-1,'}');
-			} else {
-		   jsonstr = jsonstr+"\""+split_str.at(split_str.size()-1)+"\""+std::string(split_str.size()-1,'}');
-			 }
-		}
+     is_bool = true;
+    }
+    if(is_bool || is_num){
+      jsonstr = jsonstr+split_str.at(split_str.size()-1)+std::string(split_str.size()-1,'}');
+    } else {
+      if(strcmp(split_str.at(split_str.size()-1).c_str(),"")==0){
+        jsonstr = jsonstr+"[]"+std::string(split_str.size()-1,'}');
+      } else {
+       jsonstr = jsonstr+"\""+split_str.at(split_str.size()-1)+"\""+std::string(split_str.size()-1,'}');
+     }
+   }
 
 		
 	} else {
-  	for(unsigned i = 0; i<split_str.size()-1;i++){
-		 if(i==split_str.size()-2){
-  	  jsonstr = jsonstr+split_str.at(i)+"\":[";
-  	 } else jsonstr = jsonstr+split_str.at(i)+"\":{\"";
-		}
-		bool is_num = true;
-		bool are_all_num = true;
-		for(unsigned k = 0; k<split_lastarg.size(); k++){
-		 if(strcmp((split_lastarg.at(k)).c_str(),"")!=0){
+   for(unsigned i = 0; i<split_str.size()-1;i++){
+    if(i==split_str.size()-2){
+      jsonstr = jsonstr+split_str.at(i)+"\":[";
+     } else jsonstr = jsonstr+split_str.at(i)+"\":{\"";
+   }
+    bool is_num = true;
+    bool are_all_num = true;
+    for(unsigned k = 0; k<split_lastarg.size(); k++){
+      if(strcmp((split_lastarg.at(k)).c_str(),"")!=0){
         try{
-		      boost::lexical_cast<double>(split_lastarg.at(k));
-	      }
-		    catch(boost::bad_lexical_cast& e){
-		     is_num = false;
-		   }
+          boost::lexical_cast<double>(split_lastarg.at(k));
+        }
+        catch(boost::bad_lexical_cast& e){
+          is_num = false;
+        }
        are_all_num = are_all_num&&is_num;
-		 }
-		}
-		bool is_bool=false;
-		bool are_all_bool = true;
+     }
+    }
+    bool is_bool=false;
+    bool are_all_bool = true;
     for( unsigned k = 0; k<split_lastarg.size();k++){
-		 if(strcmp((split_lastarg.at(k)).c_str(),"")!=0){
-      if(strcmp((split_lastarg.at(k)).c_str(),"false")==0||strcmp((split_lastarg.at(k)).c_str(),"true")==0){
-  	  	is_bool = true;
-			}
-		  are_all_bool = are_all_bool&&is_bool;
-		 }
+      if(strcmp((split_lastarg.at(k)).c_str(),"")!=0){
+        if(strcmp((split_lastarg.at(k)).c_str(),"false")==0||strcmp((split_lastarg.at(k)).c_str(),"true")==0){
+          is_bool = true;
+        }
+        are_all_bool = are_all_bool&&is_bool;
+      }
 	  }
-		if(are_all_bool || are_all_num){
-		  for(unsigned k = 0; k<split_lastarg.size()-1;k++){ 
+    if(are_all_bool || are_all_num){
+      for(unsigned k = 0; k<split_lastarg.size()-1;k++){ 
         if(strcmp("",split_lastarg.at(k).c_str())!=0){
-  		   jsonstr = jsonstr+split_lastarg.at(k)+",";
-			  }
-			} 
-  	  jsonstr=jsonstr+split_lastarg.at(split_lastarg.size()-1)+"]"+std::string(split_str.size()-1,'}');
-		} else {
-		  for(unsigned k = 0; k<split_lastarg.size()-1;k++){ 
-        if(strcmp("",split_lastarg.at(k).c_str())!=0){
-			    jsonstr = jsonstr+"\""+split_lastarg.at(k)+"\",";
-				}
-			} 
-			jsonstr=jsonstr+"\""+split_lastarg.at(split_lastarg.size()-1)+"\"]"+std::string(split_str.size()-1,'}');
-		}
-	}
+          jsonstr = jsonstr+split_lastarg.at(k)+",";
+        }
+      } 
+      jsonstr=jsonstr+split_lastarg.at(split_lastarg.size()-1)+"]"+std::string(split_str.size()-1,'}');
+     } else {
+     for(unsigned k = 0; k<split_lastarg.size()-1;k++){ 
+       if(strcmp("",split_lastarg.at(k).c_str())!=0){
+         jsonstr = jsonstr+"\""+split_lastarg.at(k)+"\",";
+       }
+      } 
+      jsonstr=jsonstr+"\""+split_lastarg.at(split_lastarg.size()-1)+"\"]"+std::string(split_str.size()-1,'}');
+    }
+  }
 
   Json::Value js;
   Json::Reader reader(Json::Features::all());
