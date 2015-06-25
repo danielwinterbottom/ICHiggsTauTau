1a684dfef36fbdeebf17360b4a792e1549adbc84
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-06-25 21:48:38 +0200

    Including correct header file

--- plugins/ICLeptonIsolation.cc
+++ plugins/ICLeptonIsolation.cc
@@ -1,4 +1,4 @@
-#include "UserCode/ICHiggsTauTau/plugins/ICLeptonIsolation.h"
+#include "UserCode/ICHiggsTauTau/plugins/ICLeptonIsolation.hh"
 #include <vector>
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
