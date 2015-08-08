7e90386631fbf4c51e625113431febf575c21a9d
Patrick <pdunne@cern.ch>
2015-08-08 11:03:22 +0100

    adding macro for sasha check

--- Analysis/HiggsNuNu/macros/SashaCheck.C
+++ Analysis/HiggsNuNu/macros/SashaCheck.C
@@ -26,44 +26,43 @@
 
 int SashaCheck(){
 
-  double btagthreshold=0.679;
-  //double btagthreshold=0.898;
+  //Define thresholds
+  //double btagthreshold=0.679;
+  double btagthreshold=0.898;
   double centraljetthreshold=30;
   double forwardjetthreshold=30;
 
-  std::string outfolder="output_sashacheck_csvmedium";
+  std::string outfolder="./";
 
-  std::string lMuCut = "mu1_pt>25&&mu2_pt>25&&(mu1_charge*mu2_charge==-1)";
+  //Define cuts
+  std::string lMuCut = "mu1_pt>25&&mu2_pt>25&&(mu1_charge*mu2_charge==-1)&&(passparkedtrigger1==1||passparkedtrigger2==1)";
 
   std::string lAnyCentralCut="(((jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_pt>30)||((jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_pt>30)||((jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_pt>30)||((jet4_eta<2.4&&jet4_eta>-2.4)&&jet4_pt>30))";
   std::string lCentralBCut="(((jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_pt>30&&jet1_csv>"+boost::lexical_cast<std::string>(btagthreshold)+")||((jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_pt>30&&jet2_csv>"+boost::lexical_cast<std::string>(btagthreshold)+")||((jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_pt>30&&jet3_csv>"+boost::lexical_cast<std::string>(btagthreshold)+")||((jet4_eta<2.4&&jet4_eta>-2.4)&&jet4_pt>30&&jet4_csv>"+boost::lexical_cast<std::string>(btagthreshold)+"))";
 
   std::string lCentral1BCut="((jet1_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_csv>"+boost::lexical_cast<std::string>(btagthreshold)+"&&!((jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet4_eta<2.4&&jet4_eta>-2.4)&&jet4_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"))||(jet2_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_csv>"+boost::lexical_cast<std::string>(btagthreshold)+"&&!((jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet4_eta<2.4&&jet4_eta>-2.4)&&jet4_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"))||(jet3_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_csv>"+boost::lexical_cast<std::string>(btagthreshold)+"&&!((jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet4_eta<2.4&&jet4_eta>-2.4)&&jet4_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"))||(jet4_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet4_eta<2.4&&jet4_eta>-2.4)&&jet4_csv>"+boost::lexical_cast<std::string>(btagthreshold)+"&&!((jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")&&!((jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+")))";
     
-  //std::string lCentralCut="((jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_csv>"+boost::lexical_cast<std::string>(btagthreshold)+")";
-  //std::string lCentralCut="((jet1_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet1_eta<2.4&&jet1_eta>-2.4)&&jet1_csv>"+boost::lexical_cast<std::string>(btagthreshold)+")||(jet2_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet2_eta<2.4&&jet2_eta>-2.4)&&jet2_csv>"+boost::lexical_cast<std::string>(btagthreshold)+")||(jet3_pt>"+boost::lexical_cast<std::string>(centraljetthreshold)+"&&(jet3_eta<2.4&&jet3_eta>-2.4)&&jet3_csv>"+boost::lexical_cast<std::string>(btagthreshold)+"))";
   std::string lForwardCut="(((jet1_eta>=2.4||jet1_eta<=-2.4)&&jet1_pt>"+boost::lexical_cast<std::string>(forwardjetthreshold)+")||((jet2_eta>=2.4||jet2_eta<=-2.4)&&jet2_pt>"+boost::lexical_cast<std::string>(forwardjetthreshold)+")||((jet3_eta>=2.4||jet3_eta<=-2.4)&&jet3_pt>"+boost::lexical_cast<std::string>(forwardjetthreshold)+")||((jet4_eta>=2.4||jet4_eta<=-2.4)&&jet4_pt>"+boost::lexical_cast<std::string>(forwardjetthreshold)+"))";
 
   std::string lMassWindowCut = "(m_mumu>26&&m_mumu<32)";
 
   std::string histName = "m_mumu";
 
-  //TFile* fS = TFile::Open("allsinglemu.root");
-  //  TTree *tree = (TTree*)gDirectory->Get("LightTree");
+  //Define files to read
   TChain *tree = new TChain("SashaLightTree");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012A-22Jan2013-v1.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split1.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split2.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split3.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split4.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split1.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split2.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split3.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split4.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split1.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split2.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split3.root");
-  tree->Add("output_lighttree_singlemuforsashacrosschecksashaonly_150715/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split4.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012A-22Jan2013-v1.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split1.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split2.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split3.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012B-22Jan2013-v1-split4.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split1.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split2.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split3.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012C-22Jan2013-v1-split4.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split1.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split2.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split3.root");
+  tree->Add("sashachecks/output_lighttree_singlemuforsashacrosschecksashaonly_070815/SINGLEMU_SingleMu-2012D-22Jan2013-v1-split4.root");
 
   
 
@@ -74,7 +73,8 @@ int SashaCheck(){
 
   std::vector<std::string> cuts;
   std::vector<std::string> cutname;
-
+  
+  //Set up cut steps
   std::string fullcut;
   fullcut=lMuCut;
   cuts.push_back(fullcut);
@@ -98,37 +98,13 @@ int SashaCheck(){
   std::ostringstream var;
   var << histName << ">>htmp";
 
+  //Loop over cut steps and get m_mumu plot and integral at each step
   for(unsigned iCut=0;iCut<cuts.size();iCut++){
     
     TH1F* htmp = new TH1F("htmp","",29,12,70);
-    //tree->Draw(var.str().c_str(),(lCut+"&&"+lForwardCut).c_str());
     std::cout<<"getting histogram"<<std::endl;
     tree->Draw(var.str().c_str(),cuts[iCut].c_str());
 
-    //get event list
-    // std::cout<<"getting event list"<<std::endl;
-    // TEntryList *list=(TEntryList*)gDirectory->Get("evtlist");
-    // tree->SetEntryList(list);
-    // TFile *tmpfile=new TFile("tmp.root","RECREATE");
-    // TTree *small = tree->CopyTree("");
-    //   tree->SetEventList(0);
-    //   unsigned run,lumi,event;
-    //   small->SetBranchAddress("run",&run);
-    //   small->SetBranchAddress("lumi",&lumi);
-    //   small->SetBranchAddress("event",&event);
-    //   int nentries=small->GetEntries();
-      
-    //   std::cout<<"printing event list"<<std::endl;
-    //   ofstream myfile;
-    //   myfile.open(("mumuevents"+cutname[iCut]+".txt").c_str());
-      
-    //   for(int iEvent=0;iEvent<nentries;iEvent++){
-    // 	small->GetEntry(iEvent);
-    // 	myfile<<run<<" "<<lumi<<" "<<event<<std::endl;
-    //   }
-    //   myfile.close();
-    //small->Delete();
-    //tmpfile->Delete();
     std::cout<<cutname[iCut]<<" has "<<htmp->Integral(1,htmp->GetNbinsX())<<" events left"<<std::endl;
 
     std::cout<<"drawing histogram"<<std::endl;    
@@ -143,7 +119,23 @@ int SashaCheck(){
     htmp->GetXaxis()->SetTitle("M_{#mu#mu}/GeV");
     htmp->GetYaxis()->SetTitle("Events");
     htmp->SetTitle("");
+
     
+    TF1* myfit = new TF1("myfit","gaus + [3]+[4]*x+[5]*x*x",0,70);
+    myfit->SetParName(0, "Norm");
+    myfit->SetParName(1, "Center");
+    myfit->SetParName(2, "#sigma");
+    myfit->SetParName(3, "const term");
+    myfit->SetParName(4, "linear term");
+    myfit->SetParName(5, "quad term");
+    myfit->SetParameters(100, 30., 4, 10, 1, 1);
+    
+    //Fit to distribution after requiring forward jet
+    if(cutname[iCut]=="_forwardjet"){
+      htmp->Fit("myfit");
+    }
+
+    //Draw and save
     htmp->Draw("e1lp");
     
     gStyle->SetOptStat(0);
