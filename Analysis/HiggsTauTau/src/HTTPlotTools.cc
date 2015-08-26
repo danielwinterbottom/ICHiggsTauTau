53443fb2b5391948b86ca926b0efda7b8a304da2
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-08-26 09:44:32 +0100

    Adding signal scheme for first run2 datacards

--- Analysis/HiggsTauTau/src/HTTPlotTools.cc
+++ Analysis/HiggsTauTau/src/HTTPlotTools.cc
@@ -256,6 +256,11 @@ namespace ic {
         (boost::lexical_cast<std::string>(signal_scale_)+"#times SM H("+draw_signal_mass_+" GeV)#rightarrow#tau#tau"),
         {"ggH","qqH","VH"}, TColor::GetColor(0,18,255), true)
     };
+    sig_schemes_["run2_mssm"] = {
+      PlotSigComponent("sig",
+        ("SUSYGluGluH("+draw_signal_mass_+" GeV)#rightarrow#tau#tau"),
+        {"ggH"}, TColor::GetColor(0,18,255), false)
+    };
     sig_schemes_["sm_nomult"] = {
       PlotSigComponent("sig",
         ("SM H("+draw_signal_mass_+" GeV)#rightarrow#tau#tau"),
