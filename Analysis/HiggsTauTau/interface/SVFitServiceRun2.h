073daed1ffbde85db6899f53f94c4b5cb96bf0a7
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-08-27 14:26:30 +0100

    Moving SVFit into Analysis/HiggsTauTau

--- Analysis/HiggsTauTau/interface/SVFitServiceRun2.h
+++ Analysis/HiggsTauTau/interface/SVFitServiceRun2.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include "UserCode/ICHiggsTauTau/interface/Candidate.hh"
 #include "UserCode/ICHiggsTauTau/interface/Met.hh"
-#include "TauAnalysis/SVfitStandalone/interface/SVfitStandaloneAlgorithm.h"
+#include "HiggsTauTau/SVFit/interface/SVfitStandaloneAlgorithm.h"
 
 #include "TTree.h"
 
