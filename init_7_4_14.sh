b7d4ac4d3adb176de9e70bd7e305a283ee64759d
amagnan <a.magnan@imperial.ac.uk>
2015-10-13 17:00:21 +0200

    update config and init script

---
+++ init_7_4_14.sh
@@ -0,0 +1,5 @@
+git cms-addpkg RecoMET/METPUSubtraction
+git clone https://github.com/rfriese/RecoMET-METPUSubtraction RecoMET/METPUSubtraction/data -b 74X-13TeV-Summer15-July2015
+sed 's!label = cms.string("full")!label = cms.string("full"),\n\tetaBinnedWeights=cms.bool(False)!' RecoMET/METPUSubtraction/python/mvaPFMET_cff.py >  tmp
+mv tmp RecoMET/METPUSubtraction/python/mvaPFMET_cff.py
+scramv1 b -j 6
