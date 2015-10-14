36289483b672a336efa688feccc4411964fe30de
amagnan <a.magnan@imperial.ac.uk>
2015-10-14 10:05:43 +0200

    debug 7_4_14 config + add puppi met

--- init_7_4_14.sh
+++ init_7_4_14.sh
@@ -1,5 +1,7 @@
+cd ../../
 git cms-addpkg RecoMET/METPUSubtraction
 git clone https://github.com/rfriese/RecoMET-METPUSubtraction RecoMET/METPUSubtraction/data -b 74X-13TeV-Summer15-July2015
 sed 's!label = cms.string("full")!label = cms.string("full"),\n\tetaBinnedWeights=cms.bool(False)!' RecoMET/METPUSubtraction/python/mvaPFMET_cff.py >  tmp
 mv tmp RecoMET/METPUSubtraction/python/mvaPFMET_cff.py
 scramv1 b -j 6
+cd -
