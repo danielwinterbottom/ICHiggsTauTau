64d80ef8bf3d73ea4042d2913d2f9fba85584640
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-10-21 15:31:32 +0200

    Adding an extra trigger, latest submission scripts

--- test/crab_higgstautau_spring15_miniAODv2_QCD.py
+++ test/crab_higgstautau_spring15_miniAODv2_QCD.py
@@ -2,25 +2,24 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
-config.General.workArea='Oct15_Data_74X'
-#config.General.requestName = 'May13_MC'
+config.General.workArea='Oct15_MC_74X'
 config.section_('JobType')
 config.JobType.psetName = '/afs/cern.ch/work/a/adewit/private/CMSSW_7_4_12/src/UserCode/ICHiggsTauTau/test/higgstautau_cfg_74X_Sep15.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.outputFiles = ['EventTree.root']
 #config.JobType.inputFiles = ['Summer15_V5_MC.db']
-config.JobType.pyCfgParams = ['release=7412MINIAOD','isData=1','isNLO=0', 'globalTag=74X_dataRun2_Prompt_v2']
+config.JobType.pyCfgParams = ['release=7412MINIAOD','isData=0','isNLO=0', 'globalTag=74X_mcRun2_asymptotic_v2']
 config.section_('Data')
 #config.Data.inputDataset = 'DUMMY'
 #config.Data.unitsPerJob = 30000 
 config.Data.unitsPerJob = 1
 config.Data.splitting = 'FileBased'
 config.Data.publication = False
-config.Data.ignoreLocality=False
-config.Data.outLFNDirBase='/store/user/adewit/Oct15_Data_74X/'
+#config.Data.ignoreLocality= True
+config.Data.outLFNDirBase='/store/user/adewit/Oct15_MC_74X/'
 config.section_('User')
 config.section_('Site')
-config.Site.whitelist = ['T2_UK_London_IC', 'T2_CH_CERN', 'T2_FR_GRIF_LLR', 'T2_UK_SGrid_Bristol', 'T3_US_FNALLPC', 'T2_DE_DESY', 'T2_IT_Bari', 'T2_BE_IIHE', 'T2_US_UCSD', 'T2_US_MIT', 'T2_IT_Pisa', 'T2_US_Wisconsin', 'T2_US_Florida', 'T2_IT_Rome','T2_FR_IPHC']
+config.Site.whitelist = ['T2_UK_London_IC', 'T2_CH_CERN', 'T2_FR_GRIF_LLR', 'T2_UK_SGrid_Bristol', 'T3_US_FNALLPC', 'T2_DE_DESY', 'T2_IT_Bari', 'T2_BE_IIHE', 'T2_US_UCSD', 'T2_US_MIT', 'T2_IT_Pisa', 'T2_US_Wisconsin', 'T2_US_Florida', 'T2_IT_Rome','T2_FR_IPHC','T2_US_Caltech','T2_DE_RWTH']
 config.Site.storageSite = 'T2_UK_London_IC'
 
 if __name__ == '__main__':
@@ -47,15 +46,14 @@ if __name__ == '__main__':
     #tasks.append(('SingleMuon-2015B-17Jul','/SingleMuon/Run2015B-17Jul2015-v1/MINIAOD'))
     #tasks.append(('SingleElectron-2015B-17Jul','/SingleElectron/Run2015B-17Jul2015-v1/MINIAOD'))
     #tasks.append(('MuonEG-2015B-17Jul','/MuonEG/Run2015B-17Jul2015-v1/MINIAOD'))
-    tasks.append(('Tau-2015D-promptv4','/Tau/Run2015D-PromptReco-v4/MINIAOD'))
-    tasks.append(('SingleMuon-2015D-promptv4','/SingleMuon/Run2015D-PromptReco-v4/MINIAOD'))
-    tasks.append(('SingleElectron-2015D-promptv4','/SingleElectron/Run2015D-PromptReco-v4/MINIAOD'))
-    tasks.append(('MuonEG-2015D-promptv4','/MuonEG/Run2015D-PromptReco-v4/MINIAOD'))
-    tasks.append(('Tau-2015D-prompt','/Tau/Run2015D-PromptReco-v3/MINIAOD'))
-    tasks.append(('SingleMuon-2015D-prompt','/SingleMuon/Run2015D-PromptReco-v3/MINIAOD'))
-    tasks.append(('SingleElectron-2015D-prompt','/SingleElectron/Run2015D-PromptReco-v3/MINIAOD'))
-    tasks.append(('MuonEG-2015D-prompt','/MuonEG/Run2015D-PromptReco-v3/MINIAOD'))
-
+   # tasks.append(('QCD_Ht100to200','/QCD_HT100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+   # tasks.append(('QCD_Ht200to300','/QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+    tasks.append(('QCD_Ht300to500','/QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+   # tasks.append(('QCD_Ht500to700','/QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+   # tasks.append(('QCD_Ht700to1000','/QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+   # tasks.append(('QCD_Ht1000to1500','/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+   # tasks.append(('QCD_Ht1500to2000','/QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
+    tasks.append(('QCD_Ht2000toInf','/QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'))
 
     for task in tasks:
         print task[0]
