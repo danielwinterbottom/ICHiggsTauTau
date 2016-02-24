598a20a7e4f17b7271d68234429a101d26f0e804
Joao Pela <pela@cern.ch>
2016-02-24 18:56:28 +0000

    Now sums have been defined

---
+++ interface/L1TSum.hh
@@ -0,0 +1,31 @@
+#ifndef ICHiggsTauTau_L1TSum_hh
+#define ICHiggsTauTau_L1TSum_hh
+
+#include "UserCode/ICHiggsTauTau/interface/L1TObject.hh"
+
+#include <vector>
+
+namespace ic {
+  
+  class L1TSum : public ic::L1TObject {
+  public:
+    L1TSum();
+    virtual ~L1TSum();
+    virtual void Print() const;
+
+  #ifndef SKIP_CINT_DICT
+  public:
+    ClassDef(L1TSum, 1);
+  #endif
+    
+  public:
+    short int sumType;
+    float     et;
+    float     phi;
+    
+  };
+  
+  typedef std::vector<ic::L1TSum> L1TSumCollection;
+}
+
+#endif
---
+++ src/L1TSum.cc
@@ -0,0 +1,9 @@
+#include "UserCode/ICHiggsTauTau/interface/L1TSum.hh"
+
+namespace ic {
+  L1TSum::L1TSum() {}
+  
+  L1TSum::~L1TSum() {}
+  
+  void L1TSum::Print() const {}
+}
