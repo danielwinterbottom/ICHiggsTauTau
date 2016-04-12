1326c8729740bd883fb134b8aae76cbf79679041
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2016-04-12 18:09:18 +0100

    Adding some extra yield printout in verbose mode

--- Analysis/HiggsTauTau/src/HTTRun2AnalysisTools.cc
+++ Analysis/HiggsTauTau/src/HTTRun2AnalysisTools.cc
@@ -1534,9 +1534,15 @@ push_back(sample_names_,this->ResolveSamplesAlias("data_samples"));
                                       std::string const& weight) {
     auto result = GetLumiScaledRate(samples.at(0), selection, category, weight);
     double err_sqr = result.second * result.second;
+    if(verbosity_ >0 ){
+      PrintValue(samples.at(0),result);
+    }
     if (samples.size() > 1) {
       for (unsigned i = 1; i < samples.size(); ++i) {
         auto tmp = GetLumiScaledRate(samples.at(i), selection, category, weight);
+        if(verbosity_>0){
+          PrintValue(samples.at(i),tmp);
+        }
         result.first += tmp.first;
         err_sqr += (tmp.second * tmp.second);
       }
