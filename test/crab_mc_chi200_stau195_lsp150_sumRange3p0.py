aee29f8a6204e5d8b4b6b7f6439ee8f3f9d972cb
Daniel <d.winterbottom15@imperial.ac.uk>
2016-05-26 10:12:57 +0100

    crab config files for all samples

---
+++ test/crab_mc_chi200_stau195_lsp150_sumRange3p0.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName     = 'chi200_stau195_lsp150'
+config.General.workArea        = 'L1NTuplesV2_l1t-tsg-v5-CMSSW_8_0_2_METEtaRange3p0_v1'
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+config.JobType.pluginName = 'ANALYSIS'
+config.JobType.psetName   = 'L1NTuplesv4_CMSSW_8_0_2_cfg.py'
+config.JobType.pyCfgParams = ['sample=mc','sumRange=3']
+
+datalist = ['/store/user/lpcsusyvbf/mc/r2.2015autumn/chi200_stau195_lsp150/evts_5raw_10k.root']
+config.Data.userInputFiles     = datalist
+config.Data.inputDBS         = 'global'
+config.Data.splitting        = 'FileBased'
+config.Data.unitsPerJob      = 20000
+config.Data.outLFNDirBase    = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication      = False
+config.Data.outputDatasetTag = 'L1NTuplesV2_l1t-tsg-v5-CMSSW_8_0_2_METEtaRange3p0_v1'
+
+#config.Site.whitelist   = ["T2_UK_London_IC"]
+config.Site.storageSite = 'T2_UK_London_IC'
