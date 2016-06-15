f2ab2e32ad4542dddb5fe3b5debc0ab7f3cbb1f6
Daniel <d.winterbottom15@imperial.ac.uk>
2016-06-15 14:37:35 +0100

    updeted plots script

--- Analysis/HiggsTauTau/scripts/runSMPlots2015.sh
+++ Analysis/HiggsTauTau/scripts/runSMPlots2015.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a cat=("vbf_new")
+declare -a cat=("inclusive" "vbf" "1jet_high_highhpt" "1jet_high_lowhpt" "1jet_low" "1jet_medium" "0jet_low" "0jet_medium" "0jet_high" "1jet_high_highhpt_incbveto" "1jet_high_lowhpt_incbveto" "1jet_low_incbveto" "1jet_medium_incbveto" "vbf_incbveto")
 declare -a channel=("et" "mt" "tt")
 for i in "${channel[@]}"
 do
@@ -27,5 +27,4 @@ do
     mv "m_sv_"${j}_${i}"_2015.pdf" $folder.
     mv "datacard_m_sv_"${j}_${i}"_2015.root" $folder.
   done
-done       
-#"inclusive" "vbf" "1jet_high_highhpt" "1jet_high_lowhpt" "1jet_low" "1jet_medium" "0jet_low" "0jet_medium" "0jet_high" "1jet_high_highhpt_incbveto" "1jet_high_lowhpt_incbveto" "1jet_low_incbveto" "1jet_medium_incbveto" "vbf_incbveto"
+done
