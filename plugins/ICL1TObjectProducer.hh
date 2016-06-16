5839f08a3f4287e4116ca61edfbf56b7ef413b61
Riccardo Di Maria <r.di-maria15@imperial.ac.uk>
2016-06-16 09:48:44 +0100

    renaming of variables and changing the constructSpecific

--- plugins/ICL1TObjectProducer.hh
+++ plugins/ICL1TObjectProducer.hh
@@ -11,7 +11,6 @@
 #include "FWCore/Framework/interface/EventSetup.h"                  
 #include "FWCore/Utilities/interface/InputTag.h"                    
 
-#include "DataFormats/Common/interface/ValueMap.h"                  
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 
@@ -26,6 +25,7 @@
 #include "DataFormats/L1Trigger/interface/Jet.h"
 #include "DataFormats/L1Trigger/interface/Muon.h"
 #include "DataFormats/L1Trigger/interface/EtSum.h"
+#include "DataFormats/L1Trigger/interface/BXVector.h"
 
 // system include files
 #include <memory>                                                   
@@ -48,7 +48,7 @@
 
 
 
-template <class ICL1TObj_template>
+template <class T>
 class ICL1TObjectProducer : public edm::EDProducer {
  
  public:
@@ -59,15 +59,14 @@ class ICL1TObjectProducer : public edm::EDProducer {
   virtual void beginJob();
   virtual void produce(edm::Event &, const edm::EventSetup &);
   virtual void endJob();
-  void constructSpecific(edm::Handle<ICL1TObj_template> const& candidate_collection,
-                         edm::Event& event, const edm::EventSetup& setup);
   
+  void constructSpecific(T const &src, ic::ICL1TObject &dest);
+
   edm::InputTag input_;
   std::string branch_;
-  std::string input_TObject_type_;
 
-  std::vector<ic::ICL1TObject> *ICL1TObj_;
-  edm::EDGetTokenT<ICL1TObj_template> m_EDToken_L1TObj;
+  std::vector<ic::ICL1TObject> *ic_l1t_object_;
+  edm::EDGetTokenT<T> m_EDToken_l1t_object;
   
   // output file
   edm::Service<TFileService> fs_;
@@ -80,31 +79,25 @@ class ICL1TObjectProducer : public edm::EDProducer {
 // =============================
 // Template class implementation
 // =============================
-
-
-template <class ICL1TObj_template>
-ICL1TObjectProducer<ICL1TObj_template>::ICL1TObjectProducer(const edm::ParameterSet& config)
+template <class T>
+ICL1TObjectProducer<T>::ICL1TObjectProducer(const edm::ParameterSet& config)
     : input_(config.getParameter<edm::InputTag>("input")),
-      branch_(config.getParameter<std::string>("branch")),
-      input_TObject_type_(config.getParameter<std::string>("TObject_type")) {
+      branch_(config.getParameter<std::string>("branch")) {
   
-  consumes<edm::View<ICL1TObj_template>>(input_);
-  produces<edm::ValueMap<double> >();
-  
-  ICL1TObj_ = new std::vector<ic::ICL1TObject>();
-  
+  consumes< BXVector<T> >(input_);
+
+  ic_l1t_object_ = new std::vector<ic::ICL1TObject>();
+
   PrintHeaderWithProduces(config, input_, branch_);
   
-//  PrintOptional(1, input_TObject_type_, "TObject_type");       // ???
-  
   edm::InputTag inputTag_TObject = config.getUntrackedParameter<edm::InputTag>( ("inputTag_"+branch_).c_str(), edm::InputTag("simCaloStage2Digis") );
-  m_EDToken_L1TObj = consumes<ICL1TObj_template>(inputTag_TObject);
+  m_EDToken_l1t_object = consumes<T>(inputTag_TObject);
 
 }
 
-template <class ICL1TObj_template>
-ICL1TObjectProducer<ICL1TObj_template>::~ICL1TObjectProducer() {   
-  delete ICL1TObj_;
+template <class T>
+ICL1TObjectProducer<T>::~ICL1TObjectProducer() {   
+  delete ic_l1t_object_;
 }
 
 
@@ -112,18 +105,38 @@ ICL1TObjectProducer<ICL1TObj_template>::~ICL1TObjectProducer() {
 // =============
 // Main producer
 // =============
-
-
-template <class ICL1TObj_template>
-void ICL1TObjectProducer<ICL1TObj_template>::produce(edm::Event& event, const edm::EventSetup& setup) {
+template <class T>
+void ICL1TObjectProducer<T>::produce(edm::Event& event, const edm::EventSetup& setup) {
   
-  edm::Handle<ICL1TObj_template> candidate_collection;
-  event.getByLabel(input_, candidate_collection);
+  edm::Handle< BXVector<T> > candidate_collection;
+  //edm::Handle<T > candidate_collection;
+  event.getByToken(m_EDToken_l1t_object, candidate_collection);
+  //event.getByLabel(input_, candidate_collection);  //adding something which should work with older CMSSW versions you need to use getByLabel
+  ic_l1t_object_->clear();
+  ic_l1t_object_->resize(candidate_collection->size(), ic::ICL1TObject());
+  
+  unsigned counter_objects = 0;
+  
+  if (candidate_collection.isValid()){ 
+    for (int ibx = candidate_collection->getFirstBX(); ibx <= candidate_collection->getLastBX(); ++ibx) {
 
-  constructSpecific(candidate_collection, event, setup);
-
-  event.getByToken(m_EDToken_L1TObj, candidate_collection); //????
-  ICL1TObj_->clear();
+      for (unsigned i = 0; i < candidate_collection->size(); ++i){
+        T const& src = candidate_collection->at(ibx,i);
+        ic::ICL1TObject& dest = ic_l1t_object_->at(counter_objects);
+        
+        constructSpecific(src,dest);
+         
+        ROOT::Math::PtEtaPhiEVector tempVector( src.pt(), src.eta(), src.phi(), src.energy() );
+        dest.set_vector(tempVector);
+        ic_l1t_object_->push_back(dest);
+        
+        counter_objects++;
+      }
+    }
+  } 
+  else {
+    edm::LogWarning("MissingProduct ") << " L1Upgrade " << candidate_collection << " not found. Branch will not be filled." << std::endl;
+  }
 }
 
 
@@ -131,49 +144,20 @@ void ICL1TObjectProducer<ICL1TObj_template>::produce(edm::Event& event, const ed
 // ==================
 // Specific producers
 // ==================
-template <class ICL1TObj_template>
-void ICL1TObjectProducer<ICL1TObj_template>::constructSpecific(edm::Handle<ICL1TObj_template> const& candidate_collection,
-                                                               edm::Event& event, 
-                                                               const edm::EventSetup& setup) {
+template <class T>
+void ICL1TObjectProducer<T>::constructSpecific(T const &src, ic::ICL1TObject &dest) {
 
-// common 
-
+  // common 
 }
+
 
 // ======
 // EGamma
 // ======                                                               
 template <>
-void ICL1TObjectProducer<l1t::EGammaBxCollection>::constructSpecific( edm::Handle<l1t::EGammaBxCollection> const& EGammaBxCollection_,
-                                                                      edm::Event& event, 
-                                                                      const edm::EventSetup& setup) {
-    
-    if (EGammaBxCollection_.isValid()){ 
-        for (int ibx = EGammaBxCollection_->getFirstBX(); ibx <= EGammaBxCollection_->getLastBX(); ++ibx) {
-            int i = 0;
-            for (typename l1t::EGammaBxCollection::const_iterator it=EGammaBxCollection_->begin(ibx); it!=EGammaBxCollection_->end(ibx); it++){
-                ic::ICL1TObject EGamma_;
-                
-                EGamma_.isolation = it->hwIso();  
-                
-                ROOT::Math::PtEtaPhiEVector tempVector(it->pt(), it->eta(),it->phi(), it->energy());
-                EGamma_.set_vector(tempVector);
-                ICL1TObj_->push_back(EGamma_);
-                //std::cout << i <<" "<< it.bx() << std::endl;
-                //std::cout << i <<" "<< it.pt() << std::endl;
-                ++i;
-            }
-        }
-    } 
-    else {
-        edm::LogWarning("MissingProduct ") << "L1Upgrade EGamma not found. Branch will not be filled" << std::endl;
-    }
-    
-//  edm::ValueMap<double>::Filler filler(*product);
-//  filler.insert(candidate_collection, values.begin(), values.end());
-//  filler.fill();
-    
-//  event.put(product);
+void ICL1TObjectProducer<l1t::EGamma>::constructSpecific( l1t::EGamma const &src, ic::ICL1TObject &dest ) {
+  
+  dest.setIsolation(src.hwIso());
 }                                                               
 
 
@@ -181,69 +165,16 @@ void ICL1TObjectProducer<l1t::EGammaBxCollection>::constructSpecific( edm::Handl
 // Jet
 // ===
 template <>
-void ICL1TObjectProducer<l1t::JetBxCollection>::constructSpecific( edm::Handle<l1t::JetBxCollection> const& JetBxCollection_,
-                                                                   edm::Event& event,
-                                                                   const edm::EventSetup& setup) {
-
-  if (JetBxCollection_.isValid()){ 
-      for (int ibx = JetBxCollection_->getFirstBX(); ibx <= JetBxCollection_->getLastBX(); ++ibx) {
-          int i = 0;
-          for (typename l1t::JetBxCollection::const_iterator it=JetBxCollection_->begin(ibx); it!=JetBxCollection_->end(ibx); it++){
-               ic::ICL1TObject Jet_;
-          
-               
-         
-               ROOT::Math::PtEtaPhiEVector tempVector(it->pt(), it->eta(),it->phi(), it->energy());
-               Jet_.set_vector(tempVector);
-               ICL1TObj_->push_back(Jet_);
-               ++i;
-          }
-      }
-  } 
-  else {
-      edm::LogWarning("MissingProduct") << "L1Upgrade Jet not found. Branch will not be filled" << std::endl;
-  }
- 
-//   edm::ValueMap<double>::Filler filler(*product);
-//   filler.insert(candidate_collection, values.begin(), values.end());
-//   filler.fill();
- 
- //  event.put(product);
-}                                                                                    
+void ICL1TObjectProducer<l1t::Jet>::constructSpecific( l1t::Jet const &src, ic::ICL1TObject &dest ) {}                                                                                    
 
 
 // ===
 // Tau
 // ===
 template <>
-void ICL1TObjectProducer<l1t::TauBxCollection>::constructSpecific( edm::Handle<l1t::TauBxCollection> const& TauBxCollection_,
-                                                                   edm::Event& event,
-                                                                   const edm::EventSetup& setup) {
+void ICL1TObjectProducer<l1t::Tau>::constructSpecific( l1t::Tau const &src, ic::ICL1TObject &dest ) {
 
-    if (TauBxCollection_.isValid()){ 
-        for (int ibx = TauBxCollection_->getFirstBX(); ibx <= TauBxCollection_->getLastBX(); ++ibx) {
-            int i = 0;
-            for (typename l1t::TauBxCollection::const_iterator it=TauBxCollection_->begin(ibx); it!=TauBxCollection_->end(ibx); it++){
-                ic::ICL1TObject Tau_;
-                
-                Tau_.isolation = it->hwIso();  
-        
-                ROOT::Math::PtEtaPhiEVector tempVector(it->pt(), it->eta(),it->phi(), it->energy());
-                Tau_.set_vector(tempVector);
-                ICL1TObj_->push_back(Tau_);
-                ++i;
-            }
-        }
-    } 
-    else {
-        edm::LogWarning("MissingProduct") << "L1Upgrade Tau not found. Branch will not be filled" << std::endl;
-    }
-
-//  edm::ValueMap<double>::Filler filler(*product);
-//  filler.insert(candidate_collection, values.begin(), values.end());
-//  filler.fill();
-//
-//  event.put(product);
+  dest.setIsolation(src.hwIso());  
 }                                                               
 
 
@@ -251,36 +182,11 @@ void ICL1TObjectProducer<l1t::TauBxCollection>::constructSpecific( edm::Handle<l
 // Muon                                                                
 // ====                                                                
 template <>
-void ICL1TObjectProducer<l1t::MuonBxCollection>::constructSpecific( edm::Handle<l1t::MuonBxCollection> const& MuonBxCollection_,
-                                                                    edm::Event& event, 
-                                                                    const edm::EventSetup& setup) {
-
-  if (MuonBxCollection_.isValid()){ 
-      for (int ibx = MuonBxCollection_->getFirstBX(); ibx <= MuonBxCollection_->getLastBX(); ++ibx) {
-          int i = 0;
-          for (typename l1t::MuonBxCollection::const_iterator it=MuonBxCollection_->begin(ibx); it!=MuonBxCollection_->end(ibx); it++){
-              ic::ICL1TObject Muon_;
-              
-              Muon_.isolation = it->hwIso();
-              Muon_.charge    = it->charge();
-              Muon_.quality   = it->hwQual();  
-              
-              ROOT::Math::PtEtaPhiEVector tempVector(it->pt(), it->eta(),it->phi(), it->energy());
-              Muon_.set_vector(tempVector);
-              ICL1TObj_->push_back(Muon_);
-              ++i;
-          }
-      }
-  } 
-  else {
-    edm::LogWarning("MissingProduct") << "L1Upgrade Muon not found. Branch will not be filled" << std::endl;
-  }
-
-//  edm::ValueMap<double>::Filler filler(*product);
-//  filler.insert(candidate_collection, values.begin(), values.end());
-//  filler.fill();
-//
-//  event.put(product);
+void ICL1TObjectProducer<l1t::Muon>::constructSpecific( l1t::Muon const &src, ic::ICL1TObject &dest ) {
+                
+  dest.setIsolation(src.hwIso());
+  dest.setCharge(src.charge());
+  dest.setQuality(src.hwQual());  
 }                                                               
 
 
@@ -288,49 +194,21 @@ void ICL1TObjectProducer<l1t::MuonBxCollection>::constructSpecific( edm::Handle<
 // EtSum
 // =====
 template <>
-void ICL1TObjectProducer<l1t::EtSumBxCollection>::constructSpecific( edm::Handle<l1t::EtSumBxCollection> const& EtSumBxCollection_,
-                                                                     edm::Event& event, 
-                                                                     const edm::EventSetup& setup) {
-
-  if (EtSumBxCollection_.isValid()){ 
-      for (int ibx = EtSumBxCollection_->getFirstBX(); ibx <= EtSumBxCollection_->getLastBX(); ++ibx) {
-          int i = 0;
-          for (typename l1t::EtSumBxCollection::const_iterator it=EtSumBxCollection_->begin(ibx); it!=EtSumBxCollection_->end(ibx); it++){
-              ic::ICL1TObject EtSum_;
-              
-              int type = static_cast<int>( it->getType() );
-              EtSum_.sumType = type;
-              //std::cout << type <<"    "<< std::endl;
-              
-              ROOT::Math::PtEtaPhiEVector tempVector(it->pt(), it->eta(),it->phi(), it->energy());
-              EtSum_.set_vector(tempVector);
-              ICL1TObj_->push_back(EtSum_);
-              ++i;
-          }
-      }
-  } 
-  else {
-    edm::LogWarning("MissingProduct") << "L1Upgrade EtSum not found. Branch will not be filled" << std::endl;
-  }
-
-//  edm::ValueMap<double>::Filler filler(*product);
-//  filler.insert(candidate_collection, values.begin(), values.end());
-//  filler.fill();
-//
-//  event.put(product);
-}                                                               
-
-
-
-
-
-template <class ICL1TObj_template>
-void ICL1TObjectProducer<ICL1TObj_template>::beginJob() {
-ic::StaticTree::tree_->Branch(branch_.c_str(),   &ICL1TObj_);  
+void ICL1TObjectProducer<l1t::EtSum>::constructSpecific( l1t::EtSum const &src, ic::ICL1TObject &dest ) {
+                
+                int type = static_cast<int>( src.getType() );
+                dest.setSumType(type);
+                //std::cout << dest.getSumType() <<"    "<< std::endl;
 }
 
-template <class ICL1TObj_template>
-void ICL1TObjectProducer<ICL1TObj_template>::endJob() {}                                                               
+
+template <class T>
+void ICL1TObjectProducer<T>::beginJob() {
+  ic::StaticTree::tree_->Branch( branch_.c_str(), &ic_l1t_object_ );
+}
+
+template <class T>
+void ICL1TObjectProducer<T>::endJob() {}
 
 
 #endif
