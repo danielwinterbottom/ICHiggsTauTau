c39b2ca9d6ba15f81097f3779155050757a67a55
Riccardo Di Maria <r.di-maria15@imperial.ac.uk>
2016-06-16 09:49:42 +0100

    cleaning of includes since present in the header, renaming of FWK_MODULES

--- plugins/ICL1TObjectProducer.cc
+++ plugins/ICL1TObjectProducer.cc
@@ -1,25 +1,7 @@
 #include "UserCode/ICHiggsTauTau/plugins/ICL1TObjectProducer.hh"
 
-//typedef ICL1TObjectProducer<l1t::EGammaBxCollection> type_L1TEGamma;
-//typedef ICL1TObjectProducer<l1t::JetBxCollection>    type_L1TJet;
-//typedef ICL1TObjectProducer<l1t::EtSumBxCollection>  type_L1TSum;
-//typedef ICL1TObjectProducer<l1t::MuonBxCollection>   type_L1TMuon;
-//typedef ICL1TObjectProducer<l1t::TauBxCollection>    type_L1TTau;
-//typedef ICL1TObjectProducer<l1t::TauBxCollection>    type_L1TTauIso;
-//
-//DEFINE_FWK_MODULE(type_L1TEGamma);
-//DEFINE_FWK_MODULE(type_L1TJet);
-//DEFINE_FWK_MODULE(type_L1TSum);
-//DEFINE_FWK_MODULE(type_L1TMuon);
-//DEFINE_FWK_MODULE(type_L1TTau);
-//DEFINE_FWK_MODULE(type_L1TTauIso);
-
-
-
-
-
-
-
-
-
-
+DEFINE_FWK_MODULE(ICL1TObjectProducer<l1t::EGamma>);
+DEFINE_FWK_MODULE(ICL1TObjectProducer<l1t::Jet>);
+DEFINE_FWK_MODULE(ICL1TObjectProducer<l1t::EtSum>);
+DEFINE_FWK_MODULE(ICL1TObjectProducer<l1t::Muon>);
+DEFINE_FWK_MODULE(ICL1TObjectProducer<l1t::Tau>);
