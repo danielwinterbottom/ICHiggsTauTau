ce288499613abc9f2da819eff1452953217a9eaa
Riccardo Di Maria <r.di-maria15@imperial.ac.uk>
2016-06-17 15:46:34 +0100

    160617 ntuples prod

--- test/higgsinvcrab/crab_run2data_2016.py
+++ test/higgsinvcrab/crab_run2data_2016.py
@@ -1,5 +1,5 @@
 from WMCore.Configuration import Configuration
-prod ='160603'     #!!TO BE UPDATED ON EACH PROCESSING
+prod ='160617'     #!!TO BE UPDATED ON EACH PROCESSING
 config = Configuration()
 
 config.section_('General')
