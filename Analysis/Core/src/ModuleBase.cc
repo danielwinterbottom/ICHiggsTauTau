1c1f7c23aa2870f48e4e7ada4fc7748ad2f54463
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-06-19 18:14:33 +0200

    Adding methods for printing options to ModuleBase, saves having to fire up boost::format or something similar in each module

--- Analysis/Core/src/ModuleBase.cc
+++ Analysis/Core/src/ModuleBase.cc
@@ -6,4 +6,10 @@ ModuleBase::ModuleBase(std::string const& name)
     : module_name_(name), events_processed_(0) {}
 
 ModuleBase::~ModuleBase() { ; }
+
+void ModuleBase::PrintHeader(std::string const& classname) {
+  std::cout << "-------------------------------------\n";
+  std::cout << classname << ": " << module_name_ << "\n";
+  std::cout << "-------------------------------------\n";
+}
 }
