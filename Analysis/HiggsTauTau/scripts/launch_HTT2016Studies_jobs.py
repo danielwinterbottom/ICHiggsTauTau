77ecb72393610beec8ad3f4ef63d156031a996a0
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-07-12 17:31:57 +0200

    Update plotRunYields to figure out and display which runs the trigger was prescaled in

--- Analysis/HiggsTauTau/scripts/launch_HTT2016Studies_jobs.py
+++ Analysis/HiggsTauTau/scripts/launch_HTT2016Studies_jobs.py
@@ -12,25 +12,49 @@ basedir = '%s/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau' % os.environ[
     'CMSSW_BASE']
 
 MAX_EVTS = -1
-FILES_PER_JOB = 100
+FILES_PER_JOB = 200
 
-SAMPLES = [
-  'Tau-Run2016B-PromptReco-v1',
-  'Tau-Run2016B-PromptReco-v2'
-]
+SAMPLES = {
+  'Tau': [
+        'Tau-Run2016B-PromptReco-v1',
+        'Tau-Run2016B-PromptReco-v2'
+        ],
+  'SingleMuon': [
+        'SingleMuon-Run2016B-PromptReco-v1',
+        'SingleMuon-Run2016B-PromptReco-v2',
+        'SingleMuon-Run2016C-PromptReco-v2'
+        ],
+  'SingleElectron': [
+        'SingleElectron-Run2016B-PromptReco-v1',
+        'SingleElectron-Run2016B-PromptReco-v2',
+        'SingleElectron-Run2016C-PromptReco-v2'
+        ],
+}
+
+OUTPUT = 'output/HTT2016Studies'
+os.system('mkdir -p %s' % OUTPUT)
 
 for sa in SAMPLES:
-    filelist = '%s/filelists/%s.dat' % (basedir, sa)
+    filelists = ['filelists/%s.dat' % (X) for X in SAMPLES[sa]]
     cfg = {
-        'output': '%s.root' % sa,
-        'filelists': [filelist],
+        # General settings
+        'output': '%s/%s.root' % (OUTPUT, sa),
+        'filelists': filelists,
         'max_events': MAX_EVTS,
-        'sample_name': sa,
-        'lumi_mask': 'Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt',
-        'lumi_out': 'lumi',
-        'has_map_mode': 0,
-        'trigger_info_output': 'test_info.json'
+        # Lumi settings
+        'lumi_mask': 'Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt',
+        'lumi_out': '%s/lumi_%s' % (OUTPUT, sa),
+        # Hash map settings
+        'hash_map_mode': 0,
+        'hash_map_input': 'input/hash_map.json',
+        'hash_map_output': 'hash_map_%s.json' % sa,
+        # Trigger info settings
+        'trigger_info_output': '%s/trigger_info_%s.json' % (OUTPUT, sa)
     }
-    job_mgr.add_filelist_split_jobs(prog=basedir+'/bin/HTT2016Studies', cfg=cfg, files_per_job=FILES_PER_JOB)
+    job_mgr.add_filelist_split_jobs(
+        prog=basedir+'/bin/HTT2016Studies',
+        cfg=cfg,
+        files_per_job=FILES_PER_JOB,
+        output_cfgs=['output', 'lumi_out', 'trigger_info_output'])
 
 job_mgr.flush_queue()
--- Analysis/HiggsTauTau/scripts/plotRunYields.py
+++ Analysis/HiggsTauTau/scripts/plotRunYields.py
@@ -3,12 +3,13 @@ import ROOT
 import argparse
 import json
 import math
+import bisect
 import CombineHarvester.CombineTools.plotting as plot
 
 # Boilerplate
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
-plot.ModTDRStyle(width=1600, height=1000, b=0.17)
+plot.ModTDRStyle(width=2000, height=1000, b=0.17)
 
 LS_TIME = 23.310779  # seconds
 MINBIAS_XS = 71000000000.0  # pb
@@ -20,6 +21,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--yields', default=None)
 parser.add_argument('--lumi', default=None)
 parser.add_argument('--beam', default=None)
+parser.add_argument('--trg', default=None)
+parser.add_argument('--json', default=None)
 parser.add_argument('--min-lumi', default=0., type=float)
 parser.add_argument('--output', default='yield_per_run')
 
@@ -40,6 +43,45 @@ if args.beam is not None:
     with open(args.beam) as f:
         beam_lines = [line.split(',') for line in f.read().splitlines() if not line.startswith('#')]
 
+if args.trg is not None:
+    with open(args.trg) as f:
+        trg_lines = [line.split(',') for line in f.read().splitlines() if not line.startswith('#')]
+
+trg_info = []
+for line in trg_lines:
+    if line[1] == 'None':
+        continue
+    trg_info.append(((int(line[0]), int(line[1])), line[2:]))
+
+trg_info.sort(key=lambda x: x[0])
+for x in trg_info:
+    print x
+
+trg_keys = [x[0] for x in trg_info]
+
+prescale_ok = {}
+
+if args.json is not None:
+    with open(args.json) as jsonfile:
+        cert_json = json.load(jsonfile)
+    for run in cert_json:
+        prescale_ok[run] = True
+        for ls in cert_json[run]:
+            print 'Checking %s, %s' % (run,ls)
+            idx_lo = bisect.bisect_right(trg_keys, (int(run), int(ls[0])))
+            idx_hi = bisect.bisect_right(trg_keys, (int(run), int(ls[1])))
+            if idx_lo > 0:
+                idx_lo -= 1
+            if idx_hi > 0:
+                idx_hi -= 1
+            for info in trg_info[idx_lo:idx_hi+1]:
+                if int(info[1][1]) != 1:
+                    prescale_ok[run] = False
+                print info
+        if not prescale_ok[run]:
+            print 'Trigger is prescaled in run %s' % run
+
+
 nbx_dict = {}
 for line in beam_lines:
     run = int(line[1])
@@ -48,6 +90,7 @@ for line in beam_lines:
 
 runs = sorted(x for x in lumi_dict.keys() if lumi_dict[x] > args.min_lumi)
 
+yield_dict = {}
 if args.yields is not None:
     yields = args.yields.split(':')
 
@@ -55,7 +98,6 @@ if args.yields is not None:
         yield_dict_tmp = json.load(jsonfile)
     for key in yields[1:]:
         yield_dict_tmp = yield_dict_tmp[key]
-    yield_dict = {}
     for run, rate in yield_dict_tmp.iteritems():
         yield_dict[int(run)] = rate
 
@@ -94,23 +136,41 @@ for i, run in enumerate(runs):
 canv = ROOT.TCanvas(args.output, args.output)
 pads = plot.MultiRatioSplit([0.4, 0.2], [0.01, 0.01], [0.01, 0.01])
 
+bad_trigger = ROOT.TBox()
+bad_trigger.SetFillColorAlpha(ROOT.kRed, 0.3)
 
+
+boxes = []
 pads[0].cd()
 plot.Set(pads[0], Grid=(0, 1), Tickx=1, Ticky=1)
 plot.Set(yield_hist, LineWidth=2, MarkerSize=2, LineColor=ROOT.kRed, MarkerColor=ROOT.kRed)
 plot.Set(yield_hist.GetXaxis(), Title='', LabelSize=0)
 plot.Set(yield_hist.GetYaxis(), Title='Yield / pb^{-1}')
 yield_hist.Draw()
+for i, run in enumerate(runs):
+    if not prescale_ok[str(run)]:
+        boxes.append(bad_trigger.DrawBox(float(i), yield_hist.GetYaxis().GetXmin(), float(i+1), yield_hist.GetYaxis().GetXmax()))
 
 pads[1].cd()
+bad_trigger2 = ROOT.TBox()
+bad_trigger2.SetFillColorAlpha(ROOT.kRed, 0.3)
+
 plot.Set(pads[1], Grid=(0, 1), Tickx=1, Ticky=1)
 plot.Set(npu_hist, LineWidth=2, MarkerSize=2, LineColor=ROOT.kGreen-2, MarkerColor=ROOT.kGreen-2)
 plot.Set(npu_hist.GetXaxis(), Title='', LabelSize=0)
 plot.Set(npu_hist.GetYaxis(), Title='Avg. PU')
 npu_hist.GetYaxis().CenterTitle(True)
 npu_hist.Draw('P')
+plot.FixBothRanges(pads[1], plot.GetAxisHist(pads[1]).GetMinimum(), 0.05, plot.GetAxisHist(pads[1]).GetMaximum(), 0.05)
+print (plot.GetAxisHist(pads[1]).GetMinimum(), plot.GetAxisHist(pads[1]).GetMaximum())
+for i, run in enumerate(runs):
+    if not prescale_ok[str(run)]:
+        boxes.append(bad_trigger2.DrawBox(float(i), plot.GetAxisHist(pads[1]).GetMinimum(), float(i+1), plot.GetAxisHist(pads[1]).GetMaximum()))
+
 
 pads[2].cd()
+bad_trigger = ROOT.TBox()
+bad_trigger.SetFillColorAlpha(ROOT.kRed, 0.3)
 plot.Set(pads[2], Grid=(0, 1), Tickx=1, Ticky=1)
 plot.Set(lumi_hist, LineWidth=2, MarkerSize=2, LineColor=ROOT.kBlue, MarkerColor=ROOT.kBlue)
 plot.Set(lumi_hist.GetXaxis(), Title='Run')
@@ -118,6 +178,10 @@ plot.Set(lumi_hist.GetYaxis(), Title='Lumi (pb^{-1})')
 lumi_hist.GetXaxis().LabelsOption('v1')
 lumi_hist.GetYaxis().CenterTitle(True)
 lumi_hist.Draw('P')
+plot.FixBothRanges(pads[2], 0.00, 0.00, plot.GetAxisHist(pads[2]).GetMaximum(), 0.05)
+for i, run in enumerate(runs):
+    if not prescale_ok[str(run)]:
+        boxes.append(bad_trigger.DrawBox(float(i), plot.GetAxisHist(pads[2]).GetMinimum(), float(i+1), plot.GetAxisHist(pads[2]).GetMaximum()))
 
 tot_lumi = sum(lumi_dict.values()) / 1000.  # fb-1 for now
 
@@ -125,7 +189,8 @@ right_label = '%.1f fb^{-1} (13 TeV)' % tot_lumi
 if args.min_lumi > 0.:
     right_label += ', runs with L > %g pb^{-1}' % args.min_lumi
 
-plot.DrawTitle(pads[0], yields[-1], 1)
+# plot.DrawTitle(pads[0], yields[-1], 1)
+# plot.DrawTitle(pads[0], 'HLT_IsoMu22_v', 1)
 if args.min_lumi > 0.:
     plot.DrawTitle(pads[0], right_label, 3)
 
