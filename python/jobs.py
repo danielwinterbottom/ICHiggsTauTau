8e20a69b434eaf70abb6aa15b98b7babd0b08b42
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-07-14 14:32:26 +0200

    Status tracking in jobs.py!
    This commit adds a basic status tracking system for any script-based
    job submission. At job creation time an additional (empty) file named
    the same as the job script script but with the extension
    .status.[STATUS] instead of .sh will be created, with [STATUS] being
    updated through various stages either by the job submission script or
    the job script itself:
    
      - created: job script was created
      - submitted: script was submitted (currently all jobs will move from
        created to submitted near-instantly, even if the submission itself
        failed - will try to detect submission failure in the future)
      - running: set by the job script itself at the start
      - done: set by the job script if everything finished ok
      - exit.[EXIT_CODE]: set by the job script if any error occured - the
        exit code from the part that failed is added at the end
    
    Note this status tracking system has to be enabled explicitly by adding
    --tracking. Repeating the submission command will cause jobs.py to first
    check if any .status file exists for each job, and if so skips the job
    submission step. The user can currently remove any .status files by
    hand, and repeat the submission command to effectively resubmit those
    jobs. If '--tracking long' is specified a list of the scripts in each
    status will also be printed at the end, otherwise only a short summary.
    
    NB: it's possible a batch job could fail or be killed in some way that
    leaves the status as 'running'. Will investigate if it's possible to
    figure out if a job is actually running or not by interacting with the
    batch system directly.

--- python/jobs.py
+++ python/jobs.py
@@ -2,12 +2,13 @@ import os
 import json
 import stat
 import copy
+import glob
 from math import ceil
 from functools import partial
 from multiprocessing import Pool
 
-
 JOB_PREFIX = """#!/bin/sh
+set -o pipefail
 cd %(CMSSW_BASE)s/src
 export SCRAM_ARCH=%(SCRAM_ARCH)s
 eval `scramv1 runtime -sh`
@@ -19,6 +20,7 @@ cd %(PWD)s
 })
 
 JOB_NAF_PREFIX = """#!/bin/sh
+set -o pipefail
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd %(CMSSW_BASE)s/src
 export SCRAM_ARCH=%(SCRAM_ARCH)s
@@ -29,6 +31,16 @@ cd %(PWD)s
     'SCRAM_ARCH': os.environ['SCRAM_ARCH'],
     'PWD': os.environ['PWD']
 })
+
+ERROR_CAPTURE = """
+function error_exit
+{
+    mv %s %s.$1
+    exit $1
+}
+
+trap 'error_exit $?' ERR
+"""
 
 
 def run_command(dry_run, command):
@@ -51,12 +63,16 @@ class Jobs:
         self.task_name = 'combine_task'
         self.dry_run = False
         self.bopts = ''  # batch submission options
+        self.tracking = False
+        self.task_dir = ''
 
     def attach_job_args(self, group):
         group.add_argument('--job-mode', default=self.job_mode, choices=[
                            'interactive', 'script', 'lxbatch', 'NAF', 'ts'], help='Task execution mode')
         group.add_argument('--task-name', default=self.task_name,
                            help='Task name, used for job script and log filenames for batch system tasks')
+        group.add_argument('--dir', default=self.task_dir,
+                           help='Area for creating the job scripts')
         group.add_argument('--parallel', type=int, default=self.parallel,
                            help='Number of jobs to run in parallel [only affects interactive job-mode]')
         group.add_argument('--merge', type=int, default=self.merge,
@@ -67,6 +83,8 @@ class Jobs:
                            help='Options for batch submission')
         group.add_argument('--memory', type=int,
                            help='Request memory for job [MB]')
+        group.add_argument('--tracking', nargs='?', default=False, const='short',
+                           help='Track job status (if applicable)')
 
     def set_args(self, args):
         self.args = args
@@ -77,6 +95,10 @@ class Jobs:
         self.dry_run = self.args.dry_run
         self.bopts = self.args.sub_opts
         self.memory = self.args.memory
+        self.tracking = self.args.tracking
+        self.task_dir = self.args.dir
+        # if self.dry_run:
+        #     self.tracking = False
 
     def file_len(self, fname):
         with open(fname) as f:
@@ -115,19 +137,44 @@ class Jobs:
                 text_file.write(JOB_NAF_PREFIX)
             else:
                 text_file.write(JOB_PREFIX)
+            if self.tracking:
+                full_path = os.path.abspath(script_filename)
+                text_file.write('mv %s %s\n' % (
+                        full_path.replace('.sh', '.status.submitted'),
+                        full_path.replace('.sh', '.status.running')
+                    ))
+                text_file.write(ERROR_CAPTURE % (
+                        full_path.replace('.sh', '.status.running'),
+                        full_path.replace('.sh', '.status.error')
+                    ))
             for i, command in enumerate(commands):
                 tee = 'tee' if i == 0 else 'tee -a'
                 log_part = '\n'
                 if do_log: log_part = ' 2>&1 | %s ' % tee + logname + log_part
-                # if command.startswith('combine') or command.startswith('pushd'):
-                #     text_file.write(
-                #         'eval ' + command + log_part)
-                # else:
-                #     text_file.write(command)
                 text_file.write(command + log_part)
+                if self.tracking:
+                    full_path = os.path.abspath(script_filename)
+                    text_file.write('mv %s %s\n' % (
+                            full_path.replace('.sh', '.status.running'),
+                            full_path.replace('.sh', '.status.done')
+                        ))
         st = os.stat(fname)
         os.chmod(fname, st.st_mode | stat.S_IEXEC)
         print 'Created job script: %s' % script_filename
+        if self.tracking and not self.dry_run:
+            trackname = script_filename.replace('.sh', '.status.created')
+            open(trackname, 'a').close()
+
+    def read_job_status(self, script_filename):
+        full_path = os.path.abspath(script_filename)
+        status_files = glob.glob(full_path.replace('.sh', '.status.*'))
+        statuses = []
+        for status_f in status_files:
+            pos = status_f.rfind('status')
+            status = (status_f[pos:]).split('.')[1:]
+            statuses.append(status)
+        # print statuses
+        return statuses
 
     def flush_queue(self):
         if self.job_mode == 'interactive':
@@ -135,9 +182,30 @@ class Jobs:
             result = pool.map(
                 partial(run_command, self.dry_run), self.job_queue)
         script_list = []
+        status_result = {}
+        njobs = 0
         if self.job_mode in ['script', 'lxbatch', 'NAF', 'ts']:
             for i, j in enumerate(range(0, len(self.job_queue), self.merge)):
+                njobs += 1
                 script_name = 'job_%s_%i.sh' % (self.task_name, i)
+                if self.task_dir is not '':
+                    script_name = os.path.join(self.task_dir, script_name)
+                status = 'unknown'
+                if self.tracking:
+                    statuses = self.read_job_status(script_name)
+                    if len(statuses) == 0:
+                        status = 'new'
+                    elif len(statuses) == 1:
+                        status = statuses[0][0]
+                    else:
+                        status = 'confused'
+                    if status not in status_result:
+                       status_result[status] = []
+                    status_result[status].append(script_name)
+                    if len(statuses) > 0:
+                        # print '%s appears to already be in progress, skipping...' % script_name
+                        continue
+
                 # each job is given a slice from the list of combine commands of length 'merge'
                 # we also keep track of the files that were created in case submission to a
                 # batch system was also requested
@@ -158,5 +226,19 @@ class Jobs:
             for script in script_list:
                 full_script = os.path.abspath(script)
                 run_command(self.dry_run, 'ts bash -c "eval %s"' % (full_script))
+        if self.job_mode in ['lxbatch', 'NAF', 'ts'] and self.tracking:
+            if not self.dry_run:
+                for script in script_list:
+                    full_script = os.path.abspath(script)
+                    os.rename(full_script.replace('.sh', '.status.created'), full_script.replace('.sh', '.status.submitted'))
+            print '>> Status summary'
+            for status in status_result:
+                counter = '[%i/%i]' % (len(status_result[status]), njobs)
+                print '%20s %10s' % (status, counter)
+                if self.tracking == 'long':
+                    for f in status_result[status]:
+                        print ' '*20 + '%s' % f
+
+
 
         del self.job_queue[:]
