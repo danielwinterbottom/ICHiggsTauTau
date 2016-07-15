866518c9b5fdf8ed4cc0f6324b6f8c709d4080ba
Riccardo Di Maria <r.di-maria15@imperial.ac.uk>
2016-07-15 17:13:39 +0100

    for reHLT samples

---
+++ test/higgsinvcrab/crab_run2background_reHLT.py
@@ -0,0 +1,100 @@
+from WMCore.Configuration import Configuration
+prod ='160715'     #!!TO BE UPDATED ON EACH PROCESSING
+config = Configuration()
+
+config.section_('General')
+config.section_('Data')
+config.section_('JobType')
+config.section_('User')
+config.section_('Site')
+
+config.General.transferOutputs = True
+config.General.workArea=prod+'/MC'
+
+config.Data.outLFNDirBase='/store/user/rdimaria/'+prod+'_MC/'
+
+config.JobType.psetName ='/home/hep/rd1715/CMSSW_8_0_12/src/UserCode/ICHiggsTauTau/test/higgsinv_8_0_12_miniAODcfg_reHLT.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['EventTree.root']
+
+config.Data.unitsPerJob  = 30000
+config.Data.splitting    = 'EventAwareLumiBased'
+config.Data.inputDataset = True # Allow CRAB to run over (the valid files of) the input dataset given in Data.inputDataset even if its status in DBS is not VALID. Defaults to False.
+#config.Data.inputDataset = 'DUMMY'
+#config.Data.unitsPerJob = 1
+#config.Data.splitting = 'FileBased'
+
+config.JobType.pyCfgParams = ['isData=0','doHT=0','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']   #!!TO BE CHECKED ON EACH PROCESSING
+
+config.Data.publication = False
+config.Site.whitelist = ['T2_UK_London_IC', 'T2_CH_CERN', 'T2_FR_GRIF_LLR', 'T2_UK_SGrid_Bristol', 'T3_US_FNALLPC', 'T2_DE_DESY', 'T2_IT_Bari', 'T2_BE_IIHE', 'T2_US_UCSD', 'T2_US_MIT', 'T2_IT_Pisa', 'T2_US_Wisconsin', 'T2_US_Florida', 'T2_IT_Rome','T2_FR_IPHC','T2_US_Purdue','T2_IT_Legnaro','T2_FR_GRIF_IRFU']
+
+config.Site.storageSite = 'T2_UK_London_IC'
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+
+    # We want to put all the CRAB project directories from the tasks we submit here into one common directory.
+    # That's why we need to set this parameter (here or above in the configuration file, it does not matter, we will not overwrite it).
+
+    def submit(inconfig):
+        try:
+            crabCommand('submit', config = inconfig)
+#            crabCommand('status')
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+
+    tasks=list()
+    
+    #Entries are in the format:
+    #tasks.append((taskname,dataset name from das))
+
+
+    ### reHLT
+    tasks.append(('Powheg-VBFHtoinv-mH110-reHLT','/VBF_HToInvisible_M110_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH125-reHLT','/VBF_HToInvisible_M125_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH150-reHLT','/VBF_HToInvisible_M150_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH200-reHLT','/VBF_HToInvisible_M200_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH300-reHLT','/VBF_HToInvisible_M300_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH400-reHLT','/VBF_HToInvisible_M400_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH500-reHLT','/VBF_HToInvisible_M500_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH600-reHLT','/VBF_HToInvisible_M600_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH800-reHLT','/VBF_HToInvisible_M800_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+    tasks.append(('Powheg-VBFHtoinv-mH1000-reHLT','/VBF_HToInvisible_M1000_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'))
+
+    for task in tasks:
+        print task[0]
+        config.General.requestName = task[0]
+        config.Data.inputDataset = task[1]
+        if "_HT" in task[1]:
+            if "Invisible" in task[1]:
+                config.JobType.pyCfgParams = ['isData=0','doHT=0','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+            else :
+                config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        else:
+            config.JobType.pyCfgParams = ['isData=0','doHT=0','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        
+        if "DYJetsToLL_M-50_Tune" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        if "DY1JetsToLL_M-50_Tune" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        if "DY2JetsToLL_M-50_Tune" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        if "DY3JetsToLL_M-50_Tune" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        if "DY4JetsToLL_M-50_Tune" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        if "DYJetsToLL_M-50_Zpt" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        if "WJetsToLNu_Tune" in task[1]:
+          config.JobType.pyCfgParams = ['isData=0','doHT=1','release=80XMINIAOD','globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1']
+        submit(config)
