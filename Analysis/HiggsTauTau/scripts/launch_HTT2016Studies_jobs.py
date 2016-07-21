70568a7d637781f402adea074ea6152cd30874fd
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-07-21 13:38:30 +0200

    prep trigger info jobs for new ntuples and new lumi json file

--- Analysis/HiggsTauTau/scripts/launch_HTT2016Studies_jobs.py
+++ Analysis/HiggsTauTau/scripts/launch_HTT2016Studies_jobs.py
@@ -13,42 +13,47 @@ basedir = '%s/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau' % os.environ[
 
 MAX_EVTS = -1
 FILES_PER_JOB = 100
+PROD='July19_'
 
 SAMPLES = {
   'Tau': [
         'Tau-Run2016B-PromptReco-v1',
         'Tau-Run2016B-PromptReco-v2',
-        'Tau-Run2016C-PromptReco-v2'
+        'Tau-Run2016C-PromptReco-v2',
+        'Tau-Run2016D-PromptReco-v2'
         ],
   'SingleMuon': [
         'SingleMuon-Run2016B-PromptReco-v1',
         'SingleMuon-Run2016B-PromptReco-v2',
-        'SingleMuon-Run2016C-PromptReco-v2'
+        'SingleMuon-Run2016C-PromptReco-v2',
+        'SingleMuon-Run2016D-PromptReco-v2'
         ],
   'SingleElectron': [
         'SingleElectron-Run2016B-PromptReco-v1',
         'SingleElectron-Run2016B-PromptReco-v2',
-        'SingleElectron-Run2016C-PromptReco-v2'
+        'SingleElectron-Run2016C-PromptReco-v2',
+        'SingleElectron-Run2016D-PromptReco-v2'
         ],
   'MuonEG': [
         'MuonEG-Run2016B-PromptReco-v1',
         'MuonEG-Run2016B-PromptReco-v2',
-        'MuonEG-Run2016C-PromptReco-v2'
+        'MuonEG-Run2016C-PromptReco-v2',
+        'MuonEG-Run2016D-PromptReco-v2'
         ]
 }
 
-OUTPUT = 'output/HTT2016Studies'
+OUTPUT = 'output/HTT2016Studies_July19'
 os.system('mkdir -p %s' % OUTPUT)
 
 for sa in SAMPLES:
-    filelists = ['filelists/%s.dat' % (X) for X in SAMPLES[sa]]
+    filelists = ['filelists/%s%s.dat' % (PROD, X) for X in SAMPLES[sa]]
     cfg = {
         # General settings
         'output': '%s/%s.root' % (OUTPUT, sa),
         'filelists': filelists,
         'max_events': MAX_EVTS,
         # Lumi settings
-        'lumi_mask': 'input/json/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON.txt',
+        'lumi_mask': 'input/json/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt',
         'lumi_out': '%s/lumi_%s' % (OUTPUT, sa),
         # Hash map settings
         'hash_map_mode': 0,
