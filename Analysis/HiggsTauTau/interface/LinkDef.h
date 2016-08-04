2abd0030872439d360e9cb0f9be039dcde768ac4
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-08-04 11:22:57 +0200

    Adding CrystalBallEfficiency class for dictionary generation

---
+++ Analysis/HiggsTauTau/interface/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+#include <utility>
+#include <string>
+
+#pragma link off all globals;
+#pragma link off all classes;
+// #pragma link C++ all typedef;
+#pragma link off all functions;
+// #pragma link off nestedtypedef;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class CrystalBallEfficiency+;
+#endif
