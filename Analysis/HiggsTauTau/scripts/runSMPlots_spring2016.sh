b88d029f929b506bdf10f1428d46a18a9e464f2f
Daniel <[Email>
2016-08-12 13:07:35 +0100

    change mass range

--- Analysis/HiggsTauTau/scripts/runSMPlots_spring2016.sh
+++ Analysis/HiggsTauTau/scripts/runSMPlots_spring2016.sh
@@ -29,7 +29,7 @@ do
     else
       blind="true"
     fi
-    ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_sm_2016.cfg --channel=$i --set_alias="sel:mt_1<40." --method=8 --var="m_vis"["50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250"] --cat=$j --datacard=$j --x_axis_label="M_{#tau#tau} [GeV]" --blind=$blind --x_blind_min=30 --x_blind_max=200 --background_scheme=$back --extra_pad=0.2 --draw_error_band=true --auto_error_band=0.00001
+    ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_sm_2016.cfg --channel=$i --set_alias="sel:mt_1<40." --method=8 --var="m_vis"["10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250"] --cat=$j --datacard=$j --x_axis_label="M_{#tau#tau} [GeV]" --blind=$blind --x_blind_min=30 --x_blind_max=200 --background_scheme=$back --extra_pad=0.2 --draw_error_band=true --auto_error_band=0.00001
     mv "m_vis_"${j}_${i}"_2016.png" $folder.
     mv "m_vis_"${j}_${i}"_2016.pdf" $folder.
     mv "datacard_m_vis_"${j}_${i}"_2016.root" $folder.
