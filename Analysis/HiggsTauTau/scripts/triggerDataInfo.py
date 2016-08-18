bce056b85e98cc7ac51857cdb38c1c692fa0572a
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-08-18 23:40:31 +0200

    move some of the printouts around and detect when the L1 seed changes within the same HLT version number (didn't think this was supposed to happen but apparently it does)

--- Analysis/HiggsTauTau/scripts/triggerDataInfo.py
+++ Analysis/HiggsTauTau/scripts/triggerDataInfo.py
@@ -39,7 +39,7 @@ class Visitor:
         pass
 
 
-def DoFilters(path, menu, save):
+def DoFilters(path, menu, save, append=''):
     # print ' '.join(['hltConfigFromDB', '--v2', '--adg', '--configName', menu, '--cff', '--paths', path])
     out = subprocess.check_output(['hltConfigFromDB', '--v2', '--adg' , '--configName', menu, '--cff', '--paths', path])
     module = imp.new_module('trg')
@@ -49,9 +49,9 @@ def DoFilters(path, menu, save):
     process.extend(module)
     visitor = Visitor(save)
     if save == 'filters':
-        visitor.file = open('cfg_filters_%s.txt' % path, 'w')
+        visitor.file = open('cfg_filters_%s%s.txt' % (path, append), 'w')
     if save == 'all':
-        visitor.file = open('cfg_all_%s.txt' % path, 'w')
+        visitor.file = open('cfg_all_%s%s.txt' % (path, append), 'w')
     print '    %-40s %-80s %-8s %-6s' % ('Type', 'Label', 'Objects', 'Min')
     print '    %s' % ('-'*137)
     getattr(process, path).visit(visitor)
@@ -65,9 +65,15 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--trg', default=None)
 parser.add_argument('--map', default=None)
+parser.add_argument('--run-range', default=None)
 parser.add_argument('--save-cfg', default=None)
 
 args = parser.parse_args()
+
+
+if args.run_range is not None:
+    min_run = int(args.run_range.split(',')[0])
+    max_run = int(args.run_range.split(',')[1])
 
 run_menu = {}
 run_id = {}
@@ -90,6 +96,10 @@ trg_info = []
 for line in trg_lines:
     if len(line) >= 2 and line[1] == 'None':
         continue
+    if args.run_range is not None:
+        run = int(line[0])
+        if run < min_run or run > max_run:
+            continue
     trg_info.append(((int(line[0]), int(line[1])), line[2:]))
 
 trg_info.sort(key=lambda x: x[0])
@@ -106,40 +116,60 @@ curr_run = None
 curr_menu = None
 curr_L1 = None
 curr_path = None
+curr_L1_set = None
 
-
+L1_alt_idx = 1
 for i, x in enumerate(trg_info):
     # print x
     run = x[0][0]
     path = x[1][2].split('/')[0]
     v = int(re.match(r'.*_v(\d+)', path).group(1))
     menu = run_menu[run]
-    L1 = x[1][4].split('/')[0]
+    L1 = re.sub(r'/\d+', '', x[1][4])
+    L1_set = set(L1.split())
+    #if path not in L1_per_name:
+    #    L1_per_name[path] = L1_set
+    #else:
+    #    if L1_per_name[path] != L1_set:
+    #        print 'Found different L1 seeds for the same trigger version!'
+    #        print '>> %s' % path
+    #        print '>> %s' % L1_per_name[path]
+    #        print '>> %s' % L1_set
+
+    L1 = ' OR '.join(L1.split())
     if curr_v is None:
         print ' - V%i first seen in run %i' % (v, run)
         print '    * HLT menu: %s' % (menu)
         print '    * L1T seed: %s' % (L1)
-        print '    * Filter sequence:'
+        print ' - V%i filter sequence:' % (v)
+        mod = DoFilters(path, menu, args.save_cfg)
     if curr_v is not None and v != curr_v:
         print ' - V%i last seen in run %i' % (curr_v, curr_run)
         print '    * HLT menu: %s' % (curr_menu)
         print '    * L1T seed: %s' % (curr_L1)
-        print ' - V%i filter sequence:' % (curr_v)
-        mod = DoFilters(curr_path, curr_menu, args.save_cfg)
         print ' - V%i first seen in run %i' % (v, run)
-        print '    * HLT menu: %s' % (menu)
-        print '    * L1T seed: %s' % (L1)
-    if i == (len(trg_info) - 1):
-        print ' - V%i last seen in run %i' % (v, run)
         print '    * HLT menu: %s' % (menu)
         print '    * L1T seed: %s' % (L1)
         print ' - V%i filter sequence:' % (v)
         mod = DoFilters(path, menu, args.save_cfg)
+        L1_alt_idx = 1
+    if i == (len(trg_info) - 1):
+        print ' - V%i last seen in run %i' % (v, run)
+        print '    * HLT menu: %s' % (menu)
+        print '    * L1T seed: %s' % (L1)
+    if curr_v is not None and v == curr_v and curr_L1_set != L1_set:
+        print ' - V%i has a change in L1 seed in run %i' % (v, run)
+        print '    * HLT menu: %s' % (menu)
+        print '    * L1T seed: %s' % (L1)
+        print ' - V%i filter sequence:' % (v)
+        mod = DoFilters(path, menu, args.save_cfg, '_altL1_' + str(L1_alt_idx))
+        L1_alt_idx += 1
 
     curr_v = v
     curr_run = run
     curr_menu = menu
     curr_L1 = L1
     curr_path = path
+    curr_L1_set = L1_set
 
 print ('\n\n')
