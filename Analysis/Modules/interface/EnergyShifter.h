35d85cdfcadb925cd5a24fca8b536abd25fcf749
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-08-21 14:04:07 +0200

    EnergyShifter: Adding option to save a map<id, PxPyPzEVector> of the change in 4-momentum for each corrected object. Idea to is make it easier to correct MET later in a more general way

--- Analysis/Modules/interface/EnergyShifter.h
+++ Analysis/Modules/interface/EnergyShifter.h
@@ -1,11 +1,12 @@
 #ifndef ICHiggsTauTau_Module_EnergyShifter_h
 #define ICHiggsTauTau_Module_EnergyShifter_h
 
-#include "UserCode/ICHiggsTauTau/Analysis/Core/interface/TreeEvent.h"
-#include "UserCode/ICHiggsTauTau/Analysis/Core/interface/ModuleBase.h"
-#include "UserCode/ICHiggsTauTau/Analysis/Utilities/interface/FnPredicates.h"
 #include <string>
-#include "boost/bind.hpp"
+#include <map>
+#include "Math/Vector4D.h"
+#include "Math/Vector4Dfwd.h"
+#include "Core/interface/TreeEvent.h"
+#include "Core/interface/ModuleBase.h"
 
 namespace ic {
 
@@ -14,6 +15,8 @@ class EnergyShifter : public ModuleBase {
  private:
   CLASS_MEMBER(EnergyShifter, std::string, input_label)
   CLASS_MEMBER(EnergyShifter, double, shift)
+  CLASS_MEMBER(EnergyShifter, bool, save_shifts)
+  CLASS_MEMBER(EnergyShifter, std::string, shift_label)
 
  public:
   EnergyShifter(std::string const& name);
@@ -29,6 +32,8 @@ template <class T>
 EnergyShifter<T>::EnergyShifter(std::string const& name) : ModuleBase(name) {
   input_label_ = "";
   shift_ = 1.0;
+  save_shifts_ = false;
+  shift_label_ = "shifts_"+ModuleName();
 }
 
 template <class T>
@@ -38,19 +43,31 @@ EnergyShifter<T>::~EnergyShifter() {
 
 template <class T>
 int EnergyShifter<T>::PreAnalysis() {
-  std::cout << "----------------------------------------" << std::endl;
-  std::cout << "PreAnalysis Info for EnergyShifter" << std::endl;
-  std::cout << "----------------------------------------" << std::endl;
-  std::cout << "Input: [" << input_label_ << "] Shift: [" << shift_ << "]" << std::endl;
+  PrintHeader("EnergyShifter");
+  PrintArg("input_label", input_label_);
+  PrintArg("shift", shift_);
+  PrintArg("save_shifts", save_shifts_);
+  if (save_shifts_) {
+    PrintArg("shift_label", shift_label_);
+  }
   return 0;
 }
 
 template <class T>
 int EnergyShifter<T>::Execute(TreeEvent *event) {
   std::vector<T *> & vec = event->GetPtrVec<T>(input_label_);
+  std::map<std::size_t, ROOT::Math::PxPyPzEVector> shifts;
   for (unsigned i = 0; i < vec.size(); ++i) {
+    ROOT::Math::PxPyPzEVector before(vec[i]->vector());
     vec[i]->set_pt(vec[i]->pt()*shift_);
     vec[i]->set_energy(vec[i]->energy()*shift_);
+    if (save_shifts_) {
+      ROOT::Math::PxPyPzEVector after(vec[i]->vector());
+      shifts[vec[i]->id()] = after - before;
+    }
+  }
+  if (save_shifts_) {
+    event->Add(shift_label_, shifts);
   }
   return 0;
 }
