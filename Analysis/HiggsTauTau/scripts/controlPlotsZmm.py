b74029796714bcf8528949cdf458b8582750a13c
Andrew <andrew.gilbert@cern.ch>
2016-09-10 19:46:13 +0200

    Zmm control plots: use full list of diboson samples

--- Analysis/HiggsTauTau/scripts/controlPlotsZmm.py
+++ Analysis/HiggsTauTau/scripts/controlPlotsZmm.py
@@ -21,16 +21,17 @@ ana.remaps = {
 }
 ana.AddSamples('output/HTT2016Studies_Aug16/Zmm/*.root', 'Zmm')
 ana.AddInfo('scripts/params_Aug16.json', scaleTo='data_obs')
+vv_samples = ['VVTo2L2Nu', 'WWTo1L1Nu2Q', 'WZJToLLLNu',
+     'WZTo1L1Nu2Q', 'WZTo1L3Nu', 'WZTo2L2Q', 'ZZTo2L2Q', 'ZZTo4L']
 
 sel = '(os && pt_1>23 && pt_2>20 && iso_1<0.15 && iso_2<0.15 && trg_IsoMu22) * wt'
 for plot in ['m_ll(60,70,130)', 'pt_1(50,0,200)', 'pt_2(50,0,200)', 'eta_1(50,-2.4,2.4)', 'eta_2(50,-2.4,2.4)', 'mvamet_et(40,0,400)']:
     nodename = plot.split('(')[0]
     ana.nodes.AddNode(ListNode(nodename))
-    for sa in ana.trees:
-        ana.nodes[nodename].AddNode(BasicNode(sa, sa, plot, sel, factors=[ana.info[sa]['sf']]))
-        ana.nodes[nodename].AddNode(SummedNode('VV'))
-        for vv_sample in ['ZZTo2L2Q', 'ZZTo4L']:
-            ana.nodes[nodename]['VV'].AddNode(BasicNode(vv_sample, vv_sample, plot, sel, factors=[ana.info[vv_sample]['sf']]))
+    ana.nodes[nodename].AddNode(ana.BasicFactory('data_obs', 'data_obs', plot, sel))
+    ana.nodes[nodename].AddNode(ana.BasicFactory('ZLL', 'DYJetsToLL', plot, sel))
+    ana.nodes[nodename].AddNode(ana.BasicFactory('TT', 'TT', plot, sel))
+    ana.nodes[nodename].AddNode(ana.SummedFactory('VV', vv_samples, plot, sel))
 ana.Run()
 ana.nodes.PrintTree()
 
