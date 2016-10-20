f1ae9cb296e56253db9cdeed80eda7c62ebc4b98
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2016-10-20 10:48:55 +0100

    Adding sfs for mT plots

--- Analysis/HiggsTauTau/scripts/run2_control_2016.sh
+++ Analysis/HiggsTauTau/scripts/run2_control_2016.sh
@@ -28,6 +28,8 @@ if [ "$POSTFIT" -gt "0" ]
     MT_BAND_ONLY=" --draw_error_band=true --auto_error_band=0.00639 "
     EM_BAND_ONLY=" --draw_error_band=true --auto_error_band=0.00703 "
     TT_BAND_ONLY=" --draw_error_band=true --auto_error_band=0.02749 "
+    ET_INC_SHIFT_MT=" --draw_error_band=true --auto_error_band=0.01674 --shift_backgrounds=QCD:1.07230,TTJ:0.89118,TTT:0.92003,VVJ:0.96862,VVT:0.95155,ZJ:0.91755,ZL:1.08203,ZTT:0.98755 "
+    MT_INC_SHIFT_MT=" --draw_error_band=true --auto_error_band=0.02116 --shift_backgrounds=QCD:0.99163,TTJ:0.93723,TTT:0.94396,VVJ:0.96610,VVT:0.98556,ZJ:0.96421,ZL:0.99400,ZTT:1.02205 "
   else
     echo "Setting prefit uncertainty..."
     ET_INC_SHIFT=" --draw_error_band=true --auto_error_band=0.07852 " 
@@ -489,12 +491,12 @@ fi
 ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=et  \
   --method=12 --cat="inclusive" --set_alias="sel:1" --var="mt_1(20,0,160)" \
   --x_axis_label="m_{T} [GeV]" --datacard="inclusive" --extra_pad=0.2 \
-  --background_scheme="et_default" $ET_BAND_ONLY
+  --background_scheme="et_default" $ET_INC_SHIFT_MT
 
 ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=mt \
   --method=12 --cat="inclusive" --set_alias="sel:1" --var="mt_1(20,0,160)" \
   --x_axis_label="m_{T} [GeV]" --datacard="inclusive" --extra_pad=0.2 \
-  --background_scheme="mt_with_zmm" $MT_BAND_ONLY
+  --background_scheme="mt_with_zmm" $MT_INC_SHIFT_MT
 '''
 ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=et  \
   --method=12 --cat="inclusive" --set_alias="sel:1" --var="pfmt_1(20,0,160)" \
@@ -507,9 +509,9 @@ fi
   --background_scheme="mt_with_zmm" #--draw_error_band=true --auto_error_band=0.00001
 '''
 ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=em --set_alias="sel:1" \
-  --method=15 --cat="inclusive" --var="pzeta(48,-100,100)" --extra_pad=0.3 \
+  --method=15 --cat="inclusive" --var="pzeta(24,-100,100)" --extra_pad=0.3 \
   --x_axis_label="D_{#zeta} [GeV]" --datacard="inclusive" \
-  --background_scheme="em_default" $EM_BAND_ONLY
+  --background_scheme="em_default" $EM_INC_SHIFT
 '''
 ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=em --set_alias="sel:1" \
   --method=15 --cat="inclusive" --var="pfpzeta(48,-100,100)" --extra_pad=0.3 $EM_BAND_ONLY \
@@ -519,12 +521,12 @@ fi
 ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=em --set_alias="sel:1" \
   --method=15 --cat="inclusive" --var="pzetavis(25,0,50)" \
   --x_axis_label="P_{#zeta}^{vis} [GeV]" --datacard="inclusive" \
-  --background_scheme="em_default" --extra_pad=0.2 $EM_BAND_ONLY
+  --background_scheme="em_default" --extra_pad=0.2 $EM_INC_SHIFT
 
  ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=em --set_alias="sel:1" \
   --method=15 --cat="inclusive" --var="pzetamiss(25,-100,100)"  \
   --x_axis_label="#slash{P}_{#zeta} [GeV]" --datacard="inclusive" \
-  --background_scheme="em_default" --extra_pad=0.3 $EM_BAND_ONLY
+  --background_scheme="em_default" --extra_pad=0.3 $EM_INC_SHIFT
 #
 # ./bin/HiggsTauTauPlot5 --cfg=scripts/new_plot_"$ANA"_"$YEAR".cfg --channel=em --set_alias="sel:pzeta>-20." \
 #  --method=8 --cat="inclusive" --var="em_gf_mva(40,-1,1)" --extra_pad=0.2 $EM_BAND_ONLY \
