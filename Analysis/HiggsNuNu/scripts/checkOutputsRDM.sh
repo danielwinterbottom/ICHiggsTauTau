86051f892499e92876a9db8844b32d1e3d6558e2
Riccardo Di Maria <r.di-maria15@imperial.ac.uk>
2016-10-27 16:51:23 +0100

    ready for new prod

--- Analysis/HiggsNuNu/scripts/checkOutputsRDM.sh
+++ Analysis/HiggsNuNu/scripts/checkOutputsRDM.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PROD=161003
-#JOBDIR=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_${PROD}_ICHEP_withSYS/
-JOBDIR=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_161015/
+PROD=161031
+JOBDIR=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_${PROD}_ICHEP/
+#JOBDIR=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_161015/
 INJOBDIR=0
 
 while [ $# -gt 0 ] ; do
