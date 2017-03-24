cd736c26db3acbea608fc2581afe399cbf046018
Daniel <d.winterbottom15@imperial.ac.uk>
2017-03-24 15:05:23 +0000

    adding inclusive category when running inputs for Z re-weighting. Also producing JES shifted templates

--- Analysis/HiggsTauTau/scripts/makeZWeightsFile3D.C
+++ Analysis/HiggsTauTau/scripts/makeZWeightsFile3D.C
@@ -1,5 +1,5 @@
-{
-std::vector<std::string> cats = {"0jet", "1jet", "ge2jet"};
+void makeZWeights3D(std::string outfile, std::string MC_add_string){
+std::vector<std::string> cats = {"0jet", "1jet", "ge2jet", "inclusive"};
 
 double x_bins[4] = {0,1,2,10000};
 double y_bins[7] = {0,50,80,120,200,800,10000};
@@ -12,7 +12,12 @@ TH3D *h3_data = new TH3D("znjetmasspt_histo","znjetmasspt_histo", n_xbins, x_bin
 TH3D *h3_mc = new TH3D("znjetmasspt_histo_mc","znjetmasspt_histo_mc", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
 TH3D *h3_data_normxbins = new TH3D("znjetmasspt_histo_normxbins","znjetmasspt_histo_normxbins", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
 TH3D *h3_mc_normxbins = new TH3D("znjetmasspt_histo_normxbins_mc","znjetmasspt_histo_normxbins_mc", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-
+TH2D *h_2dweights = new TH2D("zptmass_histo","zptmass_histo",n_ybins,y_bins,n_zbins,z_bins);
+h3_data           ->Sumw2()
+h3_mc             ->Sumw2()
+h3_data_normxbins ->Sumw2()
+h3_mc_normxbins   ->Sumw2()
+h_2dweights       ->Sumw2()
 for(unsigned cati=0; cati<cats.size(); ++cati){
     std::vector<std::string> file_names = {"datacard_pt_tt_"+cats[cati]+"_zmm_2016_mvis50to80.root", "datacard_pt_tt_"+cats[cati]+"_zmm_2016_mvis80to120.root", "datacard_pt_tt_"+cats[cati]+"_zmm_2016_mvis120to200.root","datacard_pt_tt_"+cats[cati]+"_zmm_2016_mvis200to800.root"};
     
@@ -28,8 +33,8 @@ for(unsigned cati=0; cati<cats.size(); ++cati){
       std::string file_name = file_names[i];
       TFile f(file_name.c_str());
       TH1D *h1 = (TH1D*)f.Get(("zmm_"+cats[cati]+"/data_obs").c_str())->Clone();
-      TH1D *h2 = (TH1D*)f.Get(("zmm_"+cats[cati]+"/total_bkg").c_str())->Clone();
-      TH1D *h3 = (TH1D*)f.Get(("zmm_"+cats[cati]+"/ZLL").c_str())->Clone();
+      TH1D *h2 = (TH1D*)f.Get(("zmm_"+cats[cati]+"/total_bkg"+MC_add_string).c_str())->Clone();
+      TH1D *h3 = (TH1D*)f.Get(("zmm_"+cats[cati]+"/ZLL"+MC_add_string).c_str())->Clone();
       h1->SetDirectory(0);
       h2->SetDirectory(0);
       h3->SetDirectory(0);
@@ -50,6 +55,8 @@ for(unsigned cati=0; cati<cats.size(); ++cati){
     
     TH2D *h_data = new TH2D("zptmass_histo","zptmass_histo",n_ybins,y_bins,n_zbins,z_bins);
     TH2D *h_mc = new TH2D("zptmass_histo_mc","zptmass_histo_mc",n_ybins,y_bins,n_zbins,z_bins);
+    h_data->Sumw2();
+    h_mc  ->Sumw2();
     
     for(unsigned i=0; i<h_data->GetNbinsX(); ++i){
       unsigned x_bin = i+1;
@@ -60,8 +67,11 @@ for(unsigned cati=0; cati<cats.size(); ++cati){
           double z_pt = h_data->GetYaxis()->GetBinCenter(y_bin);
           unsigned bin = h->GetXaxis()->FindBin(z_pt);
           double content = h->GetBinContent(bin);
-          if(y_bin == h_data->GetNbinsY()) content = 1;
+          double error = h->GetBinError(bin);
+          if(y_bin == h_data->GetNbinsY()){ content = 1; error = 0;}
           h_data->SetBinContent(x_bin,y_bin,content);
+          h_data->SetBinError(x_bin,y_bin,error);          
+          
       }
       
     }
@@ -74,97 +84,58 @@ for(unsigned cati=0; cati<cats.size(); ++cati){
           double z_pt = h_mc->GetYaxis()->GetBinCenter(y_bin);
           unsigned bin = h->GetXaxis()->FindBin(z_pt);
           double content = h->GetBinContent(bin);
-          if(y_bin == h_mc->GetNbinsY()) content = 1;
+          double error = h->GetBinError(bin);
+          if(y_bin == h_mc->GetNbinsY()){ content = 1; error = 0;}
           h_mc->SetBinContent(x_bin,y_bin,content);
+          h_mc->SetBinError(x_bin,y_bin,error);
       }
       
     }
     
+    if(cats[cati] == "inclusive"){
+      h_data->Scale(h_data->Integral());  
+      h_mc->Scale(h_mc->Integral());
+      h_data->Divide(h_mc);  
+      h_2dweights = h_data->Clone();
+      continue;    
+    }
+    // Set 3D hist bins content and errors
     for(unsigned i=0; i<(unsigned)h_data->GetNbinsX(); ++i){
         for(unsigned j=0; j<(unsigned)h_data->GetNbinsY(); ++j){
-          //double content=1;
           double content = h_data->GetBinContent(i+1,j+1);
+          double error = h_data->GetBinError(i+1,j+1);
           h3_data->SetBinContent(cati+1,i+1,j+1,content);
+          h3_data->SetBinError(cati+1,i+1,j+1,error);
         }
     }
     for(unsigned i=0; i<(unsigned)h_mc->GetNbinsX(); ++i){
         for(unsigned j=0; j<(unsigned)h_mc->GetNbinsY(); ++j){
           double content = h_mc->GetBinContent(i+1,j+1);
+          double error = h_mc->GetBinError(i+1,j+1);
           h3_mc->SetBinContent(cati+1,i+1,j+1,content);
+          h3_mc->SetBinError(cati+1,i+1,j+1,error);
         }
     }
-    
-    //h_data->Scale(1/h_data->Integral());
-    //h_mc->Scale(1/h_mc->Integral());
     
     for(unsigned i=0; i<(unsigned)h_data->GetNbinsX(); ++i){
         for(unsigned j=0; j<(unsigned)h_data->GetNbinsY(); ++j){
           double content = h_data->GetBinContent(i+1,j+1);
+          double error = h_data->GetBinError(i+1,j+1);
           h3_data_normxbins->SetBinContent(cati+1,i+1,j+1,content);
+          h3_data_normxbins->SetBinError(cati+1,i+1,j+1,error);
         }
     }
     for(unsigned i=0; i<(unsigned)h_mc->GetNbinsX(); ++i){
         for(unsigned j=0; j<(unsigned)h_mc->GetNbinsY(); ++j){
           double content = h_mc->GetBinContent(i+1,j+1);
+          double error = h_mc->GetBinError(i+1,j+1);
           h3_mc_normxbins->SetBinContent(cati+1,i+1,j+1,content);
+          h3_mc_normxbins->SetBinError(cati+1,i+1,j+1,error);
         }
     }
 }
 
-//TH3D *h3_data_statsup = new TH3D("znjetmasspt_histo_statsup","znjetmasspt_histo_statsup", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_mc_statsup = new TH3D("znjetmasspt_histo_statsup","znjetmasspt_histo_statsup", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_data_normxbins_statsup = new TH3D("znjetmasspt_histo_normxbins_statsup","znjetmasspt_histo_normxbins_statsup", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_mc_normxbins_statsup = new TH3D("znjetmasspt_histo_normxbins_statsup","znjetmasspt_histo_normxbins_statsup", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_data_statsdown = new TH3D("znjetmasspt_histo_statsdown","znjetmasspt_histo_statsdown", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_mc_statsdown = new TH3D("znjetmasspt_histo_statsdown","znjetmasspt_histo_statsdown", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_data_normxbins_statsdown = new TH3D("znjetmasspt_histo_normxbins_statsdown","znjetmasspt_histo_normxbins_statsdown", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//TH3D *h3_mc_normxbins_statsdown = new TH3D("znjetmasspt_histo_normxbins_statsdown","znjetmasspt_histo_normxbins_statsdown", n_xbins, x_bins, n_ybins, y_bins, n_zbins, z_bins);
-//
-//h3_data_statsup = h3_data->Clone();
-//h3_mc_statsup = h3_mc->Clone();
-//h3_data_normxbins_statsup = h3_data_normxbins->Clone();
-//h3_mc_normxbins_statsup = h3_mc_normxbins->Clone();
-//h3_data_statsdown = h3_data->Clone();
-//h3_mc_statsdown = h3_mc->Clone();
-//h3_data_normxbins_statsdown = h3_data_normxbins->Clone();
-//h3_mc_normxbins_statsdown = h3_mc_normxbins->Clone();
-//
-//for (unsigned i=0; i<(unsigned)h3_data_statsup->GetNbinsX(); ++i){
-//  for (unsigned j=0; j<(unsigned)h3_data_statsup->GetNbinsY(); ++j){
-//    for (unsigned k=0; k<(unsigned)h3_data_statsup->GetNbinsZ(); ++k){
-//      h3_data_statsup             ->SetBinContent(h3_data_statsup             ->GetBinContent(i+1,j+1,k+1)  +       h3_data_statsup             ->GetBinError(i+1,j+1,k+1) );  
-//      h3_mc_statsup               ->SetBinContent(h3_mc_statsup               ->GetBinContent(i+1,j+1,k+1)  +       h3_mc_statsup               ->GetBinError(i+1,j+1,k+1) );  
-//      h3_data_normxbins_statsup   ->SetBinContent(h3_data_normxbins_statsup   ->GetBinContent(i+1,j+1,k+1)  +       h3_data_normxbins_statsup   ->GetBinError(i+1,j+1,k+1) );  
-//      h3_mc_normxbins_statsup     ->SetBinContent(h3_mc_normxbins_statsup     ->GetBinContent(i+1,j+1,k+1)  +       h3_mc_normxbins_statsup     ->GetBinError(i+1,j+1,k+1) );  
-//      h3_data_statsdown           ->SetBinContent(h3_data_statsdown           ->GetBinContent(i+1,j+1,k+1)  -       h3_data_statsdown           ->GetBinError(i+1,j+1,k+1) );  
-//      h3_mc_statsdown             ->SetBinContent(h3_mc_statsdown             ->GetBinContent(i+1,j+1,k+1)  -       h3_mc_statsdown             ->GetBinError(i+1,j+1,k+1) );  
-//      h3_data_normxbins_statsdown ->SetBinContent(h3_data_normxbins_statsdown ->GetBinContent(i+1,j+1,k+1)  -       h3_data_normxbins_statsdown ->GetBinError(i+1,j+1,k+1) );  
-//      h3_mc_normxbins_statsdown   ->SetBinContent(h3_mc_normxbins_statsdown   ->GetBinContent(i+1,j+1,k+1)  -       h3_mc_normxbins_statsdown   ->GetBinError(i+1,j+1,k+1) );  
-//  }          
-//}
-//
-//for (unsigned i=0; i<(unsigned)h3_data_normxbins_statsup->GetNbinsX(); ++i){
-//  for (unsigned j=0; j<(unsigned)h3_data_normxbins_statsup->GetNbinsY(); ++j){
-//    for (unsigned k=0; k<(unsigned)h3_data_normxbins_statsup->GetNbinsZ(); ++k){
-//      h3_data_normxbins_statsup  ->SetBinContent(i+1,j+1,k+1, h3_data_normxbins_statsup   ->GetBinContent(i+1,j+1,k+1)/h3_data_normxbins_statsup   ->Integral(i,i,1,-1,1,-1)));
-//      h3_mc_normxbins_statsup    ->SetBinContent(i+1,j+1,k+1, h3_mc_normxbins_statsup     ->GetBinContent(i+1,j+1,k+1)/h3_mc_normxbins_statsup     ->Integral(i,i,1,-1,1,-1)));
-//      h3_data_normxbins_statsdown->SetBinContent(i+1,j+1,k+1, h3_data_normxbins_statsdown ->GetBinContent(i+1,j+1,k+1)/h3_data_normxbins_statsdown ->Integral(i,i,1,-1,1,-1)));
-//      h3_mc_normxbins_statsdown  ->SetBinContent(i+1,j+1,k+1, h3_mc_normxbins_statsdown   ->GetBinContent(i+1,j+1,k+1)/h3_mc_normxbins_statsdown   ->Integral(i,i,1,-1,1,-1)));     
-//    }
-//  }
-//}
-
-//for (unsigned i=0; i<(unsigned)h3_data->GetNbinsX(); ++i){
-//  for (unsigned j=0; j<(unsigned)h3_data->GetNbinsY(); ++j){
-//    for (unsigned k=0; k<(unsigned)h3_data->GetNbinsZ(); ++k){
-//      double n_jets = h3_data->GetXaxis()->GetBinLowEdge(i+1);    
-//      double mass   = h3_data->GetYaxis()->GetBinLowEdge(j+1);
-//      double pt     = h3_data->GetZaxis()->GetBinLowEdge(k+1);
-//      double weight = h3_data->GetBinContent(i+1,j+1,k+1);
-//      std::cout << "n_jets = " << n_jets << ", mass = " << mass << ", pt = " << pt << ", weight = " << weight << std::endl;
-//    }
-//  }
-//}
+//Normalize histograms - *_normxbins histogram is normalized seperatly in jet bins so that re-weighting leaved n_jets distribution un-changed
 
 double h3_data_total_0jet = 0;
 double h3_data_total_1jet = 0;
@@ -187,11 +158,13 @@ for (unsigned i=0; i<(unsigned)h3_data->GetNbinsX(); ++i){
   for (unsigned j=0; j<(unsigned)h3_data->GetNbinsX(); ++j){
     for (unsigned k=0; k<(unsigned)h3_data->GetNbinsZ(); ++k){
        double content = h3_data->GetBinContent(i+1,j+1,k+1);
+       double error = h3_data->GetBinError(i+1,j+1,k+1);
        if(!(j==0 || j==h3_data->GetNbinsY()-1 ||k==h3_data->GetNbinsZ()-1)){
          h3_data->SetBinContent(i+1,j+1,k+1,content/h3_data_total);
-         if(i==0) h3_data_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_data_total_0jet);
-         if(i==1) h3_data_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_data_total_1jet);
-         if(i==2) h3_data_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_data_total_2jet);
+         h3_data->SetBinError(i+1,j+1,k+1,error/h3_data_total);
+         if(i==0){ h3_data_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_data_total_0jet); h3_data_normxbins->SetBinError(i+1,j+1,k+1,error/h3_data_total_0jet); }
+         if(i==1){ h3_data_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_data_total_1jet); h3_data_normxbins->SetBinError(i+1,j+1,k+1,error/h3_data_total_1jet); }
+         if(i==2){ h3_data_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_data_total_2jet); h3_data_normxbins->SetBinError(i+1,j+1,k+1,error/h3_data_total_2jet); }
        }
     }
   }
@@ -218,56 +191,53 @@ for (unsigned i=0; i<(unsigned)h3_mc->GetNbinsX(); ++i){
   for (unsigned j=0; j<(unsigned)h3_mc->GetNbinsX(); ++j){
     for (unsigned k=0; k<(unsigned)h3_mc->GetNbinsZ(); ++k){
        double content = h3_mc->GetBinContent(i+1,j+1,k+1);
+       double error = h3_mc->GetBinError(i+1,j+1,k+1);
        if(!(j==0 || j==h3_mc->GetNbinsY()-1 ||k==h3_mc->GetNbinsZ()-1)){
          h3_mc->SetBinContent(i+1,j+1,k+1,content/h3_mc_total);
-         if(i==0) h3_mc_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_mc_total_0jet);
-         if(i==1) h3_mc_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_mc_total_1jet);
-         if(i==2) h3_mc_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_mc_total_2jet);
+         h3_mc->SetBinError(i+1,j+1,k+1,error/h3_mc_total);
+         if(i==0){ h3_mc_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_mc_total_0jet); h3_mc_normxbins->SetBinError(i+1,j+1,k+1,error/h3_mc_total_0jet); }
+         if(i==1){ h3_mc_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_mc_total_1jet); h3_mc_normxbins->SetBinError(i+1,j+1,k+1,error/h3_mc_total_1jet); }
+         if(i==2){ h3_mc_normxbins->SetBinContent(i+1,j+1,k+1,content/h3_mc_total_2jet); h3_mc_normxbins->SetBinError(i+1,j+1,k+1,error/h3_mc_total_2jet); }
        }
     }
   }
 }
 
-//h3_data->Scale(1/h3_data->Integral());
-//h3_mc->Scale(1/h3_mc->Integral());
-
-//h3_data_normxbins->Scale(1/h3_data_normxbins->Integral());
-//h3_mc_normxbins->Scale(1/h3_mc_normxbins->Integral());
-
+// Divide to get weights and save to file
 h3_data->Divide(h3_mc);
 h3_data_normxbins->Divide(h3_mc_normxbins);
 
-TFile *fout = new TFile("z_njet_mass_pt_weights_3D_v2.root","RECREATE");
+TFile *fout = new TFile(outfile.c_str(),"RECREATE");
 fout->cd();
 
-
+h_2dweights->Write();
 h3_data->Write();      
 h3_data_normxbins->Write();
 
-for (unsigned i=0; i<(unsigned)h3_data->GetNbinsX(); ++i){
-  for (unsigned j=0; j<(unsigned)h3_data->GetNbinsY(); ++j){
-    for (unsigned k=0; k<(unsigned)h3_data->GetNbinsZ(); ++k){
-      double n_jets = h3_data->GetXaxis()->GetBinLowEdge(i+1);    
-      double mass   = h3_data->GetYaxis()->GetBinLowEdge(j+1);
-      double pt     = h3_data->GetZaxis()->GetBinLowEdge(k+1);
-      double weight = h3_data->GetBinContent(i+1,j+1,k+1);
-      std::cout << "n_jets = " << n_jets << ", mass = " << mass << ", pt = " << pt << ", weight = " << weight << std::endl;
-    }
-  }
-}
-std::cout << "-------------------------" << std::endl;
-
-for (unsigned i=0; i<(unsigned)h3_data_normxbins->GetNbinsX(); ++i){
-  for (unsigned j=0; j<(unsigned)h3_data_normxbins->GetNbinsY(); ++j){
-    for (unsigned k=0; k<(unsigned)h3_data_normxbins->GetNbinsZ(); ++k){
-      double n_jets = h3_data_normxbins->GetXaxis()->GetBinLowEdge(i+1);    
-      double mass   = h3_data_normxbins->GetYaxis()->GetBinLowEdge(j+1);
-      double pt     = h3_data_normxbins->GetZaxis()->GetBinLowEdge(k+1);
-      double weight = h3_data_normxbins->GetBinContent(i+1,j+1,k+1);
-      std::cout << "n_jets = " << n_jets << ", mass = " << mass << ", pt = " << pt << ", weight = " << weight << std::endl;
-    }
-  }
-}
+//for (unsigned i=0; i<(unsigned)h3_data->GetNbinsX(); ++i){
+//  for (unsigned j=0; j<(unsigned)h3_data->GetNbinsY(); ++j){
+//    for (unsigned k=0; k<(unsigned)h3_data->GetNbinsZ(); ++k){
+//      double n_jets = h3_data->GetXaxis()->GetBinLowEdge(i+1);    
+//      double mass   = h3_data->GetYaxis()->GetBinLowEdge(j+1);
+//      double pt     = h3_data->GetZaxis()->GetBinLowEdge(k+1);
+//      double weight = h3_data->GetBinContent(i+1,j+1,k+1);
+//      std::cout << "n_jets = " << n_jets << ", mass = " << mass << ", pt = " << pt << ", weight = " << weight << std::endl;
+//    }
+//  }
+//}
+//std::cout << "-------------------------" << std::endl;
+//
+//for (unsigned i=0; i<(unsigned)h3_data_normxbins->GetNbinsX(); ++i){
+//  for (unsigned j=0; j<(unsigned)h3_data_normxbins->GetNbinsY(); ++j){
+//    for (unsigned k=0; k<(unsigned)h3_data_normxbins->GetNbinsZ(); ++k){
+//      double n_jets = h3_data_normxbins->GetXaxis()->GetBinLowEdge(i+1);    
+//      double mass   = h3_data_normxbins->GetYaxis()->GetBinLowEdge(j+1);
+//      double pt     = h3_data_normxbins->GetZaxis()->GetBinLowEdge(k+1);
+//      double weight = h3_data_normxbins->GetBinContent(i+1,j+1,k+1);
+//      std::cout << "n_jets = " << n_jets << ", mass = " << mass << ", pt = " << pt << ", weight = " << weight << std::endl;
+//    }
+//  }
+//}
 
 }
 
--- Analysis/HiggsTauTau/scripts/run_Z_weights_summer16.sh
+++ Analysis/HiggsTauTau/scripts/run_Z_weights_summer16.sh
@@ -1,4 +1,4 @@
-cats=("0jet" "1jet" "ge2jet")
+cats=("inclusive" "0jet" "1jet" "ge2jet")
 for cat in "${cats[@]}"; do
   if [ $cat == "0jet" ]; then
     bins1="0,40,80,600"
@@ -17,22 +17,22 @@ for cat in "${cats[@]}"; do
     bins4="0,120,200,600"
   fi
   
-  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>50 && m_vis<80)"  --method=8 --cat="$cat" --var="pt_tt[$bins1]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down
+  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>50 && m_vis<80)"  --method=8 --cat="$cat" --var="pt_tt[$bins1]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down --syst_scale_j=JES
   mv datacard_pt_tt_$cat"_zmm_2016".root datacard_pt_tt_$cat"_zmm_2016_mvis50to80.root"
   mv pt_tt_$cat"_zmm_2016_logy.png" pt_tt_$cat"_zmm_2016_logy_mvis50to80.png"
   mv pt_tt_$cat"_zmm_2016_logy.pdf" pt_tt_$cat"_zmm_2016_logy_mvis50to80.pdf"
   
-  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>80 && m_vis<120)"  --method=8 --cat="$cat" --var="pt_tt[$bins2]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down
+  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>80 && m_vis<120)"  --method=8 --cat="$cat" --var="pt_tt[$bins2]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down --syst_scale_j=JES
   mv datacard_pt_tt_$cat"_zmm_2016.root" datacard_pt_tt_$cat"_zmm_2016_mvis80to120.root"
   mv pt_tt_$cat"_zmm_2016_logy.png" pt_tt_$cat"_zmm_2016_logy_mvis80to120.png"
   mv pt_tt_$cat"_zmm_2016_logy.pdf" pt_tt_$cat"_zmm_2016_logy_mvis80to120.pdf"
   
-  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>120 && m_vis<200)"  --method=8 --cat="$cat" --var="pt_tt[$bins3]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down
+  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>120 && m_vis<200)"  --method=8 --cat="$cat" --var="pt_tt[$bins3]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down --syst_scale_j=JES
   mv datacard_pt_tt_$cat"_zmm_2016.root" datacard_pt_tt_$cat"_zmm_2016_mvis120to200.root"
   mv pt_tt_$cat"_zmm_2016_logy.png" pt_tt_$cat"_zmm_2016_logy_mvis120to200.png"
   mv pt_tt_$cat"_zmm_2016_logy.pdf" pt_tt_$cat"_zmm_2016_logy_mvis120to200.pdf"
   
-  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>200 && m_vis<800)"  --method=8 --cat="$cat" --var="pt_tt[$bins4]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down
+  python scripts/HiggsTauTauPlot.py --cfg=scripts/new_plot_mssm_2016_NewPlotting.cfg --channel=zmm --set_alias="sel:(m_vis>200 && m_vis<800)"  --method=8 --cat="$cat" --var="pt_tt[$bins4]"  --x_title="Z pt [GeV]" --y_title="Entries" --datacard="$cat" --extra_pad=0.5 --log_y --outputfolder="./" --ratio --add_wt=wt_zpt_down --syst_scale_j=JES
   mv datacard_pt_tt_$cat"_zmm_2016.root" datacard_pt_tt_$cat"_zmm_2016_mvis200to800.root"
   mv pt_tt_$cat"_zmm_2016_logy.png" pt_tt_$cat"_zmm_2016_logy_mvis200to800.png"
   mv pt_tt_$cat"_zmm_2016_logy.pdf" pt_tt_$cat"_zmm_2016_logy_mvis200to800.pdf"
