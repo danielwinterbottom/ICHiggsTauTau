150162703d813b52bac48b0dad78ba184d0a6d11
Daniel <d.winterbottom15@imperial.ac.uk>
2017-04-26 12:14:18 +0100

    updating to use latest FF inputs - FF inputs should now be added to work area directly from gitlab area

--- Analysis/HiggsTauTau/src/HTTFakeFactorWeights.cc
+++ Analysis/HiggsTauTau/src/HTTFakeFactorWeights.cc
@@ -38,13 +38,11 @@ namespace ic {
     boost::split(category_names_, categories_, boost::is_any_of(","), boost::token_compress_on);
     std::string baseDir = (std::string)getenv("CMSSW_BASE") + "/src/";
     
-    std::string file_name = "";
-    if(strategy_ == strategy::smspring16) file_name = "fakeFactors_20170111.root";
-    else if (strategy_ == strategy::mssmsummer16) file_name = "fakeFactors_20170330_tight.root";
-    
     std::string channel = Channel2String(channel_);
     for(unsigned i=0; i<category_names_.size(); ++i){
-      std::string ff_file_name = "UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/input/fake_factors/"+channel+"/"+category_names_[i]+"/"+file_name; 
+      std::string ff_file_name="";
+      if (channel == "tt") ff_file_name = "UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/input/fake_factors/Jet2TauFakesFiles/20170423_tight/tt/"+category_names_[i]+"/fakeFactors_20170423_tight.root";
+      else ff_file_name = "UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/input/fake_factors/Jet2TauFakesFiles/20170330_tight/"+channel+"/"+category_names_[i]+"/fakeFactors_20170330_tight.root";
       ff_file_name = baseDir + ff_file_name;
       TFile* ff_file = new TFile(ff_file_name.c_str());
       FakeFactor* ff = (FakeFactor*)ff_file->Get("ff_comb");
@@ -104,8 +102,9 @@ namespace ic {
     if(channel_ == channel::et || channel_ == channel::mt){
       inputs[0] = pt_2_; inputs[1] = tau_decaymode_2_; inputs[2] = n_jets_; inputs[3] = m_vis_; inputs[4] = mt_1_; inputs[5] = iso_1_;    
     } else if (channel_ == channel::tt){
-      tt_inputs_1[0] = pt_1_; tt_inputs_1[1] = pt_2_; tt_inputs_1[2] = tau_decaymode_1_; tt_inputs_1[3] = n_jets_; tt_inputs_1[4] = m_vis_; tt_inputs_1[5] = 0;
-      tt_inputs_2[0] = pt_2_; tt_inputs_2[1] = pt_1_; tt_inputs_2[2] = tau_decaymode_2_; tt_inputs_2[3] = n_jets_; tt_inputs_2[4] = m_vis_; tt_inputs_2[5] = 0;
+      double mt_tot_ = sqrt(pow(MT(lep1, met),2) + pow(MT(lep2, met),2) + pow(MT(lep1, met),2));  
+      tt_inputs_1[0] = pt_1_; tt_inputs_1[1] = pt_2_; tt_inputs_1[2] = tau_decaymode_1_; tt_inputs_1[3] = n_jets_; tt_inputs_1[4] = m_vis_; tt_inputs_1[5] = mt_tot_;
+      tt_inputs_2[0] = pt_2_; tt_inputs_2[1] = pt_1_; tt_inputs_2[2] = tau_decaymode_2_; tt_inputs_2[3] = n_jets_; tt_inputs_2[4] = m_vis_; tt_inputs_2[5] = mt_tot_;
     }
     
     // Need to loop over all categories and add a ff weight to the event for each
