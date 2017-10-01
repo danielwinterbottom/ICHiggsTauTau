72f8baf4d5b24cb865ca556073d07ba00cbf7953
danielwinterbottom <daniel.winterbottom@cern.ch>
2017-10-01 17:22:06 +0100

    adding commented line to switch to VBF sample

--- Analysis/HiggsTauTau/scripts/CP_plots_reweighted.C
+++ Analysis/HiggsTauTau/scripts/CP_plots_reweighted.C
@@ -4,12 +4,10 @@ void GenCPPlots(std::string outfile_name, std::string channel, int cp_chan){
   std::string dir_name = "/vols/cms/dw515/CPStudies/Gen/";
       
   TFile *f1 = new TFile((dir_name+"GluGluToHToTauTau_M-125_tt_2016.root").c_str());    
-  TFile *f2 = new TFile((dir_name+"SUSYGluGluToHToTauTau_M-120_tt_2016.root").c_str());    
-  TFile *f3 = new TFile((dir_name+"DYJetsToLL_tt_2016.root").c_str());
+  //TFile *f1 = new TFile((dir_name+"VBFHToTauTau_M-125_tt_2016.root").c_str());
+
   
   TTree *t1 = (TTree*)f1->Get("gen_ntuple");
-  TTree *t2 = (TTree*)f1->Get("gen_ntuple");
-  TTree *t3 = (TTree*)f1->Get("gen_ntuple");    
   TH1D *h = new TH1D();
   std::string plot_string = "aco_angle_1>>h";
   std::string binning = "(20,0,6.3)";
@@ -26,11 +24,11 @@ void GenCPPlots(std::string outfile_name, std::string channel, int cp_chan){
   h = (TH1D*)gDirectory->Get("h");
   h->SetName("even_1");
   h->Write();
-  t2->Draw((plot_string+binning).c_str(),(cut_string_1+"*wt_cp_0p5/wt_cp_0").c_str(),"goff");
+  t1->Draw((plot_string+binning).c_str(),(cut_string_1+"*wt_cp_0p5/wt_cp_0").c_str(),"goff");
   h = (TH1D*)gDirectory->Get("h");
   h->SetName("odd_1");
   h->Write();
-  t3->Draw((plot_string+binning).c_str(),(cut_string_1+"*1/wt_cp_0").c_str(),"goff");
+  t1->Draw((plot_string+binning).c_str(),(cut_string_1+"*1/wt_cp_0").c_str(),"goff");
   h = (TH1D*)gDirectory->Get("h");
   h->SetName("none_1");
   h->Clone()->Write();
@@ -39,15 +37,15 @@ void GenCPPlots(std::string outfile_name, std::string channel, int cp_chan){
   if(cp_chan>1){
     std::string cut_string_2 = "wt*("+cp_channel+")*(channel==\""+channel+"\")";
     cut_string_2+="*(cp_sign_1>0)";      
-    t1->Draw((plot_string+binning).c_str(),cut_string_2.c_str(),"goff");
+    t1->Draw((plot_string+binning).c_str(),(cut_string_2).c_str(),"goff");
     h = (TH1D*)gDirectory->Get("h");
     h->SetName("even_2");
     h->Write();
-    t2->Draw((plot_string+binning).c_str(),cut_string_2.c_str(),"goff");
+    t1->Draw((plot_string+binning).c_str(),(cut_string_2+"*wt_cp_0p5/wt_cp_0").c_str(),"goff");
     h = (TH1D*)gDirectory->Get("h");
     h->SetName("odd_2");
     h->Write();
-    t3->Draw((plot_string+binning).c_str(),cut_string_2.c_str(),"goff");
+    t1->Draw((plot_string+binning).c_str(),(cut_string_2+"*1/wt_cp_0").c_str(),"goff");
     h = (TH1D*)gDirectory->Get("h");
     h->SetName("none_2");
     h->Write();   
