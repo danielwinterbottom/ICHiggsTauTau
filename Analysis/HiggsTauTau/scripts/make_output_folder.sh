3d0b311e8f422f91c66a4e0236da7a372efaa366
danielwinterbottom <daniel.winterbottom@cern.ch>
2017-11-09 10:51:00 +0000

    making directories needed for mu->tau ES uncerts

--- Analysis/HiggsTauTau/scripts/make_output_folder.sh
+++ Analysis/HiggsTauTau/scripts/make_output_folder.sh
@@ -54,6 +54,10 @@ mkdir -p -v $1/EFAKE0PI_DOWN
 mkdir -p -v $1/EFAKE0PI_UP
 mkdir -p -v $1/EFAKE1PI_DOWN
 mkdir -p -v $1/EFAKE1PI_UP
+mkdir -p -v $1/MUFAKE0PI_DOWN
+mkdir -p -v $1/MUFAKE0PI_UP
+mkdir -p -v $1/MUFAKE1PI_DOWN
+mkdir -p -v $1/MUFAKE1PI_UP
 
 if [ $2 ]; then
   mkdir -p -v $1/JES_1_DOWN
