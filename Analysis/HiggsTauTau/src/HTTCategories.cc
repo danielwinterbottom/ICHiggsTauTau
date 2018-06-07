532b1dd65e6531b70f8f87a472d398172875e31f
Albert <dow.albert@gmail.com>
2018-06-07 16:12:46 +0100

    adding some variables to trees

--- Analysis/HiggsTauTau/src/HTTCategories.cc
+++ Analysis/HiggsTauTau/src/HTTCategories.cc
@@ -73,6 +73,8 @@ namespace ic {
       
       outtree_->Branch("event",             &event_);
       outtree_->Branch("npu",               &n_pu_, "n_pu/F");
+      outtree_->Branch("rho",               &rho_, "rho/F");
+      outtree_->Branch("puweight",          &pu_weight_, "pu_weight/F");
       outtree_->Branch("wt",                &wt_.var_double);
       outtree_->Branch("wt_btag",           &wt_btag_);
       outtree_->Branch("wt_tau_id_loose", &wt_tau_id_loose_);
