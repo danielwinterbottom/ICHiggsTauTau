d30008c4c2f701f3eedde40ac5732315e31ea698
Albert <dow.albert@gmail.com>
2018-06-20 13:10:34 +0100

    edits in Btag efficiency script

--- Analysis/HiggsTauTau/scripts/calculateBTagEff.py
+++ Analysis/HiggsTauTau/scripts/calculateBTagEff.py
@@ -16,114 +16,73 @@ def btag_eff(tree, channel, name, norm):
     hist_num_1 = ROOT.TH1F("hist_num_1","hist_num_1",10,0,150)
     hist_denom_1 = ROOT.TH1F("hist_denom_1","hist_denom_1",10,0,150)
     if channel == "mt":
-        tree.Draw("(1)>>hist_num_1", "wt*({} & n_bjets==0)".format(mt_baseline))
+        tree.Draw("(1)>>hist_num_1", "wt*({} && n_bjets==0)".format(mt_baseline))
         tree.Draw("(1)>>hist_denom_1", "wt*{}".format(mt_baseline))
     if channel == "et":
-        tree.Draw("(1)>>hist_num_1", "wt*({} & n_bjets==0)".format(et_baseline))
-        tree.Draw("(1)>>hist_denom_1", "{}".format(et_baseline))
+        tree.Draw("(1)>>hist_num_1", "wt*({} && n_bjets==0)".format(et_baseline))
+        tree.Draw("(1)>>hist_denom_1", "wt*{}".format(et_baseline))
     if channel == "tt":
-        tree.Draw("(1)>>hist_num_1", "wt*({} & n_bjets==0)".format(tt_baseline))
+        tree.Draw("(1)>>hist_num_1", "wt*({} && n_bjets==0)".format(tt_baseline))
         tree.Draw("(1)>>hist_denom_1", "wt*{}".format(tt_baseline))
 
-    pEff = ROOT.TEfficiency(hist_num_1, hist_denom_1)
     if norm is not "":
-        pEff.SetWeight(norm)
+        hist_num_1.Scale(norm)
+        hist_denom_1.Scale(norm)
+
+    numerator = hist_num_1.Integral(-1,-1)
+    denominator = hist_denom_1.Integral(-1,-1)
+
+    basic_eff = float(numerator/denominator)
+    print "Division of integrals: {}".format(basic_eff)
+        
+    pEff = ROOT.TEfficiency(hist_num_1, hist_denom_1)
     print "\nEfficiency: {}\n".format(pEff.GetEfficiency(1))
     
     pEff.Draw()
     c1.SaveAs("{}_btag_eff_CSV_{}.pdf".format(name, channel))
 
+    return [numerator, denominator]
+
 for channel in channels:
     print "\n{} channel".format(channel)
+
+    # TT Hadronic
     f_bkg_tthad = ROOT.TFile("{}/TTToHadronic_{}_2017.root".format(path, channel), "READ")
     tree_bkg_tthad = f_bkg_tthad.Get("ntuple")
 
-    btag_eff(tree_bkg_tthad, channel, "tthad", tthad_wt)
-
-    # if channel == "mt":
-    #     num_1 = float(tree_bkg_tthad.GetEntries("{} && n_bjets==0".format(mt_baseline)))
-    #     denom_1 = float(tree_bkg_tthad.GetEntries("{}".format(mt_baseline)))
-    # elif channel == "et":
-    #     num_1 = float(tree_bkg_tthad.GetEntries("{} && n_bjets==0".format(et_baseline)))
-    #     denom_1 = float(tree_bkg_tthad.GetEntries("{}".format(et_baseline)))
-    # elif channel == "tt":
-    #     num_1 = float(tree_bkg_tthad.GetEntries("{} && n_bjets==0".format(tt_baseline)))
-    #     denom_1 = float(tree_bkg_tthad.GetEntries("{}".format(tt_baseline)))
+    tthad_nums = btag_eff(tree_bkg_tthad, channel, "tthad", tthad_wt)
 
     f_bkg_tthad.Close()
 
-    # print num_1 
-    # print denom_1
-    # efficiency_tthad = float(num_1/denom_1)
-    # print "Efficiency for TTToHadronic {} channel: {}".format(channel, efficiency_tthad)
-
+    # TT SemiLeptonic
     f_bkg_ttsemilep = ROOT.TFile("{}/TTToSemiLeptonic_{}_2017.root".format(path, channel), "READ")
     tree_bkg_ttsemilep = f_bkg_ttsemilep.Get("ntuple")
-    btag_eff(tree_bkg_ttsemilep, channel, "ttsemilep", ttsemi_wt)
-
-    # if channel == "mt":
-    #     num_2 = float(tree_bkg_ttsemilep.GetEntries("{} && n_bjets==0".format(mt_baseline)))
-    #     denom_2 = float(tree_bkg_ttsemilep.GetEntries("{}".format(mt_baseline)))
-    # elif channel == "et":
-    #     num_2 = float(tree_bkg_ttsemilep.GetEntries("{} && n_bjets==0".format(et_baseline)))
-    #     denom_2 = float(tree_bkg_ttsemilep.GetEntries("{}".format(et_baseline)))
-    # elif channel == "tt":
-    #     num_2 = float(tree_bkg_ttsemilep.GetEntries("{} && n_bjets==0".format(tt_baseline)))
-    #     denom_2 = float(tree_bkg_ttsemilep.GetEntries("{}".format(tt_baseline)))
+    
+    ttsemilep_nums = btag_eff(tree_bkg_ttsemilep, channel, "ttsemilep", ttsemi_wt)
 
     f_bkg_ttsemilep.Close()
 
-    # print num_2 
-    # print denom_2
-    # efficiency_ttsemi = float(num_2/denom_2)
-    # print "Efficiency for TTToSemiLeptonic {} channel: {}".format(channel, efficiency_ttsemi)
-
+    # TT Leptonic
     f_bkg_ttlep = ROOT.TFile("{}/TTTo2L2Nu_{}_2017.root".format(path, channel), "READ")
     tree_bkg_ttlep = f_bkg_ttlep.Get("ntuple")
-    btag_eff(tree_bkg_ttlep, channel, "ttlep", ttlep_wt)
-
-    # if channel == "mt":
-    #     num_3 = float(tree_bkg_ttlep.GetEntries("{} && n_bjets==0".format(mt_baseline)))
-    #     denom_3 = float(tree_bkg_ttlep.GetEntries("{}".format(mt_baseline)))
-    # elif channel == "et":
-    #     num_3 = float(tree_bkg_ttlep.GetEntries("{} && n_bjets==0".format(et_baseline)))
-    #     denom_3 = float(tree_bkg_ttlep.GetEntries("{}".format(et_baseline)))
-    # elif channel == "tt":
-    #     num_3 = float(tree_bkg_ttlep.GetEntries("{} && n_bjets==0".format(tt_baseline)))
-    #     denom_3 = float(tree_bkg_ttlep.GetEntries("{}".format(tt_baseline)))
+    
+    ttlep_nums = btag_eff(tree_bkg_ttlep, channel, "ttlep", ttlep_wt)
 
     f_bkg_ttlep.Close()
 
-#     print num_3 
-#     print denom_3
-#     efficiency_ttlep = float(num_3/denom_3)
-#     print "Efficiency for TTTo2L2Nu {} channel: {}".format(channel, efficiency_ttlep)
+    
+    # Average of TT split by decay
+    scaled_numerator = tthad_nums[0] + ttsemilep_nums[0] + ttlep_nums[0]
+    scaled_denominator = tthad_nums[1] + ttsemilep_nums[1] + ttlep_nums[1]
 
-    # now do weighted average of these three values
+    scaled_tt_eff = float(scaled_numerator/scaled_denominator)
+    print "TT efficiency: {}".format(scaled_tt_eff)
 
-    # weighted_eff = (efficiency_tthad * tthad_wt + efficiency_ttsemi * ttsemi_wt + efficiency_ttlep * ttlep_wt)/(tthad_wt + ttsemi_wt + ttlep_wt)
-    # print "Efficiency for TT samples weighted by xs is: {}".format(weighted_eff)
-
-
+    # ggH 
     f_sig = ROOT.TFile("{}/GluGluHToTauTau_M-125_{}_2017.root".format(path, channel), "READ")
     tree_sig = f_sig.Get("ntuple")
 
     btag_eff(tree_sig, channel, "ggh", "")
 
-    # if channel == "mt":
-    #     num = float(tree_sig.GetEntries("{} && n_bjets==0".format(mt_baseline)))
-    #     denom = float(tree_sig.GetEntries("{}".format(mt_baseline)))
-    # elif channel == "et":
-    #     num = float(tree_sig.GetEntries("{} && n_bjets==0".format(et_baseline)))
-    #     denom = float(tree_sig.GetEntries("{}".format(et_baseline)))
-    # elif channel == "tt":
-    #     num = float(tree_sig.GetEntries("{} && n_bjets==0".format(tt_baseline)))
-    #     denom = float(tree_sig.GetEntries("{}".format(tt_baseline)))
-
     f_sig.Close()
     
-    # print num 
-    # print denom
-    # efficiency = float(num/denom)
-    # print "Efficiency for ggH {} channel: {}".format(channel, efficiency)
-
