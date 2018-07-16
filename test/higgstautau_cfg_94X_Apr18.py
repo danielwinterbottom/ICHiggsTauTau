496670f3f3a9ae1c97cc89320eef6a6792474f32
Albert <dow.albert@gmail.com>
2018-07-16 09:15:02 +0100

    adding e scale and smear floats to cfg

--- test/higgstautau_cfg_94X_Apr18.py
+++ test/higgstautau_cfg_94X_Apr18.py
@@ -10,10 +10,10 @@ opts = parser.VarParsing ('analysis')
 
 opts.register('file', 
 # 'root://xrootd.unl.edu//store/mc/RunIISummer17MiniAOD/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/92X_upgrade2017_realistic_v10_ext1-v2/10000/00F9D855-E293-E711-B625-02163E014200.root'              
-'root://xrootd.unl.edu///store/data/Run2017B/SingleMuon/MINIAOD/31Mar2018-v1/80000/248C8431-B838-E811-B418-0025905B85D2.root'
+'root://xrootd.unl.edu//store/data/Run2017E/SingleElectron/MINIAOD/31Mar2018-v1/90000/24DC3796-C338-E811-BA7E-00266CFFBEB4.root'
 ,parser.VarParsing.multiplicity.singleton, 
 parser.VarParsing.varType.string, "input file")
-opts.register('globalTag', '94X_dataRun2_v6', parser.VarParsing.multiplicity.singleton, ## lates GT i can find for MC = 94X_mc2017_realistic_v14
+opts.register('globalTag', '94X_dataRun2_v10', parser.VarParsing.multiplicity.singleton, ## lates GT i can find for MC = 94X_mc2017_realistic_v14
     parser.VarParsing.varType.string, "global tag")
 opts.register('isData', 1, parser.VarParsing.multiplicity.singleton,
     parser.VarParsing.varType.int, "Process as data?")
@@ -346,6 +346,10 @@ process.electronPFIsolationValuesSequence +=cms.Sequence(
   process.elHcalPFClusterIso
 )
 
+import RecoEgamma.EgammaTools.calibratedEgammas_cff
+calibratedPatElectrons94Xv1 = RecoEgamma.EgammaTools.calibratedEgammas_cff.calibratedPatElectrons.clone(
+    produceCalibratedObjs = False
+    )
 
 process.icElectronProducer = producers.icElectronProducer.clone(
   branch                    = cms.string("electrons"),
@@ -360,6 +364,11 @@ process.icElectronProducer = producers.icElectronProducer.clone(
      generalPurposeMVASpring16  = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16GeneralPurposeV1Values"),
      mvaRun2Fall17  = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Fall17NoIsoV1Values"),
      mvaRun2IsoFall17  = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Fall17IsoV1Values")
+  ),
+  includeFloats2 = cms.PSet(
+      ecalTrkEnergyPreCorr     = cms.InputTag("selectedElectrons","ecalTrkEnergyPreCorr"),
+      ecalTrkEnergyPostCorr    = cms.InputTag("selectedElectrons","ecalTrkEnergyPostCorr"),
+      ecalTrkEnergyErrPostCorr = cms.InputTag("selectedElectrons","ecalTrkEnergyErrPostCorr")
   ),
   includeClusterIso        = cms.bool(True),
   includePFIso03           = cms.bool(True),
