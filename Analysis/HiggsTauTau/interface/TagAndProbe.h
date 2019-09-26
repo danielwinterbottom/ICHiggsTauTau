b30416e3403f307202d2dbced9c07a1a14e86e99
MohammadHassan <mhhassanshahi@yahoo.com>
2019-09-26 18:15:21 +0100

    adding statement: if photon exist

--- Analysis/HiggsTauTau/interface/TagAndProbe.h
+++ Analysis/HiggsTauTau/interface/TagAndProbe.h
@@ -92,8 +92,6 @@ class TagAndProbe : public ModuleBase {
   
   bool pass_FSR_condition_;
   double m_gamma_muons_;
-  double dR_gamma_muon_1_;
-  double dR_gamma_muon_2_;
 
     std::vector<std::string> SplitString(std::string instring){
     std::vector<std::string> outstrings;
@@ -329,27 +327,28 @@ int TagAndProbe<T>::Execute(TreeEvent *event){
   }
   
  //Add photons to check Final State Radiation in zmm channel in isolation eff.
-    std::vector<Photon*> gammas = event->GetPtrVec<Photon>("Photon");
-    ic::erase_if(gammas, !boost::bind(MinPtMaxEta, _1, 10.0, 999));
-    std::vector<Candidate const*> muon_vec_1 = {lep1};
-    std::vector<Candidate const*> muon_vec_2 = {lep2};
-    std::vector<std::pair <Photon*,Candidate const*> > input1 = MatchByDR(gammas, muon_vec_1, 0.4, false, false);   
-    std::vector<std::pair <Photon*,Candidate const*> > input2 = MatchByDR(gammas, muon_vec_2, 0.4, false, false);    
-    std::sort(input1.begin(), input1.end(), [](std::pair <Photon*,Candidate const*> a,std::pair <Photon*,Candidate const*>  b) {return a.first->pt() > b.first->pt();} );
-    std::sort(input2.begin(), input2.end(), [](std::pair <Photon*,Candidate const*> a,std::pair <Photon*,Candidate const*>  b) {return a.first->pt() > b.first->pt();} );
-
-    pass_FSR_condition_ = false;
-    auto sum_vec = lep1->vector() + lep2->vector();
-    if (input1.size()>0){
-        sum_vec +=  input1[0].first->vector();
-        pass_FSR_condition_=true;
+    if(event->ExistsInTree("Photon")){
+        std::vector<Photon*> gammas = event->GetPtrVec<Photon>("Photon");
+        ic::erase_if(gammas, !boost::bind(MinPtMaxEta, _1, 10.0, 999));
+        std::vector<Candidate const*> muon_vec_1 = {lep1};
+        std::vector<Candidate const*> muon_vec_2 = {lep2};
+        std::vector<std::pair <Photon*,Candidate const*> > input1 = MatchByDR(gammas, muon_vec_1, 0.4, false, false);   
+        std::vector<std::pair <Photon*,Candidate const*> > input2 = MatchByDR(gammas, muon_vec_2, 0.4, false, false);    
+        std::sort(input1.begin(), input1.end(), [](std::pair <Photon*,Candidate const*> a,std::pair <Photon*,Candidate const*>  b) {return a.first->pt() > b.first->pt();} );
+        std::sort(input2.begin(), input2.end(), [](std::pair <Photon*,Candidate const*> a,std::pair <Photon*,Candidate const*>  b) {return a.first->pt() > b.first->pt();} );
+    
+        pass_FSR_condition_ = false;
+        auto sum_vec = lep1->vector() + lep2->vector();
+        if (input1.size()>0){
+            sum_vec +=  input1[0].first->vector();
+            pass_FSR_condition_=true;
+        }
+        if (input2.size()>0){
+            sum_vec += input2[0].first->vector();
+            pass_FSR_condition_=true;
+        }
+        m_gamma_muons_= sum_vec.M();
     }
-    if (input2.size()>0){
-        sum_vec += input2[0].first->vector();
-        pass_FSR_condition_=true;
-    }
-    m_gamma_muons_= sum_vec.M();
-
   if(channel_ == channel::tpzmm){
     if(strategy_ == strategy::mssmsummer16 || strategy_ == strategy::smsummer16 || strategy_ == strategy::cpsummer16 || strategy_ == strategy::legacy16 || strategy_ == strategy::cpdecays16 || strategy_ == strategy::cpsummer17 || strategy_ == strategy::cpdecays17 || strategy_ == strategy::cpdecays18){
       T muon1 = dynamic_cast<T>(lep1);
