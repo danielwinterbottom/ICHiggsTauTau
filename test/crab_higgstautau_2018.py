2592a10d26220cdece814269e520b8536b2c93b3
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2019-10-01 15:17:07 +0100

    latest submission scripts

--- test/crab_higgstautau_2018.py
+++ test/crab_higgstautau_2018.py
@@ -3,7 +3,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.General.transferOutputs = True
-config.General.workArea='Sep05_Data_102X_2018'
+config.General.workArea='Oct01_Data_102X_2018'
 
 config.JobType.psetName = 'higgstautau_cfg_102X_Aug19_2018.py'
 config.JobType.pluginName = 'Analysis'
