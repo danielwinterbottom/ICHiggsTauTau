e024fd47fe5a92c9fabf5125597d91df80d70b17
Albert <dow.albert@gmail.com>
2020-03-06 17:31:54 +0000

    Fix add SVFit files so if multiple svfit output files exist for a sample it works, add sample lists from latest scripts

--- Analysis/HiggsTauTau/scripts/batch_addSVFits.sh
+++ Analysis/HiggsTauTau/scripts/batch_addSVFits.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-cd /vols/build/cms/akd116/newest/CMSSW_10_2_9/src/
+cd /vols/build/cms/akd116/newest/CMSSW_10_2_14/src/
 export SCRAM_ARCH=slc6_amd64_gcc481
 eval `scramv1 runtime -sh`
-source $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/scripts/setup_libs.sh
+cd /vols/build/cms/akd116/newest/run/CMSSW_8_0_25/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/
+source /vols/build/cms/akd116/newest/run/CMSSW_8_0_25/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/scripts/setup_libs.sh
 
 ulimit -c 0
 
 inputNumber=$(printf "%04d\n" $((SGE_TASK_ID-1)))
 
-python $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/scripts/addSVFits.py --intree $input --svfit_path $svfit_path --path $path --tag $tag --channel $channel --year $year &> jobs/${input}_${channel}_${year}_svfit.log
+python /vols/build/cms/akd116/newest/run/CMSSW_8_0_25/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/scripts/addSVFits.py --intree $input --svfit_path $svfit_path --path $path --tag $tag --channel $channel --year $year &> jobs/${input}_${channel}_${year}_svfit.log
 
