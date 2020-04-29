d10011c0e5246b687158d1319988a914fa92b0b2
Albert <dow.albert@gmail.com>
2020-04-29 09:40:54 +0100

    batch_datacards: change HiggsTauTau -> HiggsTauTauRun2 for using new analyser

--- Analysis/HiggsTauTauRun2/scripts/batch_datacards.sh
+++ Analysis/HiggsTauTauRun2/scripts/batch_datacards.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau
+cd $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTauRun2
 export SCRAM_ARCH=slc6_amd64_gcc481
 eval `scramv1 runtime -sh`
-source $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/scripts/setup_libs.sh
+source $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTauRun2/scripts/setup_libs.sh
 
 ulimit -c 0
 
 var_=${var//\'}
 
-python $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTau/scripts/HiggsTauTauPlot.py  --no_plot --cfg=$CFG --channel=$ch --method=$cat_num --cat=$cat_str --year=$YEAR --outputfolder=$output_folder --datacard=$dc --paramfile=$PARAMS --folder=$FOLDER $BLIND --var=$var_ $extra &> test.out
+python $CMSSW_BASE/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTauRun2/scripts/HiggsTauTauPlot.py  --no_plot --cfg=$CFG --channel=$ch --method=$cat_num --cat=$cat_str --year=$YEAR --outputfolder=$output_folder --datacard=$dc --paramfile=$PARAMS --folder=$FOLDER $BLIND --var=$var_ $extra &> test.out
