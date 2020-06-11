116ddafa67048f96070e3551e971cb903ba53fca
Albert <dow.albert@gmail.com>
2020-06-11 12:14:00 +0100

    HTTEventClassifier: update with latest training conditions to show example on twiki

--- Analysis/HiggsTauTauRun2/interface/HTTEventClassifier.h
+++ Analysis/HiggsTauTauRun2/interface/HTTEventClassifier.h
@@ -46,22 +46,19 @@ class HTTEventClassifier : public ModuleBase {
   float event_;
   unsigned long long evt_;
 
-  double dijetpt_;
   double jdeta_;
   double jpt_1_;
-  double m_sv_;
+  double svfit_mass_;
   double m_vis_;
   double met_;
   double mjj_;
   unsigned n_jets_;
-  unsigned n_lowpt_jets_;
   double pt_1_;
-  double pt_2_;
   double pt_tt_;
   double pt_vis_;
 
 
-  float var0_, var1_, var2_, var3_, var4_, var5_, var6_, var7_, var8_, var9_, var10_, var11_;
+  float var0_, var1_, var2_, var3_, var4_, var5_, var6_, var7_, var8_, var9_;
 
 };
 
