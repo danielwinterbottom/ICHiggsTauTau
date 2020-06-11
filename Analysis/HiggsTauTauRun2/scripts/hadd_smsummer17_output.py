67057a4bfd6e489a43441ab7c6c76758cc679d56
Albert <dow.albert@gmail.com>
2020-06-11 12:21:21 +0100

    scripts/hadd_smsummer17_output.py: minor change

--- Analysis/HiggsTauTauRun2/scripts/hadd_smsummer17_output.py
+++ Analysis/HiggsTauTauRun2/scripts/hadd_smsummer17_output.py
@@ -196,8 +196,7 @@ sample_list = [
 	]
 
 
-# channel = ['em','et','mt','tt','zee','zmm','wmnu','tpzee','tpzmm','tpmt','tpem']
-channel = ['zmm']
+channel = ['em','et','mt','tt','zee','zmm','wmnu','tpzee','tpzmm','tpmt','tpem']
 with open("%(samplelist)s"%vars(),"r") as inf:
   lines = inf.readlines()
 
