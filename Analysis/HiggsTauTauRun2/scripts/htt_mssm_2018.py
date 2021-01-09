53a492060565399296671b476083b5eb53e6ec21
George <gputtley@hotmail.com>
2021-01-09 15:21:49 +0000

    Adding year to files_per_sample text file

--- Analysis/HiggsTauTauRun2/scripts/htt_mssm_2018.py
+++ Analysis/HiggsTauTauRun2/scripts/htt_mssm_2018.py
@@ -167,10 +167,10 @@ signal_mc = [ ]
 signal_vh = [ ]
 signal_mc_ww = [ ]
 
-if os.path.isfile("./jobs/files_per_sample.txt"):
-    os.system("mv ./jobs/files_per_sample.txt ./jobs/files_per_sample-%(BACKUPNAME)s.txt"%vars())
+if os.path.isfile("./jobs/files_per_sample_2018.txt"):
+    os.system("mv ./jobs/files_per_sample_2018.txt ./jobs/files_per_sample_2018-%(BACKUPNAME)s.txt"%vars())
 
-file_persamp = open("./jobs/files_per_sample.txt", "w")
+file_persamp = open("./jobs/files_per_sample_2018.txt", "w")
 
 
 if options.proc_sm or options.proc_all:
