096db153c4afde95a5b717079c9b654a5ec926f6
KingCol13 <kingsleydgs@gmail.com>
2021-01-27 00:42:50 +0000

    Fixed signs of neutrinos.

--- Analysis/HiggsTauTauRun2/test/TauSpinnerMasters.cpp
+++ Analysis/HiggsTauTauRun2/test/TauSpinnerMasters.cpp
@@ -57,10 +57,10 @@ int main(/*int argc, char* argv[]*/)
 	
 	// Setup particles
 	Particle pi_1, pi_2, pi0_1, pi0_2;
-	setupParticle(tree, "pi", pi_1, 211, 1);
-	setupParticle(tree, "pi", pi_2, -211, 2);
-	setupParticle(tree, "pi0", pi0_1, 111, 1);
-	setupParticle(tree, "pi0", pi0_2, -111, 2);
+	setupParticle(tree, "pi", pi_1, -211, 1);
+	setupParticle(tree, "pi", pi_2, 211, 2);
+	setupParticle(tree, "pi0", pi0_1, -111, 1);
+	setupParticle(tree, "pi0", pi0_2, 111, 2);
 	// Set neutrinos fixed for now
 	Particle nu_1{0, 3, 4, 5, 16};
 	Particle nu_2{0, -3, 4, 5, -16};
@@ -90,23 +90,29 @@ int main(/*int argc, char* argv[]*/)
   	) // Event Selection
   	{
 			//std::cout << "pi_px_1 = " << pi_1.px << std::endl;
-			Particle tau_1 = addParticles(addParticles(pi_1, pi0_1, 0), nu_1, 15);
-			Particle tau_2 = addParticles(addParticles(pi_2, pi0_2, 0), nu_1, -15);
+			Particle a1_1 = addParticles(pi_1, pi0_1, -213);//20213);
+			Particle a1_2 = addParticles(pi_2, pi0_2, 213);//-20213);
+			Particle tau_1 = addParticles(a1_1, nu_1, 15);
+			Particle tau_2 = addParticles(a1_2, nu_2, -15);
 			Particle Higgs = addParticles(tau_1, tau_2, 25);
 			
 			// Make simple_tau_daughters vectors
 			std::vector<TauSpinner::SimpleParticle> simple_tau1_daughters, simple_tau2_daughters;
 			simple_tau1_daughters.push_back(convertToSimplePart(pi_1));
 			simple_tau1_daughters.push_back(convertToSimplePart(pi0_1));
+			simple_tau1_daughters.push_back(convertToSimplePart(a1_1));
 			simple_tau1_daughters.push_back(convertToSimplePart(nu_1));
+			
 			simple_tau2_daughters.push_back(convertToSimplePart(pi_2));
 			simple_tau2_daughters.push_back(convertToSimplePart(pi0_2));
+			simple_tau2_daughters.push_back(convertToSimplePart(a1_2));
 			simple_tau2_daughters.push_back(convertToSimplePart(nu_2));
 			
 			auto simple_tau1 = convertToSimplePart(tau_1);
 			auto simple_tau2 = convertToSimplePart(tau_2);
 			auto simple_boson = convertToSimplePart(Higgs);
 			
+			TauSpinner::setHiggsParametersTR(-cos(2*M_PI*0),cos(2*M_PI*0),-sin(2*M_PI*0),-sin(2*M_PI*0));
 			double weight = TauSpinner::calculateWeightFromParticlesH(simple_boson,simple_tau1,simple_tau2,simple_tau1_daughters,simple_tau2_daughters);
 			std::cout << "Weight " << i << " = " << weight << std::endl;
   	} // Event selection
