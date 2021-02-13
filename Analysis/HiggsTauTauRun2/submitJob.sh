6b3bc153fc0c865c27ca1eb5407b1ddcda6c24ec
Alie <alie.craplet@gmail>
2021-02-13 11:27:25 +0000

    Changed SubmitJob.sh for reco a1-a1

--- Analysis/HiggsTauTauRun2/submitJob.sh
+++ Analysis/HiggsTauTauRun2/submitJob.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export X509_USER_PROXY=/home/hep/ktc17/cms.proxy
+export X509_USER_PROXY=/home/hep/ac4317/cms.proxy
 source /vols/grid/cms/setup.sh
 export SCRAM_ARCH=slc7_amd64_gcc700
 eval `scramv1 runtime -sh`
-source /vols/cms/ktc17/CMSSW_10_2_19/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTauRun2/scripts/setup_libs.sh
+source /vols/cms/ac4317/msci/CMSSW_10_2_19/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTauRun2/scripts/setup_libs.sh
 ulimit -c 0
 lx03.hep.ph.ic.ac.uk
-./bin/TauSpinnerMasters pseudo /vols/cms/ktc17/MVAFILE_AllHiggs_tt.root MVAFILE_AllHiggs_tt_pseudo_phitt.root
+./bin/TauSpinnerMasters reco /vols/cms/ac4317/msci/MVAFILE_AllHiggs_tt_regr_10_10.root MVAFILE_AllHiggs_tt_reco_10_10_phitt.root
