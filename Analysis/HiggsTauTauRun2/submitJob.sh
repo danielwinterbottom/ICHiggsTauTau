20679d167eeb67c1a4a0511d296f671a5a768716
Alie <alie.craplet@gmail>
2021-02-25 11:44:03 +0000

    First works on getting the sv_angle in the a1a1 channel

--- Analysis/HiggsTauTauRun2/submitJob.sh
+++ Analysis/HiggsTauTauRun2/submitJob.sh
@@ -6,4 +6,4 @@ eval `scramv1 runtime -sh`
 source /vols/cms/ac4317/msci/CMSSW_10_2_19/src/UserCode/ICHiggsTauTau/Analysis/HiggsTauTauRun2/scripts/setup_libs.sh
 ulimit -c 0
 lx03.hep.ph.ic.ac.uk
-./bin/TauSpinnerMasters reco /vols/cms/ac4317/msci/MVAFILE_AllHiggs_tt_regr_10_10.root MVAFILE_AllHiggs_tt_reco_10_10_phitt.root
+./bin/TauSpinnerMasters reco /vols/cms/ac4317/msci/MVAFILE_AllHiggs_tt_1_1_reco.root MVAFILE_AllHiggs_tt_reco_1_1_phitt.root
