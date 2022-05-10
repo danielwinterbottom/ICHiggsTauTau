967e4edc0abbf3b474752bd0711baf1bc367cacf
Klitos <kleitos.savva.97@gmail.com>
2022-05-10 13:59:52 +0100

    Get Effective Updates

--- Analysis/HiggsTauTauRun2/scripts/getEffectiveEvents.py
+++ Analysis/HiggsTauTauRun2/scripts/getEffectiveEvents.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 file_names = {}
-channels = ['mt']
+channels = ['zmm']
 years = ['2016','2017','2018']
 for file_name in os.listdir(sys.argv[1]):
     if '.root' not in file_name:
@@ -15,7 +15,7 @@ for file_name in os.listdir(sys.argv[1]):
                 file_names[key] = file_name
 
 for f in file_names:
-    if "SingleMuon" in f or "SingleElectron" in f or "EGamma" in f or f in "MuonEG" or\
+    if not "SingleMuon" in f or "SingleElectron" in f or "EGamma" in f or f in "MuonEG" or\
             "\bTau" in f: 
         continue
         input_file = ROOT.TFile(sys.argv[1]+'/'+file_names[f])
