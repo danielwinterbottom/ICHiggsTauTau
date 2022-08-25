0a65c49a18bca90396caf7419cf8d631ab3f947e
Klitos <kleitos.savva.97@gmail.com>
2022-08-25 09:55:15 +0100

    Added option to run 3tau channel

--- Analysis/4tau/scripts/makeDatacards_4tau.py
+++ Analysis/4tau/scripts/makeDatacards_4tau.py
@@ -69,11 +69,20 @@ def SubmitBatchJob(name,time=180,memory=24,cores=1):
   else: os.system('qsub -e %(error_log)s -o %(output_log)s -V -q hep.q -l h_rt=0:%(time)s:0 -l h_vmem=%(memory)sG -cwd %(name)s' % vars())
 
 all_ch_variables = [
+                    #GetBinning('pt_1',0,400,100,round=1),
+                    #GetBinning('pt_2',0,400,100,round=1),
+                    #GetBinning('pt_3',0,400,100,round=1),
+                    #GetBinning('mt_lep_12',0,400,100,round=1),
+                    #GetBinning('mt_lep_13',0,400,100,round=1),
+                    #GetBinning('mt_lep_23',0,400,100,round=1),
+                    #GetBinning('mvis_12',0,400,100,round=1),
+                    #GetBinning('mvis_13',0,400,100,round=1),
+                    #GetBinning('mvis_23',0,400,100,round=1),
                     GetBinning('mvis_min_sum_dR_1',0,500,100,round=1),
-                    GetBinning('mvis_min_sum_dR_2',0,300,60,round=1),
-                    GetBinning('pt_min_dphi_1',0,300,60,round=1),
-                    GetBinning('pt_min_dphi_2',0,300,60,round=1),
-                    "mvis_min_sum_dR_1,mvis_min_sum_dR_2[0,50,100,150,200,250,300,350,400],[0,50,100,150,200,250,300,350,400]",
+                    #GetBinning('mvis_min_sum_dR_2',0,300,60,round=1),
+                    #GetBinning('pt_min_dphi_1',0,300,60,round=1),
+                    #GetBinning('pt_min_dphi_2',0,300,60,round=1),
+                    #"mvis_min_sum_dR_1,mvis_min_sum_dR_2[0,50,100,150,200,250,300,350,400],[0,50,100,150,200,250,300,350,400]",
                     ]
 
 ch_dep_var = {"mttt":[],
@@ -82,18 +91,16 @@ ch_dep_var = {"mttt":[],
               "eett":[],
               "tttt":[],
               "emtt":[],
+	      "ttt":[],
               }
 
 unb_ch_dep_var = {"mttt":[],
               "ettt":[],
-              "mmtt":[
-                      GetBinning('mvis_12',0,300,60,round=1),
-                      ],
-              "eett":[
-                      GetBinning('mvis_12',0,300,60,round=1),
-                      ],
+              "mmtt":[],
+              "eett":[],
               "tttt":[],
               "emtt":[],
+	      "ttt":[],
               }
 
 
@@ -103,6 +110,7 @@ config_files = {'2016':'scripts/plot_UL_2016.cfg',
                }
 
 categories = {
+              "ttt":["inclusive","nobtag","btag"],
               "tttt":["inclusive","nobtag","btag"],
               "ettt":["inclusive","nobtag","btag"],
               "mttt":["inclusive","nobtag","btag"],
@@ -112,7 +120,7 @@ categories = {
               }
 
 add_options = ''
-#add_options = '--ratio_range=\'0,3\' --plot_signals=\'phi200A100To4Tau\'  --auto_rebinning --bin_uncert_fraction=0.15 --signal_scale=10'
+add_options = '--ratio_range=\'0,3\' --plot_signals=\'phi200A100To4Tau\'  --auto_rebinning --bin_uncert_fraction=0.15 --signal_scale=10'
 #add_options = '--ratio_range=\'0,3\' --plot_signals=\'phi200A100To4Tau\'  --auto_rebinning --bin_uncert_fraction=0.15 --signal_scale=10 --vsjets=None --add_wt=\'1/(idisoweight_3*idisoweight_4)\''
  
 blind_options = '--blind --x_blind_min=-999 --x_blind_max=999'
