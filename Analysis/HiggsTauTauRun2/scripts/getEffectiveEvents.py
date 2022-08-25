ab8040077d480f4f5eb5dfd4d11bf90b7a537475
Klitos <kleitos.savva.97@gmail.com>
2022-08-25 09:02:22 +0100

    Fixed an issue related to the TTToSemiLeptonic sample

--- Analysis/HiggsTauTauRun2/scripts/getEffectiveEvents.py
+++ Analysis/HiggsTauTauRun2/scripts/getEffectiveEvents.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 file_names = {}
-channels = ['mt','zee','tt','et','zmm']
+channels = ['tt']
 years = ['2016','2017','2018']
 for file_name in os.listdir(sys.argv[1]):
     if '.root' not in file_name:
@@ -15,13 +15,14 @@ for file_name in os.listdir(sys.argv[1]):
                 file_names[key] = file_name
 
 for f in file_names:
-    if ("SingleMuon" in f or "SingleElectron" in f or "EGamma" in f or f in "MuonEG" or "Tau" in f): 
+    if "SingleMuon" in f or "SingleElectron" in f or "EGamma" in f or f in "MuonEG" or\
+            "\bTau" in f: 
+        continue
         input_file = ROOT.TFile(sys.argv[1]+'/'+file_names[f])
-        tree = input_file.Get("effective")
+        tree = input_file.Get("ntuple")
         entries = tree.GetEntries()
         print("{}  {}".format(f,entries))
     else:
-        continue
         input_file = ROOT.TFile(sys.argv[1]+'/'+file_names[f])
         tree = input_file.Get("effective")
         if 'JJH' in f and 'Higgs' not in f: entries = tree.GetEntries('wt>0&&wt_cp_prod_sm!=0') - tree.GetEntries('wt<0&&wt_cp_prod_sm!=0') 
