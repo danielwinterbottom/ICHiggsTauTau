040f5053ed903758fb1f30b7e873f0db339267bd
George <[Email>
2022-12-05 16:19:49 +0000

    Implenting fakes

--- Analysis/4tau/scripts/combined_year_4tauPlot.py
+++ Analysis/4tau/scripts/combined_year_4tauPlot.py
@@ -4,7 +4,11 @@ import ROOT
 import copy
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--channel',help= 'Channel to run for', default='mmtt')
+parser.add_argument('--extra_name',help= 'Extra name to add to datacards', default='combined')
+parser.add_argument('--cat',help= 'Category to run for', default='inclusive')
 parser.add_argument('--options',help= 'Options to add to 4tauPlot', default='')
+parser.add_argument('--combined_options',help= 'Options to add to 4tauPlot when running combined year', default='')
 parser.add_argument('--outputfolder',help= 'Options folder to store plots/datacards', default='output')
 parser.add_argument('--run_datacards', help= 'Run datacards', action='store_true')
 args = parser.parse_args()
@@ -22,20 +26,25 @@ year_cfg = {
             "2018"        :"scripts/plot_UL_2018.cfg",
             }
 
+if args.extra_name != "": args.extra_name =  args.extra_name + "_"
+dc_name = args.extra_name + args.channel + "_" + args.cat
+
+
 if args.run_datacards:
   for year, config in year_cfg.items():
-    os.system("python scripts/4tauPlot.py --outputfolder={} --datacard=combined_year_{} --cfg={} {}".format(args.outputfolder,year,config,args.options))
+    print "python scripts/4tauPlot.py --outputfolder={} --replace_name={}_{} --cfg={} --channel={} --cat={} {}".format(args.outputfolder,dc_name,year,config,args.channel,args.cat,args.options)
+    os.system("python scripts/4tauPlot.py --outputfolder={} --replace_name={}_{} --cfg={} --channel={} --cat={} {}".format(args.outputfolder,dc_name,year,config,args.channel,args.cat,args.options))
 
-ch_name = "mmtt"
-cat_name = "inclusive"
-dir_name = "{}_{}".format(ch_name,cat_name)
+dir_name = "{}_{}".format(args.channel,args.cat)
 
-cf = ROOT.TFile("{}/combined_year_all.root".format(args.outputfolder),"RECREATE")
+rfn = "{}/{}_all.root".format(args.outputfolder,dc_name) 
+if os.path.exists(rfn): os.system("rm {}".format(rfn))
+cf = ROOT.TFile(rfn,"RECREATE")
 cf.mkdir(dir_name)
 
 hist_dict = {}
 for year, config in year_cfg.items():
-  f = ROOT.TFile("{}/combined_year_{}.root".format(args.outputfolder,year))
+  f = ROOT.TFile("{}/{}_{}.root".format(args.outputfolder,dc_name,year))
   for dkey in f.GetListOfKeys():
     d = f.Get(dkey.GetName())
     fnames = []
@@ -57,5 +66,6 @@ for key, val in hist_dict.items():
 cf.Close()
 cf.Write()
 
-os.system("python scripts/4tauPlot.py --outputfolder={} --plot_from_dc='{}/combined_year_all.root' --cfg={} --lumi='138 fb^{{-1}} (13 TeV)' --datacard={} --extra_name={}_combined_year_all {}".format(args.outputfolder,args.outputfolder,config,cat_name,ch_name,args.options))
+print "python scripts/4tauPlot.py --outputfolder={} --plot_from_dc='{}/{}_all.root' --cfg={} --lumi='138 fb^{{-1}} (13 TeV)' --replace_name={}_all  --channel={} --cat={} {} {}".format(args.outputfolder,args.outputfolder,dc_name,config,dc_name,args.channel,args.cat,args.options,args.combined_options)
+os.system("python scripts/4tauPlot.py --outputfolder={} --plot_from_dc='{}/{}_all.root' --cfg={} --lumi='138 fb^{{-1}} (13 TeV)' --replace_name={}_all  --channel={} --cat={} {} {}".format(args.outputfolder,args.outputfolder,dc_name,config,dc_name,args.channel,args.cat,args.options,args.combined_options))
 
