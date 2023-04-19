f0e9d8b886a227e52ef99af202fb616a6e5efa1c
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2023-04-19 15:21:07 +0100

    checking neighbouring evenyts first

--- Analysis/HiggsTauTauRun2/scripts/check_unique_events.py
+++ Analysis/HiggsTauTauRun2/scripts/check_unique_events.py
@@ -3,7 +3,7 @@ import argparse
 from sys import getsizeof
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', '-f', help= 'File to check for duplicated events', default='/vols/cms/dw515/outputs/MSSM/UL_Mar31_2018/DYJetsToLL-LO_zmm_2018.root')
+parser.add_argument('--file', '-f', help= 'File to check for duplicated events', default='/vols/cms/dw515/outputs/MSSM/UL_Mar31_2018_effective_v2/DYJetsToLL-LO_mt_2018.root')
 args = parser.parse_args()
 filename = args.file
 
@@ -18,11 +18,30 @@ print 'Checking file: %(filename)s' % vars()
 
 count=0
 N=tree.GetEntries()
+
+#first check looks if there are cases where the next even along is a duplicate (most likely to happen give the bug that caused the duplicates) 
+print 'Checking neighbouring events:' 
+foundDuplicates=False
+for i in range(0,tree.GetEntries(),2):
+  tree.GetEntry(i)
+  values_1 = (tree.run, tree.lumi, tree.event)
+  tree.GetEntry(i+1)
+  values_2 = (tree.run, tree.lumi, tree.event)
+
+  if values_1==values_2: 
+    foundDuplicates=True 
+    break
+
+if foundDuplicates: print 'Found duplicated events!'
+else:
+  print 'No duplicates found'
+
+# the next check looks for duplicates anywhere in the tree (can have issues with memory for large samples)
+print 'Checking all events:' 
 for i in range(0,tree.GetEntries()):
     tree.GetEntry(i)
 
     values = (tree.run, tree.lumi, tree.event)
-    event = tree.GetLeaf('event').GetValue()
     if values in unique_sets:
         count+=1
         duplicated_sets.add(values)
