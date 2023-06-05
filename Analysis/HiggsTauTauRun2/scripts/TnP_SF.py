e9013871a521ad82e7c2587619d92e128a83dba5
Klitos <kleitos.savva.97@gmail.com>
2023-06-05 22:57:15 +0100

    Small update

--- Analysis/HiggsTauTauRun2/scripts/TnP_SF.py
+++ Analysis/HiggsTauTauRun2/scripts/TnP_SF.py
@@ -7,6 +7,7 @@ parser = OptionParser()
 parser.add_option('--channel',help= 'Name of channel', default='tpzee')
 parser.add_option("--year", dest="year", type='string', default='2018',help="Year input")
 parser.add_option("--input_folder", help = 'Name of the input folder', default='plots/TnP/')
+parser.add_option("--run_systs", help = 'Run systematics', default=False)
 (options, args) = parser.parse_args()
 
 channel = options.channel
@@ -16,12 +17,13 @@ file_name = "muon"
 if channel == "tpzee": file_name = "electron"
 
 # Open the source and destination root files
-f_nom = ROOT.TFile.Open("{}/Single/{}/{}/{}_nom.root".format(input_folder,channel,year,file_name))
-f_sig = ROOT.TFile.Open("{}/Single/{}/{}/{}_sig.root".format(input_folder,channel,year,file_name))
-f_bkg = ROOT.TFile.Open("{}/Single/{}/{}/{}_bkg.root".format(input_folder,channel,year,file_name))
-f_tag = ROOT.TFile.Open("{}/Single/{}/{}/{}_tightTag.root".format(input_folder,channel,year,file_name))
+f_nom = ROOT.TFile.Open("{}/{}/{}/{}_nom.root".format(input_folder,year,channel,file_name))
+if options.run_systs:
+   f_sig = ROOT.TFile.Open("{}/{}/{}/{}_sig.root".format(input_folder,year,channel,file_name))
+   f_bkg = ROOT.TFile.Open("{}/{}/{}/{}_bkg.root".format(input_folder,year,channel,file_name))
+   f_tag = ROOT.TFile.Open("{}/{}/{}/{}_tightTag.root".format(input_folder,year,channel,file_name))
 
-output_folder = "{}/Single/{}/{}/correction/".format(input_folder,channel,year)
+output_folder = "{}/{}/{}/correction/".format(input_folder,year,channel)
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
@@ -34,14 +36,14 @@ names = ["data_id_eff","ZLL_id_eff","data_iso_eff","ZLL_iso_eff","data_trg_eff",
 # Get the histogram from the source root file
 for name in types:
   h_nom_data = f_nom.Get("data_{}_eff".format(name))
-  h_sig_data = f_sig.Get("data_{}_eff".format(name))
-  h_bkg_data = f_bkg.Get("data_{}_eff".format(name))
-  h_tag_data = f_tag.Get("data_{}_eff".format(name))
-
   h_nom_ZLL = f_nom.Get("ZLL_{}_eff".format(name))
-  h_sig_ZLL = f_sig.Get("ZLL_{}_eff".format(name))
-  h_bkg_ZLL = f_bkg.Get("ZLL_{}_eff".format(name))
-  h_tag_ZLL = f_tag.Get("ZLL_{}_eff".format(name))
+  if options.run_systs:
+     h_sig_data = f_sig.Get("data_{}_eff".format(name))
+     h_sig_ZLL = f_sig.Get("ZLL_{}_eff".format(name))
+     h_bkg_data = f_bkg.Get("data_{}_eff".format(name))
+     h_bkg_ZLL = f_bkg.Get("ZLL_{}_eff".format(name))
+     h_tag_data = f_tag.Get("data_{}_eff".format(name))
+     h_tag_ZLL = f_tag.Get("ZLL_{}_eff".format(name))
 
   # Create a copy of the histogram in the destination root file
   h_SF = ROOT.TH2D('ScaleFactor_{}'.format(name), '', h_nom_data.GetNbinsX(), h_nom_data.GetXaxis().GetXmin(), h_nom_data.GetXaxis().GetXmax(),h_nom_data.GetNbinsY(), h_nom_data.GetYaxis().GetXmin(), h_nom_data.GetYaxis().GetXmax())
@@ -67,30 +69,29 @@ for name in types:
           stat_error = (((data_y_err_high**2 * ZLL_y_val) + (ZLL_y_err_high**2 * data_y_val))/ZLL_y_val**4)**0.5
 
           content_nom_data = h_nom_data.GetBinContent(xbin,ybin)
-          content_sig_data = h_sig_data.GetBinContent(xbin, ybin)
-          content_bkg_data = h_bkg_data.GetBinContent(xbin,ybin)
-          content_tag_data = h_tag_data.GetBinContent(xbin,ybin)
-
           content_nom_ZLL = h_nom_ZLL.GetBinContent(xbin,ybin)
-          content_sig_ZLL = h_sig_ZLL.GetBinContent(xbin, ybin)
-          content_bkg_ZLL = h_bkg_ZLL.GetBinContent(xbin,ybin)
-          content_tag_ZLL = h_tag_ZLL.GetBinContent(xbin,ybin)
-
-          error = (stat_error**2 + abs((content_sig_data/content_sig_ZLL)-(content_nom_data/content_nom_ZLL))**2 + abs((content_bkg_data/content_bkg_ZLL)-(content_nom_data/content_nom_ZLL))**2 + abs((content_tag_data/content_tag_ZLL)-(content_nom_data/content_nom_ZLL))**2)**0.5
-
-          errorbkg= abs((content_bkg_data/content_bkg_ZLL)-(content_nom_data/content_nom_ZLL))
-          errorww = (abs((content_sig_data/content_sig_ZLL)-(content_nom_data/content_nom_ZLL))) 
-          errortt = (abs((content_tag_data/content_tag_ZLL)-(content_nom_data/content_nom_ZLL)))
-           
-          ratio = content_nom_data/content_nom_ZLL
+          if options.run_systs:
+             content_sig_data = h_sig_data.GetBinContent(xbin, ybin)
+             content_sig_ZLL = h_sig_ZLL.GetBinContent(xbin, ybin)
+             content_bkg_data = h_bkg_data.GetBinContent(xbin,ybin)
+             content_bkg_ZLL = h_bkg_ZLL.GetBinContent(xbin,ybin)
+             content_tag_data = h_tag_data.GetBinContent(xbin,ybin)
+             content_tag_ZLL = h_tag_ZLL.GetBinContent(xbin,ybin)
 
           if name == "trg" and ((h_SF.GetXaxis().GetBinUpEdge(xbin) <= 33 and year=="2018") or (h_SF.GetXaxis().GetBinUpEdge(xbin) <= 28 and year=="2017") or (h_SF.GetXaxis().GetBinUpEdge(xbin) <= 26 and year=="2016post") or (h_SF.GetXaxis().GetBinUpEdge(xbin) <= 26 and year=="2016pre")):
              ratio = 0
              error = 0
+          else:
+             if options.run_systs:
+                error = (stat_error**2 + abs((content_sig_data/content_sig_ZLL)-(content_nom_data/content_nom_ZLL))**2 + abs((content_bkg_data/content_bkg_ZLL)-(content_nom_data/content_nom_ZLL))**2 + abs((content_tag_data/content_tag_ZLL)-(content_nom_data/content_nom_ZLL))**2)**0.5
 
-          if ratio!=0:
-             if (error) > 0.05: print(name,xbin,h_SF.GetXaxis().GetBinLowEdge(xbin),ybin,"ERROR: ",error,"STAT: ",stat_error/error,"BKG: ",errorbkg/error,"SIGNAL: ",errorww/error,"TAG: ",errortt/error)
-
+                errorbkg= abs((content_bkg_data/content_bkg_ZLL)-(content_nom_data/content_nom_ZLL))
+                errorww = (abs((content_sig_data/content_sig_ZLL)-(content_nom_data/content_nom_ZLL))) 
+                errortt = (abs((content_tag_data/content_tag_ZLL)-(content_nom_data/content_nom_ZLL)))
+             else:
+                error = 0       
+             
+             ratio = content_nom_data/content_nom_ZLL
           h_SF.SetBinContent(xbin,ybin, ratio)
           h_SF.SetBinError(xbin,ybin, error)
           
@@ -101,19 +102,21 @@ for name in types:
   
   # Close the root files
   del h_nom_data
-  del h_sig_data
-  del h_bkg_data
-  del h_tag_data
   del h_nom_ZLL
-  del h_sig_ZLL
-  del h_bkg_ZLL
-  del h_tag_ZLL
+  if options.run_systs:
+     del h_sig_data
+     del h_sig_ZLL
+     del h_bkg_data
+     del h_bkg_ZLL
+     del h_tag_data
+     del h_tag_ZLL
   del h_SF
 
 f_nom.Close()
-f_sig.Close()
-f_bkg.Close()
-f_tag.Close()
+if options.run_systs:
+   f_sig.Close()
+   f_bkg.Close()
+   f_tag.Close()
 destination_file.Close()
 
 destination_file = ROOT.TFile.Open("{}/{}_SF.root".format(output_folder,file_name), "READ")
@@ -139,6 +142,7 @@ for name in types:
          histo1.SetBinError(i, bin_error)
 
          # Set the titles and axis labels for the new histograms
+         if name == "id": histo1.SetTitle('ID Scale Factor {} < |#eta| < {}'.format(h_SF.GetYaxis().GetBinLowEdge(j),h_SF.GetYaxis().GetBinUpEdge(j))) 
          if name == "iso": histo1.SetTitle('Isolation Scale Factor {} < |#eta| < {}'.format(h_SF.GetYaxis().GetBinLowEdge(j),h_SF.GetYaxis().GetBinUpEdge(j)))
          if name == "trg": histo1.SetTitle('Trigger Scale Factor {} < |#eta| < {}'.format(h_SF.GetYaxis().GetBinLowEdge(j),h_SF.GetYaxis().GetBinUpEdge(j)))
          histo1.GetXaxis().SetTitle('pT [GeV]')
