0714962b2494b78ad9a01e01606234f761800ff4
Endre <endre.bokor21@ic.ac.uk>
2023-07-05 13:18:17 +0100

    Fix 2022 py and change output folder

--- Analysis/HiggsTauTauRun2/scripts/htt_2022_preEE.py
+++ Analysis/HiggsTauTauRun2/scripts/htt_2022_preEE.py
@@ -71,7 +71,7 @@ parser.add_option("--scales", dest="scales", type='string', default='default',
 parser.add_option("--parajobs", dest="parajobs", action='store_true', default=False,
                   help="Submit jobs parametrically")
 
-parser.add_option("--config", dest="config", type='string', default='scripts/config_2022.json',
+parser.add_option("--config", dest="config", type='string', default='scripts/config_2022_preEE.json',
                   help="Config file")
 
 parser.add_option("--list_backup", dest="slbackupname", type='string', default='prevlist',
