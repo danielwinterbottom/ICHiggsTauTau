86391c82e9c5a74d0e28ba4a44631a43f33eebf3
Endre <endre.bokor21@ic.ac.uk>
2023-07-13 14:28:32 +0100

    Adding My Plotters

--- Analysis/HiggsTauTauRun2/scripts/compare_qcdmethods.py
+++ Analysis/HiggsTauTauRun2/scripts/compare_qcdmethods.py
@@ -1,29 +1,127 @@
 import ROOT as R
 from UserCode.ICHiggsTauTau.plotting import CompareHists
+import numpy as np
 
-file_w_qcdsub_name = "../output/datacard_m_vis_inclusive_tt_2018_ss.root"
-file_wo_qcdsub_name = "../output_nosubqcd/datacard_m_vis_inclusive_nosubqcd_tt_2018_ss.root"
+file_w_qcdsub_dirname = "../output/datacard_"
+file_wo_qcdsub_dirname = "../output_nosubqcd/datacard_"
+
+variables = [
+  
+  ["n_jets","N_{jets}"],
+  ["pt_1","p_{T}^{#tau_{1}}"],
+  ["pt_2", "p_{T}^{#tau_{2}}"],
+  ["mt_1", "m_{T}(p_{T}^{#tau_{1}},p_{T}^{miss})"],
+  ["mt_2","m_{T}(p_{T}^{#tau_{2}},p_{T}^{miss})"],
+  ["m_vis","m_{#tau#tau}"],
+  ["pt_tt", "p_{T}^{#tau#tau}"]
+
+]
+
+selnames = ["medium_vloose_medium",
+            "medium_vvvloose_medium",
+            "vloose_vvvloose_medium",
+            "medium_vloose_medium_vloose",
+            "medium_vvvloose_medium_vvvloose",
+            "vloose_vvvloose_vloose_vvvloose"
+            ]
+
+types=["SS","OppS"]
 
 output_folder_name = "."
-output_plotname = "QCD_ss_compare"
 
+chi2file = open('chi2test.txt','w')
 
-file_w_qcdsub= R.TFile.Open(file_w_qcdsub_name, "READ")
-file_w_qcdsub_dir = file_w_qcdsub.Get("tt_inclusive")
-(file_w_qcdsub_dir).cd()
-# if(file_w_qcdsub.IsZombie()):
-#   print("Error")
-hist_w_qcdsub = file_w_qcdsub_dir.Get("QCD")
+selnames_array = np.expand_dims(np.array(selnames,dtype = object),axis=1)
 
-file_wo_qcdsub = R.TFile.Open(file_wo_qcdsub_name, "READ")
-(file_wo_qcdsub_name, "READ")
-file_wo_qcdsub_dir = file_wo_qcdsub.Get("tt_inclusive")
-(file_wo_qcdsub_dir).cd()
-hist_wo_qcdsub = file_wo_qcdsub_dir.Get("QCD")
+for var in variables:
+  chi2file.write("----------- %s ------------ \n"% var[0])
+  ChiList = []
+  ChiList_SS = []
+  for sel in selnames:
+    for chargetype in types:
+#--------------------------------------------------------------------
+      filename = var[0]+"_inclusive_"+sel+"_tt_2018"
+      if chargetype == "SS":
+        file_w_qcdsub= R.TFile.Open(file_w_qcdsub_dirname+filename+"_ss.root", "READ")
+      else:
+        file_w_qcdsub= R.TFile.Open(file_w_qcdsub_dirname+filename+".root", "READ")
+      file_w_qcdsub_dir = file_w_qcdsub.Get("tt_inclusive")
+      (file_w_qcdsub_dir).cd()
+      hist_w_qcdsub = file_w_qcdsub_dir.Get("QCD")
+#--------------------------------------------------------------------
+      if chargetype == "SS":
+        file_wo_qcdsub = R.TFile.Open(file_wo_qcdsub_dirname+filename+"_ss.root", "READ")
+      else:
+        file_wo_qcdsub = R.TFile.Open(file_wo_qcdsub_dirname+filename+".root", "READ")
+      file_wo_qcdsub_dir = file_wo_qcdsub.Get("tt_inclusive")
+      (file_wo_qcdsub_dir).cd()
+      hist_wo_qcdsub = file_wo_qcdsub_dir.Get("QCD")
+#--------------------------------------------------------------------
+      sub_hist_w_qcdsub = file_w_qcdsub_dir.Get("data_obs")
+      tot_bkg_hist_w = file_w_qcdsub_dir.Get("total_bkg")
+      sub_hist_w_qcdsub.Add(tot_bkg_hist_w,-1)
+      sub_hist_w_qcdsub.Add(hist_w_qcdsub)
+#--------------------------------------------------------------------
+      p = hist_wo_qcdsub.Chi2Test(hist_w_qcdsub,"WW")
+      chi2 = hist_wo_qcdsub.Chi2Test(hist_w_qcdsub,"WW CHI2")
+      chi2NDF = hist_wo_qcdsub.Chi2Test(hist_w_qcdsub,"WW CHI2/NDF")
+      p_obs = hist_w_qcdsub.Chi2Test(sub_hist_w_qcdsub,"WW")
+      chi2_obs = hist_w_qcdsub.Chi2Test(sub_hist_w_qcdsub,"WW CHI2")
+      chi2NDF_obs = hist_w_qcdsub.Chi2Test(sub_hist_w_qcdsub,"WW CHI2/NDF")      
+      
+      if chargetype == "SS":
+        ChiList_SS.append([p,chi2,chi2NDF,p_obs,chi2_obs,chi2NDF_obs])
+      else:
+        ChiList.append([p,chi2,chi2NDF,p_obs,chi2_obs,chi2NDF_obs])
+#--------------------------------------------------------------------      
+      output_file = R.TFile(output_folder_name+"/"+var[0]+"_"+sel+"_"+chargetype+".root","RECREATE")
+      if var[0] == "n_jets":
+        CompareHists(hists = [hist_w_qcdsub,hist_wo_qcdsub,sub_hist_w_qcdsub],
+              legend_titles = ["#splitline{Background}{Subtracted} (%s)"%chargetype,"Data Only ("+chargetype+")","OBS - Non-QCD ("+chargetype+")"],
+              title="59.7 fb^{-1} (13 TeV)", extra_pad=0.2,ratio_range="0.6,1.4",
+              x_title = var[1], y_title = "Events",plot_name = var[0]+"_"+sel+"_"+chargetype, output_file = output_file)
+      else:
+        CompareHists(hists = [hist_w_qcdsub,hist_wo_qcdsub,sub_hist_w_qcdsub],
+              legend_titles = ["#splitline{Background}{Subtracted} (%s)"%chargetype,"Data Only ("+chargetype+")","OBS - Non-QCD ("+chargetype+")"],
+              title="59.7 fb^{-1} (13 TeV)", extra_pad=0.2,ratio_range="0.6,1.4",
+              x_title = var[1]+" (GeV)", y_title = "Events/GeV",plot_name = var[0]+"_"+sel+"_"+chargetype, output_file = output_file)
 
-
-output_file = R.TFile(output_folder_name+"/"+output_plotname+".root","RECREATE")
-c1 = R.TCanvas()
-CompareHists(hists = [hist_w_qcdsub,hist_wo_qcdsub],
-             legend_titles = ["Background Subtracted (SS)","Data Only (SS)"],title="59.7 fb^{-1} (13 TeV)", extra_pad=0.2,ratio_range="0.6,1.4",
-             x_title = "m_{#tau#tau} (GeV)", y_title = "Events/GeV",plot_name = output_plotname, output_file = output_file)
+#--------------------------------------------------------------------  
+      output_file.Close()
+      file_w_qcdsub.Close()
+      file_wo_qcdsub.Close()
+#--------------------------------------------------------------------      
+  ChiList_SS = np.array(ChiList_SS,dtype = object)
+  ChiList = np.array(ChiList,dtype = object)
+  ChiList = np.concatenate((selnames_array,ChiList), axis=1)
+  ChiList_SS = np.concatenate((selnames_array,ChiList_SS), axis=1)
+  ChiList = ChiList[ChiList[:,3].argsort()]
+  ChiList_SS = ChiList_SS[ChiList_SS[:,3].argsort()]
+  chi2file.write("------------ OppS ------------ \n")
+  order = 1
+  for ChiVar in ChiList:
+    chi2file.write("%i Type: %s\np: %.5f, chi2: %.5f, chi2/ndf: %.5f\n"%(order,ChiVar[0],ChiVar[1],
+                                                            ChiVar[2],ChiVar[3]))
+    order+=1
+  chi2file.write("------------- SS ------------ \n")
+  order = 1
+  for ChiVar in ChiList_SS:
+    chi2file.write("%i Type: %s\np: %.5f, chi2: %.5f, chi2/ndf: %.5f\n"%(order,ChiVar[0],ChiVar[1],
+                                                            ChiVar[2],ChiVar[3]))    
+    order+=1
+    
+  chi2file.write("------------ Compared to BKG------------ \n")
+  ChiList = ChiList[ChiList[:,-1].argsort()]
+  ChiList_SS = ChiList_SS[ChiList_SS[:,-1].argsort()]
+  chi2file.write("------------ OppS ------------ \n")
+  order = 1
+  for ChiVar in ChiList:
+    chi2file.write("%i Type: %s\np: %.5f, chi2: %.5f, chi2/ndf: %.5f\n"%(order,ChiVar[0],ChiVar[4],
+                                                            ChiVar[5],ChiVar[6]))  
+    order+=1
+  chi2file.write("------------- SS ------------ \n")
+  order = 1
+  for ChiVar in ChiList_SS:
+    chi2file.write("%i Type: %s\np: %.5f, chi2: %.5f, chi2/ndf: %.5f\n"%(order,ChiVar[0],ChiVar[4],
+                                                            ChiVar[5],ChiVar[6]))    
+    order+=1
--- Analysis/HiggsTauTauRun2/scripts/control_plots_tt.py
+++ Analysis/HiggsTauTauRun2/scripts/control_plots_tt.py
@@ -1,46 +1,40 @@
 import os
 
-years = ['2018','2017','2016']
-#years = ['2017']
-
 variables = [
 
-  'met[0,10,20,30,40,50,60,70,80,90,100,120,140,200,400]',
-  'n_jets(4,0,4)', 
-  'pt_1[40,45,50,55,60,65,70,80,90,100,120,140,200,400]',
-  'pt_2[40,45,50,55,60,65,70,80,90,100,120,140,200,400]',
-  'mt_1(30,0,300)',
-  'mt_2(30,0,300)',
-  'mt_lep(30,0,300)',
-  'm_vis(50,0,500)',
-  'met_dphi_1(20,0,3.1415927)',
-  'met_dphi_2(20,0,3.1415927)',
-  'mt_tot[50,60,70,80,100,120,140,160,180,200,250,300,350,400,500,700,900,3900]', 
-  'dphi(20,0,3.1415927)',
-  'n_deepbjets(4,0,4)', 
+  #'n_jets(4,0,4)' 
+  # 'pt_1(20,0,200)',
+  # 'pt_2(20,0,200)',
+   #'mt_1(30,0,300)'
+  # 'mt_2(30,0,300)',
+  # 'm_vis(20,0,200)',
+  # 'pt_tt(20,0,200)'
 ]
 
+selnames = ["medium_vloose_medium",
+            "medium_vvvloose_medium",
+            "vloose_vvvloose_medium",
+            "medium_vloose_medium_vloose",
+            "medium_vvvloose_medium_vvvloose",
+            "vloose_vvvloose_vloose_vvvloose"]
 
-for y in years:
-  for v in variables:
+for v in variables:
+  v_tt = v
+  for sel in range(0,6):
+    extra=' --ratio_range=\"0.6,1.4\" '
+    extra+= '--extra_name=%s'%selnames[sel]
+    extra += ' --tt_qcd_sel_num=%i'%sel
 
-    yp = y
+  #if 'mt_tot' in v: extra+='--log_y --log_x'
+  
 
-    v_tt = v
-
-
-    if y != '2018':
-      extra=' --norm_bins --extra_pad=0.4 --singletau --add_wt=\'wt_tau_trg_mssm*wt_tau_id_mssm*wt_prefire\' '
-    else:
-      extra=' --norm_bins --extra_pad=0.4 --singletau --add_wt=\'wt_tau_trg_mssm*wt_tau_id_mssm\' '
- 
-    if 'mt_tot' in v: extra='--log_y --log_x'
-
-    extra+=' --ratio_range=\"0.6,1.4\" '
- 
-    # add systematic bands
-    extra += ' --do_custom_uncerts --add_stat_to_syst --syst_tau_trg_diff="trg_syst_*" --syst_tau_id_diff="id_syst_*"  --do_ff_systs --syst_tquark="syst_ttbar_pt"  --syst_embedding_tt="syst_embed_tt" ' #--syst_tau_scale_grouped="tes_*group" '
-
-    os.system('python scripts/HiggsTauTauPlot.py --cfg=scripts/plot_mssm_%(yp)s.cfg --method=17 --channel=tt --var=\"%(v_tt)s\"   --outputfolder=mssm_control/ %(extra)s --embedding ' % vars())
-    os.system('python scripts/HiggsTauTauPlot.py --cfg=scripts/plot_mssm_%(yp)s.cfg --method=17 --channel=tt --var=\"%(v_tt)s\"   --outputfolder=mssm_control/ %(extra)s --datacard=mc ' % vars())
+  # add systematic bands
+  #extra += ' --do_custom_uncerts --add_stat_to_syst --syst_tau_trg_diff="trg_syst_*" --syst_tau_id_diff="id_syst_*"  --do_ff_systs --syst_tquark="syst_ttbar_pt"  --syst_embedding_tt="syst_embed_tt" ' #--syst_tau_scale_grouped="tes_*group" '
+    os.system('python scripts/HiggsTauTauPlot.py %(extra)s --cfg scripts/plot_UL_2018_nosubqcd.cfg --no_qcd_subtract --var=\"%(v_tt)s\"' % vars ())
+    os.system('python scripts/HiggsTauTauPlot.py %(extra)s --cfg scripts/plot_UL_2018.cfg --var=\"%(v_tt)s\"' % vars() )
+    os.system('python scripts/HiggsTauTauPlot.py %(extra)s --do_ss --cfg scripts/plot_UL_2018_nosubqcd.cfg --no_qcd_subtract --var=\"%(v_tt)  s\"' % vars())
+    os.system('python scripts/HiggsTauTauPlot.py %(extra)s --do_ss --cfg scripts/plot_UL_2018.cfg --var=\"%(v_tt)s\"' % vars() )
+  
+  #os.system('python scripts/HiggsTauTauPlot.py --cfg=scripts/plot_UL_%(yp)s.cfg --var=\"%(v_tt)s\"   --outputfolder=controlOutput/ %(extra)s --embedding ' % vars())
+  #os.system('python scripts/HiggsTauTauPlot.py --cfg=scripts/plot_UL_%(yp)s.cfg --var=\"%(v_tt)s\"   --outputfolder=controlOutput/ %(extra)s --datacard=mc ' % vars())
 
