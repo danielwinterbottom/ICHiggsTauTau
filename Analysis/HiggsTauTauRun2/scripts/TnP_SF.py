f076085e8bc121bbd46a33b977ee641cd4cf17d2
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2023-09-21 09:27:27 +0100

    removing year option, fixing issues with embed selection efficiencies when using --embed option

--- Analysis/HiggsTauTauRun2/scripts/TnP_SF.py
+++ Analysis/HiggsTauTauRun2/scripts/TnP_SF.py
@@ -15,7 +15,6 @@ options, remaining_argv = conf_parser.parse_known_args()
 
 defaults = {
     "channel":"tpzmm" ,
-    "year":"2018",
     "input_folder":"",
     "embed": False,
     "run_systs" : False,
@@ -30,26 +29,29 @@ parser = argparse.ArgumentParser(
     parents=[conf_parser]
     )
 parser.add_argument('--channel',type = str, help= 'Name of channel')
-parser.add_argument("--year", dest="year", type=str, help="Year input")
 parser.add_argument("--input_folder", help = 'Name of the input folder')
 parser.add_argument("--embed", dest="embed", action='store_true')
 parser.add_argument("--run_systs", dest="run_systs", action='store_true')
 options = parser.parse_args(remaining_argv)
 
+embed=options.embed
 channel = options.channel
-year = options.year
 input_folder = options.input_folder
 file_name = "muon"
 if channel == "tpzee": file_name = "electron"
 
 # Open the source and destination root files
-f_nom = ROOT.TFile.Open("{}/{}/{}/{}_nom.root".format(input_folder,year,channel,file_name))
+nom_fname = "{}/{}_nom.root".format(input_folder,file_name)
+if not os.path.exists(nom_fname):
+  print "{} does not exist, exiting".format(nom_fname)
+  exit()
+f_nom = ROOT.TFile.Open(nom_fname)
 if options.run_systs:
-   f_sig = ROOT.TFile.Open("{}/{}/{}/{}_sig.root".format(input_folder,year,channel,file_name))
-   f_bkg = ROOT.TFile.Open("{}/{}/{}/{}_bkg.root".format(input_folder,year,channel,file_name))
-   f_tag = ROOT.TFile.Open("{}/{}/{}/{}_tightTag.root".format(input_folder,year,channel,file_name))
+   f_sig = ROOT.TFile.Open("{}/{}_sig.root".format(input_folder,file_name))
+   f_bkg = ROOT.TFile.Open("{}/{}_bkg.root".format(input_folder,file_name))
+   f_tag = ROOT.TFile.Open("{}/{}_tightTag.root".format(input_folder,file_name))
 
-output_folder = "{}/{}/{}/correction/".format(input_folder,year,channel)
+output_folder = "{}/{}/correction/".format(input_folder,channel)
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
@@ -63,7 +65,7 @@ for name in types:
   h_nom_data = f_nom.Get("data_{}_eff".format(name))
   if not isinstance(h_nom_data, ROOT.TH2): continue
   h_nom_ZLL = f_nom.Get("ZLL_{}_eff".format(name))
-  if options.embed:
+  if embed:
     h_nom_EMB = f_nom.Get("embed_{}_eff".format(name))
   if options.run_systs:
      h_sig_data = f_sig.Get("data_{}_eff".format(name))
@@ -72,7 +74,7 @@ for name in types:
      h_sig_ZLL = f_sig.Get("ZLL_{}_eff".format(name))
      h_bkg_ZLL = f_bkg.Get("ZLL_{}_eff".format(name))
      h_tag_ZLL = f_tag.Get("ZLL_{}_eff".format(name))
-     if options.embed:
+     if embed:
        h_sig_EMB = f_sig.Get("embed_{}_eff".format(name))
        h_bkg_EMB = f_bkg.Get("embed_{}_eff".format(name))
        h_tag_EMB = f_tag.Get("embed_{}_eff".format(name))
@@ -81,7 +83,7 @@ for name in types:
   h_SF = ROOT.TH2D('ScaleFactor_{}'.format(name), '', h_nom_data.GetNbinsX(), h_nom_data.GetXaxis().GetXmin(), h_nom_data.GetXaxis().GetXmax(),h_nom_data.GetNbinsY(), h_nom_data.GetYaxis().GetXmin(), h_nom_data.GetYaxis().GetXmax())
   h_SF.SetBins(h_nom_data.GetNbinsX(), h_nom_data.GetXaxis().GetXbins().GetArray(),h_nom_data.GetNbinsY(), h_nom_data.GetYaxis().GetXbins().GetArray())
   h_SF.SetDirectory(destination_file)
-  if options.embed:
+  if embed:
     h_SF_EMB = ROOT.TH2D('ScaleFactor_EMB_{}'.format(name), '', h_nom_data.GetNbinsX(), h_nom_data.GetXaxis().GetXmin(), h_nom_data.GetXaxis().GetXmax(),h_nom_data.GetNbinsY(), h_nom_data.GetYaxis().GetXmin(), h_nom_data.GetYaxis().GetXmax())
     h_SF_EMB.SetBins(h_nom_data.GetNbinsX(), h_nom_data.GetXaxis().GetXbins().GetArray(),h_nom_data.GetNbinsY(), h_nom_data.GetYaxis().GetXbins().GetArray())
     h_SF_EMB.SetDirectory(destination_file)
@@ -105,20 +107,20 @@ for name in types:
             ZLL_y_err_low = h_ZLL_eff.GetErrorYlow(xbin-1)
             ZLL_y_err_high = h_ZLL_eff.GetErrorYhigh(xbin-1)
 
-            if options.embed:
+            if embed:
               h_EMB_eff = f_nom.Get("gr_embed_{}_eff_eta_{}_to_{}".format(name,bin_low_edge,bin_up_edge))
               EMB_y_val = h_EMB_eff.GetY()[xbin-1]
               EMB_y_err_low = h_EMB_eff.GetErrorYlow(xbin-1)
               EMB_y_err_high = h_EMB_eff.GetErrorYhigh(xbin-1)
          
             stat_error_ZLL = max(ZLL_y_err_high,ZLL_y_err_low)
-            stat_error_EMB = 0 if not options.embed else max(EMB_y_err_high,EMB_y_err_low)
+            stat_error_EMB = 0 if not embed else max(EMB_y_err_high,EMB_y_err_low)
             if ZLL_y_val != 0:
               # stat error on the overall MC SF
               stat_error = ((stat_error_data/data_y_val)**2+(stat_error_ZLL/ZLL_y_val)**2)**.5*data_y_val/ZLL_y_val 
             else: 
               stat_error = 0
-            if options.embed and EMB_y_val != 0:
+            if embed and EMB_y_val != 0:
               stat_error_embed_sf = ((stat_error_data/data_y_val)**2+(stat_error_EMB/EMB_y_val)**2)**.5*data_y_val/EMB_y_val
 
           if name == 'trg' and data_y_val<0.1:
@@ -136,8 +138,8 @@ for name in types:
           else:
             content_nom_data = h_nom_data.GetBinContent(xbin,ybin)
             content_nom_ZLL = 0 if num_only else h_nom_ZLL.GetBinContent(xbin,ybin)
-            if options.embed: 
-              content_nom_EMB = h_nom_EMB.GetBinContent(xbin,ybin)
+            if embed: 
+              content_nom_EMB = 0 if num_only else h_nom_EMB.GetBinContent(xbin,ybin)
             if options.run_systs:
               content_sig_data = h_sig_data.GetBinContent(xbin, ybin)
               content_sig_ZLL = 0 if num_only else h_sig_ZLL.GetBinContent(xbin, ybin)
@@ -145,10 +147,10 @@ for name in types:
               content_bkg_ZLL = 0 if num_only else h_bkg_ZLL.GetBinContent(xbin,ybin)
               content_tag_data = h_tag_data.GetBinContent(xbin,ybin)
               content_tag_ZLL = 0 if num_only else h_tag_ZLL.GetBinContent(xbin,ybin)
-              if options.embed and not num_only:
-                content_sig_EMB = h_sig_EMB.GetBinContent(xbin, ybin)
-                content_bkg_EMB = h_bkg_EMB.GetBinContent(xbin,ybin)
-                content_tag_EMB = h_tag_EMB.GetBinContent(xbin,ybin) 
+              if embed and not num_only:
+                content_sig_EMB = 0 if num_only else h_sig_EMB.GetBinContent(xbin, ybin)
+                content_bkg_EMB = 0 if num_only else h_bkg_EMB.GetBinContent(xbin,ybin)
+                content_tag_EMB = 0 if num_only else h_tag_EMB.GetBinContent(xbin,ybin) 
               if content_nom_ZLL == 0:
                 sig_error = 0
                 bkg_error = 0
@@ -159,7 +161,7 @@ for name in types:
                 bkg_error = 0. if content_bkg_ZLL<=0. else abs((content_bkg_data/content_bkg_ZLL)-(content_nom_data/content_nom_ZLL))**2
                 tag_error = 0. if content_tag_ZLL<=0. else abs((content_tag_data/content_tag_ZLL)-(content_nom_data/content_nom_ZLL))**2
                 ratio = content_nom_data/content_nom_ZLL
-              if options.embed:
+              if embed:
                 if content_nom_EMB == 0:
                   sig_error_emb = 0
                   bkg_error_emb = 0
@@ -173,7 +175,7 @@ for name in types:
               error = 0 if num_only else (stat_error**2 + sig_error + bkg_error + tag_error)**0.5
               error_data = (stat_error_data**2+(content_sig_data-content_nom_data)**2 + (content_bkg_data-content_nom_data)**2 + (content_tag_data-content_nom_data)**2)**.5 
               error_mc = 0 if num_only else (stat_error_ZLL**2+(content_sig_ZLL-content_nom_ZLL)**2 + (content_bkg_ZLL-content_nom_ZLL)**2 + (content_tag_ZLL-content_nom_ZLL)**2)**.5
-              if options.embed and not num_only:
+              if embed and not num_only:
                 error_embed_sf = (stat_error_embed_sf**2 + sig_error_emb + bkg_error_emb + tag_error_emb)**0.5  
                 error_embed = (stat_error_EMB**2+(content_sig_EMB-content_nom_EMB)**2 + (content_bkg_EMB-content_nom_EMB)**2 + (content_tag_EMB-content_nom_EMB)**2)**.5
           h_nom_data.SetBinError(xbin,ybin, error_data)
@@ -181,7 +183,7 @@ for name in types:
             h_SF.SetBinContent(xbin,ybin, ratio)
             h_SF.SetBinError(xbin,ybin, error)
             h_nom_ZLL.SetBinError(xbin,ybin, error_mc)
-            if options.embed:
+            if embed:
               h_SF_EMB.SetBinContent(xbin,ybin, ratio_emb)
               h_SF_EMB.SetBinError(xbin,ybin, error_embed_sf)
               h_nom_EMB.SetBinError(xbin,ybin, error_embed)          
@@ -191,7 +193,7 @@ for name in types:
   if not num_only:
     h_SF.Write()
     h_nom_ZLL.Write()
-    if options.embed:
+    if embed:
       h_nom_EMB.Write()
       h_SF_EMB.Write()
 
@@ -207,7 +209,7 @@ for name in types:
      del h_tag_data
      del h_tag_ZLL
   del h_SF
-  if options.embed: del h_SF_EMB
+  if embed: del h_SF_EMB
 
 f_nom.Close()
 if options.run_systs:
@@ -228,12 +230,12 @@ types = ["id","iso","trg"]
 for name in types:
    h_SF = destination_file.Get("ScaleFactor_{}".format(name))
    if not isinstance(h_SF, ROOT.TGraphAsymmErrors): continue
-   if options.embed:
+   if embed:
      h_SF_EMB = destination_file.Get("ScaleFactor_EMB_{}".format(name))
 
    for j in range(1, h_SF.GetNbinsY()+1):
      histo1 = ROOT.TH1D('ScaleFactor_{}_1D'.format(name), '', h_SF.GetNbinsX(), h_SF.GetXaxis().GetXbins().GetArray())
-     if options.embed: 
+     if embed: 
        histo2 = ROOT.TH1D('ScaleFactor_EMB_{}_1D'.format(name), '', h_SF_EMB.GetNbinsX(), h_SF_EMB.GetXaxis().GetXbins().GetArray())
      bin_contents = []
      bin_contents_emb = []
@@ -243,7 +245,7 @@ for name in types:
         bin_error = h_SF.GetBinError(i,j)
         histo1.SetBinContent(i,bin_content)
         histo1.SetBinError(i, bin_error)
-        if options.embed:
+        if embed:
           bin_content_emb = h_SF_EMB.GetBinContent(i,j)
           if bin_content_emb!=0: bin_contents_emb.append(bin_content)
           bin_error_emb = h_SF_EMB.GetBinError(i,j)
@@ -262,7 +264,7 @@ for name in types:
         histo1.GetYaxis().SetRangeUser(0.9*min(bin_contents+bin_contents_emb),1.1*histo1.GetMaximum())
         histo1.SetStats(0)
         histo1.Draw()  
-        if options.embed:
+        if embed:
           histo2.SetLineColor(ROOT.kRed)
           histo2.SetStats(0)
           histo2.Draw('same')
@@ -270,8 +272,8 @@ for name in types:
         canvas.Update()
         del canvas
         del histo1
-        if options.embed: del histo2
+        if embed: del histo2
    del h_SF
-   if options.embed: del h_SF_EMB 
+   if embed: del h_SF_EMB 
 
 
